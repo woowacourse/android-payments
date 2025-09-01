1aa8f186dd42536d551853f85de32e3a53495fe3
wisemuji <wisemuji@gmail.com>
2025-09-01 13:12:38 +0900

    feat: add iOS greeting

--- app/src/main/java/woowacourse/payments/MainActivity.kt
+++ app/src/main/java/woowacourse/payments/MainActivity.kt
@@ -24,6 +24,10 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+                    Greeting(
+                        name = "iOS",
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
