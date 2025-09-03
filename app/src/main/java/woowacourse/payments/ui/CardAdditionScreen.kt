fc99e18b8fcb04600bfaec6abbf51dfbac9dc67d
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-09-03 15:56:39 +0900

    feat: CardAdditionScreen 구현

---
+++ app/src/main/java/woowacourse/payments/ui/CardAdditionScreen.kt
@@ -0,0 +1,27 @@
+package woowacourse.payments.ui
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun CardAdditionScreen(modifier: Modifier = Modifier) {
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            CardAdditionTopBar(
+                onBackClick = {}, onSaveClick = {}
+            )
+        }) { paddingValues: PaddingValues ->
+        CardAdditionColumn(modifier = Modifier.padding(paddingValues))
+    }
+}
+
+@Preview
+@Composable
+private fun CardAdditionScreenPreview1() {
+    CardAdditionScreen()
+}
