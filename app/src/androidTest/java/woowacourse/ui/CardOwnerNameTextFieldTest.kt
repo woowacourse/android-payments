0a5004dd8bd409396919dc14022428827c2000f1
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-09-03 17:10:19 +0900

    test: CardOwnerNameTextField 테스트

---
+++ app/src/androidTest/java/woowacourse/ui/CardOwnerNameTextFieldTest.kt
@@ -0,0 +1,48 @@
+package woowacourse.ui
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import woowacourse.payments.ui.CardOwnerNameTextField
+
+class CardOwnerNameTextFieldTest {
+    @get:Rule
+    val composeRule = createComposeRule()
+
+    @Before
+    fun setUp() {
+        composeRule.setContent {
+            CardOwnerNameTextField()
+        }
+    }
+
+    @Test
+    fun `카드_소유자_이름의_길이는_30자를_넘을_수_없다`() {
+        // when
+        composeRule.onNodeWithText("")
+            .performTextInput("모".repeat(30))
+
+        composeRule.onNodeWithText("모".repeat(30))
+            .performTextInput("모찌")
+
+
+        // then
+        composeRule.onNodeWithText("모".repeat(30))
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun `입력된_이름의_길이가_출력된다`() {
+        // when
+        composeRule.onNodeWithText("")
+            .performTextInput("모찌")
+
+        // then
+        composeRule.onNodeWithText("2/30")
+            .assertIsDisplayed()
+    }
+}
--- app/src/main/java/woowacourse/payments/ui/CardOwnerNameTextField.kt
+++ app/src/main/java/woowacourse/payments/ui/CardOwnerNameTextField.kt
@@ -4,6 +4,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
@@ -11,9 +15,14 @@ import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun CardOwnerNameTextField(modifier: Modifier = Modifier) {
+    var name by remember { mutableStateOf("") }
+
     OutlinedTextField(
-        value = "",
-        onValueChange = {},
+        value = name,
+        onValueChange = { newName: String ->
+            name = newName.substring(0, newName.length.coerceAtMost(30))
+
+        },
         modifier = modifier,
         label = {
             Text(text = "카드 소유자 이름(선택)")
@@ -22,7 +31,11 @@ fun CardOwnerNameTextField(modifier: Modifier = Modifier) {
             Text(text = "카드에 표시된 이름을 입력하세요.", color = Color.Gray)
         },
         supportingText = {
-            Text(text = "0/30", modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.End)
+            Text(
+                text = "${name.length}/30",
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.End
+            )
         },
     )
 }
