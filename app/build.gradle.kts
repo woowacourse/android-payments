35f380ef248a5a1fc462a38fc210e2a89d578841
JunHyuk Im <imjh0417@gmail.com>
2025-09-03 17:51:43 +0900

    chore: META-INF 리소스 파일 제외

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,12 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunnerArguments["runnerBuilder"] = "de.mannodermaus.junit5.AndroidJUnit5Builder"
+
+        packaging {
+            resources {
+                excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            }
+        }
     }
 
     buildTypes {
