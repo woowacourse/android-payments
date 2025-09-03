a7bd731592b544fc0ca457f38384d243713db78e
DoableTuple <doabletuple@gmail.com>
2025-09-03 11:02:22 +0900

    feat: 카드 추가 화면 UI 구현

--- app/src/main/java/woowacourse/payments/MainActivity.kt
+++ app/src/main/java/woowacourse/payments/MainActivity.kt
@@ -1,16 +1,40 @@
 package woowacourse.payments
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import woowacourse.payments.ui.theme.AndroidpaymentsTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +43,144 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AndroidpaymentsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding: PaddingValues ->
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        NewCardTopBar(
+                            onBackClick = {
+                                Toast
+                                    .makeText(this@MainActivity, "onBackClick", Toast.LENGTH_SHORT)
+                                    .show()
+                            },
+                            onSaveClick = {
+                                Toast
+                                    .makeText(this@MainActivity, "onSaveClick", Toast.LENGTH_SHORT)
+                                    .show()
+                            },
+                        )
+                        PaymentCard(
+                            modifier =
+                                Modifier
+                                    .align(Alignment.CenterHorizontally)
+                                    .padding(vertical = 30.dp),
+                        )
+                        Column(
+                            modifier = Modifier.fillMaxWidth().padding(horizontal = 30.dp),
+                            verticalArrangement = Arrangement.spacedBy(20.dp),
+                        ) {
+                            OutlinedTextField("0000 - 0000 - 0000 - 0000", "카드 번호", Modifier.fillMaxWidth())
+                            OutlinedTextField("MM / YY", "만료일", Modifier.fillMaxWidth(0.5F))
+                            OutlinedTextField("카드에 표시된 이름을 입력하세요.", "카드 소유자 이름(선택)", Modifier.fillMaxWidth())
+                            OutlinedTextField("0000", "비밀번호", Modifier.fillMaxWidth(0.5F))
+                        }
+                    }
                 }
             }
         }
     }
 }
 
+@Suppress("ktlint:standard:function-naming")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun NewCardTopBar(
+    onBackClick: () -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    TopAppBar(
+        title = { Text("카드 추가") },
+        navigationIcon = {
+            IconButton(onClick = { onBackClick() }) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "뒤로 가기",
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = { onSaveClick() }) {
+                Icon(
+                    imageVector = Icons.Filled.Check,
+                    contentDescription = "완료",
+                )
+            }
+        },
+        modifier = modifier,
     )
 }
 
+@Suppress("ktlint:standard:function-naming")
+@Composable
+fun PaymentCard(modifier: Modifier = Modifier) {
+    Box(
+        contentAlignment = Alignment.CenterStart,
+        modifier =
+            modifier
+                .shadow(8.dp)
+                .size(width = 208.dp, height = 124.dp)
+                .background(
+                    color = Color(0xFF333333),
+                    shape = RoundedCornerShape(5.dp),
+                ),
+    ) {
+        Box(
+            modifier =
+                Modifier
+                    .padding(start = 14.dp, bottom = 10.dp)
+                    .size(width = 40.dp, height = 26.dp)
+                    .background(
+                        color = Color(0xFFCBBA64),
+                        shape = RoundedCornerShape(4.dp),
+                    ),
+        )
+    }
+}
+
+@Suppress("ktlint:standard:function-naming")
+@Composable
+fun OutlinedTextField(
+    placeholder: String,
+    label: String,
+    modifier: Modifier = Modifier,
+) {
+    val text: MutableState<String> = remember { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = text.value,
+        label = { Text(label) },
+        placeholder = { Text(placeholder) },
+        onValueChange = { newValue: String -> text.value = newValue },
+        modifier = modifier,
+    )
+}
+
+@Suppress("ktlint:standard:function-naming")
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AndroidpaymentsTheme {
-        Greeting("Android")
+        Scaffold { innerPadding: PaddingValues ->
+            Column(modifier = Modifier.fillMaxSize()) {
+                NewCardTopBar(
+                    onBackClick = {},
+                    onSaveClick = {},
+                )
+                PaymentCard(
+                    modifier =
+                        Modifier
+                            .align(Alignment.CenterHorizontally)
+                            .padding(vertical = 30.dp),
+                )
+                Column(
+                    modifier = Modifier.fillMaxWidth().padding(horizontal = 30.dp),
+                    verticalArrangement = Arrangement.spacedBy(20.dp),
+                ) {
+                    OutlinedTextField("0000 - 0000 - 0000 - 0000", "카드 번호", Modifier.fillMaxWidth())
+                    OutlinedTextField("MM / YY", "만료일")
+                    OutlinedTextField("카드에 표시된 이름을 입력하세요.", "카드 소유자 이름(선택)", Modifier.fillMaxWidth())
+                    OutlinedTextField("0000", "비밀번호")
+                }
+            }
+        }
     }
 }
