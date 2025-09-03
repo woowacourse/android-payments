000184375634c010ed7fc34cb45d2aa0e436fa86
chanho0908 <uni070@naver.com>
2025-09-03 10:26:10 +0900

    feat: TextFieldLabel 컴포넌트 추가

---
+++ app/src/androidTest/java/woowacourse/payments/TextFieldLabelTest.kt
@@ -0,0 +1,29 @@
+package woowacourse.payments
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.assertIsDisplayed
+import org.junit.Rule
+import org.junit.Test
+import woowacourse.payments.component.TextFieldLabel
+
+class TextFieldLabelTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun 라벨_텍스트가_정상적으로_표시된다() {
+        // given
+        val label = "카드 번호"
+
+        // when
+        composeTestRule.setContent {
+            TextFieldLabel(text = label)
+        }
+
+        // then
+        composeTestRule.onNodeWithText(label)
+            .assertIsDisplayed()
+    }
+}
