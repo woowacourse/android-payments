e62b0ee3e4dc9f92e13c335fdffd8df125b0be93
hwannow <vnfdlvgid061@naver.com>
2025-09-03 15:40:39 +0900

    feat: 카드 번호 입력 기능 추가

--- app/src/main/java/woowacourse/payments/MainActivity.kt
+++ app/src/main/java/woowacourse/payments/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Check
@@ -31,8 +32,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.OffsetMapping
+import androidx.compose.ui.text.input.TransformedText
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import woowacourse.payments.transformation.CardNumberVisualTransformation
 import woowacourse.payments.ui.theme.AndroidpaymentsTheme
 
 class MainActivity : ComponentActivity() {
@@ -152,16 +159,18 @@ fun CardNumber(
     label: String,
     placeholder: String,
 ) {
-    var text: String by remember { mutableStateOf("") }
+    var text by remember { mutableStateOf("") }
 
     OutlinedTextField(
         modifier = modifier,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
         value = text,
         label = { Text(text = label) },
-        onValueChange = {
-            text = it
+        onValueChange = { newValue ->
+            text = newValue
         },
-        placeholder = { Text(placeholder) }
+        placeholder = { Text(placeholder) },
+        visualTransformation = CardNumberVisualTransformation()
     )
 }
 
---
+++ app/src/main/java/woowacourse/payments/transformation/CardNumberVisualTransformation.kt
@@ -0,0 +1,29 @@
+package woowacourse.payments.transformation
+
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.input.OffsetMapping
+import androidx.compose.ui.text.input.TransformedText
+import androidx.compose.ui.text.input.VisualTransformation
+
+class CardNumberVisualTransformation : VisualTransformation {
+    override fun filter(text: AnnotatedString): TransformedText {
+        val formattedText =  text.filter { it.isDigit() }
+            .take(16)
+            .chunked(4)
+            .joinToString(" - ")
+
+        val offsetTranslator = object : OffsetMapping {
+            override fun originalToTransformed(offset: Int): Int {
+                return formattedText.length
+            }
+
+            override fun transformedToOriginal(offset: Int): Int {
+                return text.length
+            }
+        }
+        return TransformedText(
+            androidx.compose.ui.text.AnnotatedString(formattedText),
+            offsetTranslator
+        )
+    }
+}
