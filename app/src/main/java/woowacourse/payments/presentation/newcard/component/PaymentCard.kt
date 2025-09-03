50fc0567811b8187737d8b47db9d1ea3cff0a9c0
JunHyuk Im <imjh0417@gmail.com>
2025-09-03 10:36:44 +0900

    refactor: 카드 추가 화면 component 패키지 분리

--- app/src/main/java/woowacourse/payments/presentation/newcard/component/PaymentCard.kt
+++ app/src/main/java/woowacourse/payments/presentation/newcard/component/PaymentCard.kt
@@ -1,4 +1,4 @@
-package woowacourse.payments.presentation.newcard
+package woowacourse.payments.presentation.newcard.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
