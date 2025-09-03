d405b3efa6ddc3058728d4bd3991a4336e67b387
jiyunlee <02leejiyun@naver.com>
2025-09-03 23:58:51 +0900

    feat: 카드 비밀번호 도메인 클래스 구현

---
+++ app/src/main/java/woowacourse/payments/domain/CardPassword.kt
@@ -0,0 +1,14 @@
+package woowacourse.payments.domain
+
+class CardPassword(
+    val password: List<Int>,
+) {
+    init {
+        require(password.size == CARD_PASSWORD_LENGTH) { ERROR_INVALID_CARD_PASSWORD_LENGTH }
+    }
+
+    companion object {
+        private const val CARD_PASSWORD_LENGTH = 4
+        private const val ERROR_INVALID_CARD_PASSWORD_LENGTH = "카드 비밀번호는 4자리여야 합니다."
+    }
+}
