a6748032a9e3457d008fe533cd9061353c3181fa
지원 <cheerfulboys12@gmail.com>
2025-09-03 14:04:13 +0900

    feat: 카드 추가 화면의 카드 UI 구현

--- app/src/main/java/woowacourse/payments/MainActivity.kt
+++ app/src/main/java/woowacourse/payments/MainActivity.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Check
@@ -16,8 +21,12 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import woowacourse.payments.ui.theme.AndroidpaymentsTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,26 +36,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidpaymentsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                    NewCardTopBar(
-                        onBackClick = { },
-                        onSaveClick = {},
-                    )
+                    Column {
+                        NewCardTopBar(
+                            onBackClick = { },
+                            onSaveClick = {},
+                        )
+                        PaymentCard(modifier = Modifier.align(Alignment.CenterHorizontally)
+                            .padding(top = 10.dp))
+                    }
                 }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -78,10 +79,31 @@ fun NewCardTopBar(
     )
 }
 
+@Composable
+fun PaymentCard(modifier: Modifier = Modifier) {
+    Box(
+        contentAlignment = Alignment.CenterStart,
+        modifier = modifier
+            .size(208.dp, 124.dp)
+            .shadow(8.dp)
+            .background(
+                color = Color(0xFF333333),
+                shape = RoundedCornerShape(5.dp),
+            )
+    ) {
+        Box(modifier = modifier
+            .padding(start = 14.dp, bottom = 10.dp)
+            .size(40.dp, 28.dp)
+            .background(
+                color = Color(0xFFCBBA64),
+                shape = RoundedCornerShape(5.dp)
+            ))
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AndroidpaymentsTheme {
-        Greeting("Android")
     }
 }
