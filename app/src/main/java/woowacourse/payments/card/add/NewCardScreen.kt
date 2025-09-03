03764f74b55bb4adbf066cf1059c575774a156fc
jiyunlee <02leejiyun@naver.com>
2025-09-03 20:10:32 +0900

    feat: 카드 추가 화면에 카드 컴포넌트 추가

--- app/src/main/java/woowacourse/payments/card/add/NewCardScreen.kt
+++ app/src/main/java/woowacourse/payments/card/add/NewCardScreen.kt
@@ -1,25 +1,43 @@
 package woowacourse.payments.card.add
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import woowacourse.payments.card.add.components.NewCardTopBar
+import woowacourse.payments.card.add.components.PaymentCard
 
 @Composable
 fun NewCardScreen(
     onBackClick: () -> Unit = {},
     onSaveClick: () -> Unit = {},
 ) {
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding: PaddingValues ->
-        NewCardTopBar(
-            onBackClick = onBackClick,
-            onSaveClick = onSaveClick,
-            modifier = Modifier.padding(innerPadding),
-        )
+    Scaffold(
+        topBar = {
+            NewCardTopBar(
+                onBackClick = onBackClick,
+                onSaveClick = onSaveClick,
+            )
+        },
+    ) { innerPadding: PaddingValues ->
+        Column(
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .padding(top = 14.dp)
+                    .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            PaymentCard(modifier = Modifier.padding(innerPadding))
+        }
     }
 }
 
