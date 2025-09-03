49a0faeb1adde4c429526d160f6cb486daf03e35
moondev03 <moondev03@gmail.com>
2025-09-03 22:01:19 +0900

    feat: 카드 번호 입력창 구현

---
+++ app/src/main/java/woowacourse/payments/ui/payments/CardNumberTextField.kt
@@ -0,0 +1,58 @@
+package woowacourse.payments.ui.payments
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.text.isDigitsOnly
+import woowacourse.payments.R
+import woowacourse.payments.ui.common.CreditCardVisualTransformation
+
+private const val CARD_NUMBER_LENGTH = 16
+private const val CARD_NUMBER_TEXT_FIELD_TEST_TAG = "CardNumberTextField"
+
+@Composable
+fun CardNumberTextField(
+    modifier: Modifier = Modifier,
+    cardNumber: String,
+    onCardNumberChanged: (String) -> Unit,
+) {
+    OutlinedTextField(
+        modifier = modifier.testTag(CARD_NUMBER_TEXT_FIELD_TEST_TAG),
+        label = {
+            Text(text = stringResource(R.string.card_number_text_field_label))
+        },
+        placeholder = {
+            Text(
+                text = stringResource(R.string.card_number_text_field_placeholder),
+                color = Color.Gray
+            )
+        },
+        value = cardNumber,
+        onValueChange = { newValue ->
+            if (newValue.length > CARD_NUMBER_LENGTH)
+                return@OutlinedTextField onCardNumberChanged(newValue.take(CARD_NUMBER_LENGTH))
+            if (newValue.isDigitsOnly().not()) return@OutlinedTextField
+            onCardNumberChanged(newValue)
+        },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        visualTransformation = CreditCardVisualTransformation
+    )
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
+@Composable
+fun CardNumberTextFieldPreview() {
+    Box(modifier = Modifier.padding(CARD_NUMBER_LENGTH.dp)) {
+        CardNumberTextField(cardNumber = "") {}
+    }
+}
