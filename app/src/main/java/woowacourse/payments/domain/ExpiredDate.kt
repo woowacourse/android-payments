1e78d024768029af651fc495df223608c6a7abee
JunHyuk Im <imjh0417@gmail.com>
2025-09-03 15:13:36 +0900

    refactor: 카드 만료일 도메인 모델 유효성 검증 로직 수정

--- app/src/main/java/woowacourse/payments/domain/ExpiredDate.kt
+++ app/src/main/java/woowacourse/payments/domain/ExpiredDate.kt
@@ -1,15 +1,16 @@
 package woowacourse.payments.domain
 
-class ExpiredDate(
+class ExpiredDate private constructor(
     val month: Int,
     val year: Int
 ) {
-    init {
-        require(month in MIN_MONTH..MAX_MONTH) { "카드 유효기간은 1~12월 사이여야 합니다." }
-        require(year >= THIS_YEAR) { "카드 유효기간은 ${THIS_YEAR}년도 이상이어야 합니다." }
-    }
-
     companion object {
+        fun of(month: Int, year: Int): ExpiredDate? {
+            if (month in MIN_MONTH..MAX_MONTH && year >= THIS_YEAR)
+                return ExpiredDate(month, year)
+            return null
+        }
+
         private const val MIN_MONTH = 1
         private const val MAX_MONTH = 12
         private const val THIS_YEAR = 25
