db73f41e8848b33d8ca5cc018477128821198480
JunHyuk Im <imjh0417@gmail.com>
2025-09-03 14:23:34 +0900

    test: 카드 번호 테스트 코드 추가
    - 카드 번호는 문자가 포함되지 않는다.
    - 카드 번호는 16자리 이하여야 한다.

---
+++ app/src/test/java/woowacourse/payments/CardNumberTest.kt
@@ -0,0 +1,29 @@
+package woowacourse.payments
+
+import io.kotest.assertions.throwables.shouldThrow
+import org.junit.jupiter.api.Test
+import woowacourse.payments.domain.CardNumber
+
+class CardNumberTest {
+    @Test
+    fun 카드_번호는_문자가_포함되지_않는다() {
+        // given
+        val numbers = "abcd1234test5678"
+
+        // when & then
+        shouldThrow<IllegalArgumentException> {
+            CardNumber(numbers)
+        }
+    }
+
+    @Test
+    fun 카드_번호는_16자리_이하여야_한다() {
+        // given
+        val numbers = "11112222333344445555"
+
+        // when & then
+        shouldThrow<IllegalArgumentException> {
+            CardNumber(numbers)
+        }
+    }
+}
