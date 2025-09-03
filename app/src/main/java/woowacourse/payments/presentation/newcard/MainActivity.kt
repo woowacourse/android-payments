557ab27959b0c5c887542097191b5234b0e5e9c1
JunHyuk Im <imjh0417@gmail.com>
2025-09-03 16:49:05 +0900

    refactor: NewCardScreen으로 분리 및 MainActivity 이름 복구

---
+++ app/src/main/java/woowacourse/payments/presentation/newcard/MainActivity.kt
@@ -0,0 +1,19 @@
+package woowacourse.payments.presentation.newcard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import woowacourse.payments.ui.theme.AndroidpaymentsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndroidpaymentsTheme {
+                NewCardScreen()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/woowacourse/payments/presentation/newcard/NewCardScreen.kt
@@ -0,0 +1,55 @@
+package woowacourse.payments.presentation.newcard
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import woowacourse.payments.presentation.newcard.component.CardNumberTextField
+import woowacourse.payments.presentation.newcard.component.ExpiredDateTextField
+import woowacourse.payments.presentation.newcard.component.NewCardTopBar
+import woowacourse.payments.presentation.newcard.component.OwnerNameTextField
+import woowacourse.payments.presentation.newcard.component.PasswordTextField
+import woowacourse.payments.presentation.newcard.component.PaymentCard
+
+@Composable
+fun NewCardScreen(
+    onBackClick: () -> Unit = {},
+    onSaveClick: () -> Unit = {},
+) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            NewCardTopBar(
+                onBackClick = onBackClick,
+                onSaveClick = onSaveClick,
+            )
+        }) { innerPadding ->
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            PaymentCard(modifier = Modifier.padding(top = 14.dp))
+
+            Column(
+                verticalArrangement = Arrangement.spacedBy(30.dp),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(vertical = 24.dp, horizontal = 40.dp)
+            ) {
+                CardNumberTextField(modifier = Modifier.fillMaxWidth())
+                ExpiredDateTextField()
+                OwnerNameTextField(modifier = Modifier.fillMaxWidth())
+                PasswordTextField()
+            }
+        }
+    }
+
+}
