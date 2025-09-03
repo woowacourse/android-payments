70c41622d35faa2b8bf92ff241092bbc0c0117ea
devfeijoa <devfeijoa@gmail.com>
2025-09-03 20:50:16 +0900

    feat: CardNumber 구현

---
+++ app/src/main/java/woowacourse/payments/CardNumber.kt
@@ -0,0 +1,29 @@
+package woowacourse.payments
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CardNumber() {
+    OutlinedTextField(
+        value = "",
+        onValueChange = { },
+        label = { Text("카드 번호") },
+        placeholder = { Text("0000 - 0000 - 0000 - 0000") },
+        modifier = Modifier
+            .padding(top = 40.dp)
+            .padding(horizontal = 24.dp)
+    )
+}
+
+@Preview
+@Composable
+fun CardNumberPreview() {
+    CardNumber()
+}
