8bd080ebc8679ef26e2186e34c21c6db55a0f2a4
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-09-03 17:33:57 +0900

    test: ExpiredDateTextField 테스트

--- app/src/main/java/woowacourse/payments/ui/ExpiredDateTextField.kt
+++ app/src/main/java/woowacourse/payments/ui/ExpiredDateTextField.kt
@@ -3,18 +3,33 @@ package woowacourse.payments.ui
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun ExpiredDateTextField(modifier: Modifier = Modifier) {
+    var expiredDate by remember { mutableStateOf("") }
+
     OutlinedTextField(
-        value = "",
-        onValueChange = {},
+        value = expiredDate,
+        onValueChange = { newValue: String ->
+            val newDate = newValue.filter { it.isDigit() }
+            if (newDate.length >= 3) {
+                val month = newDate.substring(0, 2)
+                val year = newDate.substring(2, newDate.length.coerceAtMost(4))
+                expiredDate = "$month/$year"
+            } else {
+                expiredDate = newDate
+            }
+        },
         modifier = modifier,
         label = { Text(text = "만료일", color = Color.Gray) },
-        placeholder = { Text(text = "MM / YY", color = Color.Gray) },
+        placeholder = { Text(text = "MM/YY", color = Color.Gray) },
     )
 }
 
