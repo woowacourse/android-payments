9674d12331441dac42228a4390a151ac8359c92f
moondev03 <moondev03@gmail.com>
2025-09-03 22:45:40 +0900

    feat: 카드 비밀번호 입력창 구현

---
+++ app/src/main/java/woowacourse/payments/ui/payments/CardPasswordTextField.kt
@@ -0,0 +1,69 @@
+package woowacourse.payments.ui.payments
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.text.isDigitsOnly
+import woowacourse.payments.R
+import woowacourse.payments.ui.common.MaskVisualTransformation
+
+private const val CARD_PASSWORD_LENGTH = 4
+private const val CARD_PASSWORD_TEXT_FIELD_TEST_TAG = "CardPasswordTextField"
+
+@Composable
+fun CardPasswordTextField(
+    modifier: Modifier = Modifier,
+    cardPassword: String,
+    onCardPasswordChanged: (String) -> Unit,
+) {
+    val visualTransformation = remember { MaskVisualTransformation(CARD_PASSWORD_LENGTH) }
+
+    OutlinedTextField(
+        modifier = modifier.testTag(CARD_PASSWORD_TEXT_FIELD_TEST_TAG),
+        label = {
+            Text(text = stringResource(R.string.card_password_text_field_label))
+        },
+        placeholder = {
+            Text(
+                text = stringResource(R.string.card_password_text_field_placeholder),
+                color = Color.Gray
+            )
+        },
+        value = cardPassword,
+        onValueChange = { newValue ->
+            if (newValue.length > CARD_PASSWORD_LENGTH)
+                return@OutlinedTextField onCardPasswordChanged(newValue.take(CARD_PASSWORD_LENGTH))
+            if (newValue.isDigitsOnly().not()) return@OutlinedTextField
+            onCardPasswordChanged(newValue)
+        },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        visualTransformation = visualTransformation
+    )
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
+@Composable
+fun CardPasswordTextFieldPreview() {
+    var cardPassword by remember { mutableStateOf("1234") }
+
+    Box(modifier = Modifier.padding(12.dp)) {
+        CardPasswordTextField(
+            cardPassword = cardPassword,
+            onCardPasswordChanged = { newValue -> cardPassword = newValue }
+        )
+    }
+}
