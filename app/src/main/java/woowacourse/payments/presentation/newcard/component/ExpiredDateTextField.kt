8a454db121ff3441cfd654312af11159f04dd2bd
JunHyuk Im <imjh0417@gmail.com>
2025-09-03 15:42:25 +0900

    feat: 카드 만료일자가 유효하지 않을 경우 UI 추가

--- app/src/main/java/woowacourse/payments/presentation/newcard/component/ExpiredDateTextField.kt
+++ app/src/main/java/woowacourse/payments/presentation/newcard/component/ExpiredDateTextField.kt
@@ -1,6 +1,10 @@
 package woowacourse.payments.presentation.newcard.component
 
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,17 +15,20 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
+import woowacourse.payments.domain.ExpiredDate
 import woowacourse.payments.presentation.newcard.transformation.expiredDateVisualTransformation
 
 @Composable
 fun ExpiredDateTextField(modifier: Modifier = Modifier) {
     var expiredDate: String by remember { mutableStateOf("") }
+    var isError: Boolean by remember { mutableStateOf(false) }
 
     OutlinedTextField(
         value = expiredDate,
         onValueChange = { value: String ->
             if (isValidInput(value)) {
                 expiredDate = value
+                isError = value.length == 4 && !isValidExpiredDate(value)
             }
         },
         label = { Text("만료일") },
@@ -33,10 +40,39 @@ fun ExpiredDateTextField(modifier: Modifier = Modifier) {
         },
         visualTransformation = expiredDateVisualTransformation,
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
+        isError = isError,
+        supportingText = {
+            if (isError) {
+                Text(
+                    text = "유효하지 않은 만료일자 형식입니다.",
+                    color = MaterialTheme.colorScheme.error
+                )
+            }
+        },
+        trailingIcon = {
+            if (isError) {
+                Icon(
+                    imageVector = Icons.Filled.Info,
+                    contentDescription = "error",
+                    tint = MaterialTheme.colorScheme.error
+                )
+            }
+        },
         modifier = modifier
     )
 }
 
 private fun isValidInput(date: String): Boolean {
     return date.all { it.isDigit() } && date.length <= 4
+}
+
+private fun isValidExpiredDate(date: String): Boolean {
+    return try {
+        val month = date.substring(0, 2).toInt()
+        val year = date.substring(2, 4).toInt()
+        val result = ExpiredDate.of(month, year) != null
+        result
+    } catch (e: NumberFormatException) {
+        false
+    }
 }
