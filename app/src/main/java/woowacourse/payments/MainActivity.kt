3bf0abb21240bcc25630a69d4c9131dab1d576eb
DoableTuple <doabletuple@gmail.com>
2025-09-03 12:38:28 +0900

    feat: TextField 입력 유효성 검사 로직 추가

--- app/src/main/java/woowacourse/payments/MainActivity.kt
+++ app/src/main/java/woowacourse/payments/MainActivity.kt
@@ -33,8 +33,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.text.isDigitsOnly
 import woowacourse.payments.ui.theme.AndroidpaymentsTheme
 
 class MainActivity : ComponentActivity() {
@@ -64,13 +67,41 @@ class MainActivity : ComponentActivity() {
                                     .padding(vertical = 30.dp),
                         )
                         Column(
-                            modifier = Modifier.fillMaxWidth().padding(horizontal = 30.dp),
+                            modifier =
+                                Modifier
+                                    .fillMaxWidth()
+                                    .padding(horizontal = 30.dp),
                             verticalArrangement = Arrangement.spacedBy(20.dp),
                         ) {
-                            OutlinedTextField("0000 - 0000 - 0000 - 0000", "카드 번호", Modifier.fillMaxWidth())
-                            OutlinedTextField("MM / YY", "만료일", Modifier.fillMaxWidth(0.5F))
-                            OutlinedTextField("카드에 표시된 이름을 입력하세요.", "카드 소유자 이름(선택)", Modifier.fillMaxWidth())
-                            OutlinedTextField("0000", "비밀번호", Modifier.fillMaxWidth(0.5F))
+                            OutlinedTextField(
+                                placeholder = "0000 - 0000 - 0000 - 0000",
+                                label = "카드 번호",
+                                modifier = Modifier.fillMaxWidth(),
+                            ) { text: String ->
+                                text.isDigitsOnly()
+                            }
+                            OutlinedTextField(
+                                placeholder = "MM / YY",
+                                label = "만료일",
+                                modifier = Modifier.fillMaxWidth(0.5F),
+                            ) { text: String ->
+                                text.isDigitsOnly() && text.length <= 4
+                            }
+                            OutlinedTextField(
+                                placeholder = "카드에 표시된 이름을 입력하세요.",
+                                label = "카드 소유자 이름(선택)",
+                                modifier = Modifier.fillMaxWidth(),
+                            ) { text: String ->
+                                text.length <= 30
+                            }
+                            OutlinedTextField(
+                                placeholder = "0000",
+                                label = "비밀번호",
+                                applyMasking = true,
+                                modifier = Modifier.fillMaxWidth(0.5F),
+                            ) { text: String ->
+                                text.isDigitsOnly() && text.length <= 4
+                            }
                         }
                     }
                 }
@@ -142,6 +173,8 @@ fun OutlinedTextField(
     placeholder: String,
     label: String,
     modifier: Modifier = Modifier,
+    applyMasking: Boolean = false,
+    condition: (String) -> Boolean = { true },
 ) {
     val text: MutableState<String> = remember { mutableStateOf("") }
 
@@ -149,7 +182,8 @@ fun OutlinedTextField(
         value = text.value,
         label = { Text(label) },
         placeholder = { Text(placeholder) },
-        onValueChange = { newValue: String -> text.value = newValue },
+        onValueChange = { newValue: String -> if (condition(newValue)) text.value = newValue },
+        visualTransformation = if (applyMasking) PasswordVisualTransformation() else VisualTransformation.None,
         modifier = modifier,
     )
 }
@@ -172,12 +206,19 @@ fun GreetingPreview() {
                             .padding(vertical = 30.dp),
                 )
                 Column(
-                    modifier = Modifier.fillMaxWidth().padding(horizontal = 30.dp),
+                    modifier =
+                        Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 30.dp),
                     verticalArrangement = Arrangement.spacedBy(20.dp),
                 ) {
                     OutlinedTextField("0000 - 0000 - 0000 - 0000", "카드 번호", Modifier.fillMaxWidth())
                     OutlinedTextField("MM / YY", "만료일")
-                    OutlinedTextField("카드에 표시된 이름을 입력하세요.", "카드 소유자 이름(선택)", Modifier.fillMaxWidth())
+                    OutlinedTextField(
+                        "카드에 표시된 이름을 입력하세요.",
+                        "카드 소유자 이름(선택)",
+                        Modifier.fillMaxWidth(),
+                    )
                     OutlinedTextField("0000", "비밀번호")
                 }
             }
