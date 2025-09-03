a51b92648ca251fdb6569098f878b486c7a34acc
Dongjoo Seo <83579348+etama123@users.noreply.github.com>
2025-09-03 16:47:35 +0900

    feat: 카드 유효기간, 소유자 이름 입력 필드 구현
    - 카드 유효기간 입력 필드
        - 숫자만 입력 가능
        - 4자를 초과해 입력할 수 없음
        - 입력 필드 너비 조정
    - 카드 소유자 이름 입력 필드
        - 영문 대문자만 입력 가능
        - 30자를 초과해 입력할 수 없음
        - 하단에 현재 입력 글자 수 표시

--- app/src/main/java/woowacourse/payments/card/register/CardRegisterActivity.kt
+++ app/src/main/java/woowacourse/payments/card/register/CardRegisterActivity.kt
@@ -4,27 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import woowacourse.payments.ui.theme.AndroidpaymentsTheme
@@ -68,10 +77,19 @@ private fun NewCardScreen() {
 
             CardNumberTextField(
                 modifier = Modifier
+                    .fillMaxWidth()
                     .padding(top = 40.dp)
             )
             CardExpirationDateTextField(
-                modifier = Modifier.padding(top = 30.dp)
+                modifier = Modifier
+                    .padding(top = 30.dp)
+                    .fillMaxWidth(0.5f)
+
+            )
+            CardHolderNameTextField(
+                modifier = Modifier
+                    .padding(top = 30.dp)
+                    .fillMaxWidth()
             )
         }
     }
@@ -156,3 +174,57 @@ fun CardNumberTextField(modifier: Modifier = Modifier) {
         modifier = modifier,
     )
 }
+
+@Preview
+@Composable
+fun CardExpirationDateTextField(modifier: Modifier = Modifier) {
+    var expirationDate by remember { mutableStateOf("") }
+    val numericRegex = Regex("[^0-9]")
+
+    OutlinedTextField(
+        value = expirationDate,
+        onValueChange = {
+            val stripped = numericRegex.replace(it, "")
+            expirationDate = if (stripped.length <= 4) {
+                stripped
+            } else {
+                stripped.substring(0, 4)
+            }
+        },
+        label = { Text("만료일") },
+        placeholder = { Text("MM / YY") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        singleLine = true,
+        modifier = modifier,
+    )
+}
+
+@Preview
+@Composable
+fun CardHolderNameTextField(modifier: Modifier = Modifier) {
+    var cardHolderName by remember { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = cardHolderName,
+        onValueChange = {input ->
+            val stripped = input.filter { it.isLetter() }
+            val uppercased = stripped.uppercase()
+
+            if (uppercased.length <= 30) {
+                cardHolderName = uppercased
+            }
+        },
+        label = { Text("카드 소유자 이름(선택)") },
+        placeholder = { Text("카드에 표시된 이름을 입력하세요.") },
+        singleLine = true,
+        modifier = modifier,
+        supportingText = {
+            Text(
+                text = "${cardHolderName.length} / 30",
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.End,
+            )
+        },
+    )
+}
+
