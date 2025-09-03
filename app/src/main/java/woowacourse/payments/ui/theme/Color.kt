d9b6b082e6df5023d9d1301f0366cf52d7560883
rosemin928 <rosemin928@naver.com>
2025-09-03 15:28:28 +0900

    feat: 카드 추가 뷰 구현

--- app/src/main/java/woowacourse/payments/ui/theme/Color.kt
+++ app/src/main/java/woowacourse/payments/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Gray100 = Color(0xFFAAAAAA)
+
+val Gray200 = Color(0xFF49454F)
