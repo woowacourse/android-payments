f5dc99516e276eaeb7b797951e901f2ea276943f
moondev03 <moondev03@gmail.com>
2025-09-04 17:01:37 +0900

    chore: Ktlint Format 수행

--- app/src/androidTest/java/woowacourse/payments/ui/CardExpirationDateTextFieldTest.kt
+++ app/src/androidTest/java/woowacourse/payments/ui/CardExpirationDateTextFieldTest.kt
@@ -6,13 +6,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertTextEquals
-import androidx.compose.ui.test.isDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import woowacourse.payments.ui.payments.CardExpirationDateTextField
