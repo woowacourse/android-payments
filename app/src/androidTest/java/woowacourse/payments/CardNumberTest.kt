3781b36722adb56c021727b2040fa73a051717af
지원 <cheerfulboys12@gmail.com>
2025-09-04 10:25:26 +0900

    test: DigitFieldTextTest -> CardNumberTest

--- app/src/androidTest/java/woowacourse/payments/CardNumberTest.kt
+++ app/src/androidTest/java/woowacourse/payments/CardNumberTest.kt
@@ -4,16 +4,17 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performTextInput
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
-class DigitFieldTextTest {
+class CardNumberTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
 
-    @Test
-    fun 카드_번호는_16자이다() {
+    @Before
+    fun setUp() {
         composeTestRule.setContent {
             DigitFieldText(
                 label = "카드 번호",
@@ -22,6 +23,11 @@ class DigitFieldTextTest {
                 errorMessage = "카드 번호는 16자여야 합니다."
             )
         }
+    }
+
+    @Test
+    fun 카드_번호는_16자이다() {
+
 
         composeTestRule
             .onNodeWithText("") // TextField 초기 상태
@@ -34,15 +40,6 @@ class DigitFieldTextTest {
 
     @Test
     fun 카드_번호는_16자를_초과할_수_없다() {
-        composeTestRule.setContent {
-            DigitFieldText(
-                label = "카드 번호",
-                hint = "0000 - 0000 - 0000 - 0000",
-                mask = InputMask.CardNumber,
-                errorMessage = "카드 번호는 16자여야 합니다."
-            )
-        }
-
         composeTestRule
             .onNodeWithText("")
             .performTextInput("12341234123412341") // 17자리 입력
