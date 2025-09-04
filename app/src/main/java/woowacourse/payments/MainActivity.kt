828672bdd60f19115eb17b90e1e80410adbb33a8
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-09-04 12:12:41 +0900

    refactor: CardAdditionColumn 제거

--- app/src/main/java/woowacourse/payments/MainActivity.kt
+++ app/src/main/java/woowacourse/payments/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
 import woowacourse.payments.ui.CardAdditionScreen
 
 class MainActivity : ComponentActivity() {
@@ -11,8 +13,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CardAdditionScreen()
+            CardAdditionScreen(modifier = Modifier.fillMaxSize())
         }
     }
 }
-
