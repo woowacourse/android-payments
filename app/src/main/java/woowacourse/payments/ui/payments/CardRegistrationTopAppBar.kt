e6e0b70681bb574df0a7c03c9fa808f823b18853
moondev03 <moondev03@gmail.com>
2025-09-04 16:54:17 +0900

    feat: 카드 등록 화면 TopBar 구현

---
+++ app/src/main/java/woowacourse/payments/ui/payments/CardRegistrationTopAppBar.kt
@@ -0,0 +1,47 @@
+package woowacourse.payments.ui.payments
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import woowacourse.payments.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CardRegistrationTopAppBar(
+    onBackClick: () -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    isSaveButtonEnabled: Boolean = false,
+) {
+    TopAppBar(
+        title = { Text(stringResource(R.string.card_registration_bar_title)) },
+        navigationIcon = {
+            IconButton(onClick = onBackClick) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = stringResource(R.string.icon_previous_content_description),
+                )
+            }
+        },
+        actions = {
+            IconButton(
+                onClick = onSaveClick,
+                enabled = isSaveButtonEnabled,
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Check,
+                    contentDescription = stringResource(R.string.icon_save_content_description),
+                )
+            }
+        },
+        modifier = modifier,
+    )
+}
