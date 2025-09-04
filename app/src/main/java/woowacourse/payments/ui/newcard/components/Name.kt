4c49f31675459a31f16d79409672ddb81c25b351
hwannow <vnfdlvgid061@naver.com>
2025-09-04 11:27:24 +0900

    feat: 카드 소유자 이름 입력란에 글자 수 표시 기능 추가

--- app/src/main/java/woowacourse/payments/ui/newcard/components/Name.kt
+++ app/src/main/java/woowacourse/payments/ui/newcard/components/Name.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import woowacourse.payments.R
@@ -32,6 +33,15 @@ fun Name(
             if (newValue.length <= maxLength) onValueChange(newValue)
         },
         placeholder = { Text(placeholder) },
+        supportingText = {
+            Text(
+                text = stringResource(R.string.main_name_length, value.length, maxLength),
+                textAlign = TextAlign.End,
+                modifier =
+                    Modifier
+                        .fillMaxWidth(),
+            )
+        },
     )
 }
 
