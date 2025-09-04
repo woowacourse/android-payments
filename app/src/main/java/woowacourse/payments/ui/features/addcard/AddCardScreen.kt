c35bdf43ea1f3b87f5feab3831f88db4396210c0
medAndro <med4ndro@gmail.com>
2025-09-04 20:16:12 +0900

    feat: 카드 소유자 이름 필드의 글자 수 표시 형식 변경

--- app/src/main/java/woowacourse/payments/ui/features/addcard/AddCardScreen.kt
+++ app/src/main/java/woowacourse/payments/ui/features/addcard/AddCardScreen.kt
@@ -17,9 +17,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import woowacourse.payments.R
 import woowacourse.payments.domain.Card.Companion.MAX_LENGTH_OWNER_NAME
 import woowacourse.payments.ui.features.addcard.CardMapper.getExpireDateStatus
 import woowacourse.payments.ui.features.addcard.components.CardExpireDateField
@@ -83,7 +85,12 @@ fun AddCardScreen(
                 supportingText = {
                     Box(modifier = Modifier.height(SupportingTextHeight)) {
                         Text(
-                            text = "${cardUiState.ownerName.length}/${MAX_LENGTH_OWNER_NAME}",
+                            text =
+                                stringResource(
+                                    R.string.add_card_expire_date_field_counter_format,
+                                    cardUiState.ownerName.length,
+                                    MAX_LENGTH_OWNER_NAME,
+                                ),
                             modifier = Modifier.fillMaxWidth(),
                             textAlign = TextAlign.End,
                         )
