4209007efd3c0124d8428510026a8038a17fb291
moondev03 <moondev03@gmail.com>
2025-09-04 17:30:13 +0900

    test: 카드 등록 버튼 활성화 테스트 추가

--- app/src/main/java/woowacourse/payments/ui/payments/CardRegistrationTopAppBar.kt
+++ app/src/main/java/woowacourse/payments/ui/payments/CardRegistrationTopAppBar.kt
@@ -10,8 +10,14 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import woowacourse.payments.R
+
+private const val CARD_REGISTRATION_TOP_BAR_PREVIOUS_BUTTON_TEST_TAG =
+    "CardRegistrationTopBarPreviousButtonTestTag"
+private const val CARD_REGISTRATION_TOP_BAR_REGISTRATION_BUTTON_TEST_TAG =
+    "CardRegistrationTopBarRegistrationButtonTestTag"
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -24,7 +30,10 @@ fun CardRegistrationTopAppBar(
     TopAppBar(
         title = { Text(stringResource(R.string.card_registration_bar_title)) },
         navigationIcon = {
-            IconButton(onClick = onBackClick) {
+            IconButton(
+                modifier = Modifier.testTag(CARD_REGISTRATION_TOP_BAR_PREVIOUS_BUTTON_TEST_TAG),
+                onClick = onBackClick,
+            ) {
                 Icon(
                     imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                     contentDescription = stringResource(R.string.icon_previous_content_description),
@@ -33,6 +42,7 @@ fun CardRegistrationTopAppBar(
         },
         actions = {
             IconButton(
+                modifier = Modifier.testTag(CARD_REGISTRATION_TOP_BAR_REGISTRATION_BUTTON_TEST_TAG),
                 onClick = onSaveClick,
                 enabled = isSaveButtonEnabled,
             ) {
