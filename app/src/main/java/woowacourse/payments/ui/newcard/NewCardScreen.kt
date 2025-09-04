e8e3e20660356926ab507342102860b09465aa8f
jiyunlee <02leejiyun@naver.com>
2025-09-04 16:09:40 +0900

    feat: 카드 추가 화면에서 카드 소유자 이름 상태 관리

--- app/src/main/java/woowacourse/payments/ui/newcard/NewCardScreen.kt
+++ app/src/main/java/woowacourse/payments/ui/newcard/NewCardScreen.kt
@@ -31,6 +31,7 @@ fun NewCardScreen(
 ) {
     var cardNumber: String by remember { mutableStateOf("") }
     var cardExpirationDate: String by remember { mutableStateOf("") }
+    var cardHolderName: String by remember { mutableStateOf("") }
 
     Scaffold(
         topBar = {
@@ -66,7 +67,12 @@ fun NewCardScreen(
                 onValueChange = { cardExpirationDate = it },
                 modifier = Modifier.fillMaxWidth(0.5f),
             )
-            CardHolderNameTextField(modifier = Modifier.fillMaxWidth())
+            CardHolderNameTextField(
+                cardHolderName = cardHolderName,
+                maxLength = 30,
+                onValueChange = { cardHolderName = it },
+                modifier = Modifier.fillMaxWidth(),
+            )
             CardPasswordTextField(modifier = Modifier.fillMaxWidth(0.5f))
         }
     }
--- app/src/main/java/woowacourse/payments/ui/newcard/components/CardHolderNameTextField.kt
+++ app/src/main/java/woowacourse/payments/ui/newcard/components/CardHolderNameTextField.kt
@@ -4,31 +4,31 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import woowacourse.payments.R
-import woowacourse.payments.domain.CardHolderName
 
 @Composable
-fun CardHolderNameTextField(modifier: Modifier = Modifier) {
-    var cardHolderName: String by remember { mutableStateOf("") }
-
+fun CardHolderNameTextField(
+    cardHolderName: String,
+    maxLength: Int,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+) {
     OutlinedTextField(
         value = cardHolderName,
         onValueChange = { text: String ->
-            if (text.length <= CardHolderName.MAX_NAME_LENGTH) cardHolderName = text
+            if (text.length <= maxLength) {
+                onValueChange(text)
+            }
         },
         label = { Text(stringResource(R.string.card_holder_name)) },
         placeholder = { Text(stringResource(R.string.input_card_holder_name)) },
         supportingText = {
             Text(
-                "${cardHolderName.length}/${CardHolderName.MAX_NAME_LENGTH}",
+                "${cardHolderName.length}/$maxLength",
                 modifier = modifier.fillMaxWidth(),
                 textAlign = TextAlign.End,
             )
@@ -40,5 +40,10 @@ fun CardHolderNameTextField(modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun CardHolderNameTextFieldPreview() {
-    CardHolderNameTextField()
+    CardHolderNameTextField(
+        cardHolderName = "토바에",
+        maxLength = 30,
+        onValueChange = {},
+        modifier = Modifier,
+    )
 }
