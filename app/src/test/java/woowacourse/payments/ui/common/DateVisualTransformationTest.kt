0b08e24c0231782eb2c559f2d74426297597a64e
m6z1 <sonmyungj1zz@gmail.com>
2025-09-04 12:14:40 +0900

    test: DateVisualTransaformation 테스트 추가

---
+++ app/src/test/java/woowacourse/payments/ui/common/DateVisualTransformationTest.kt
@@ -0,0 +1,39 @@
+package woowacourse.payments.ui.common
+
+import androidx.compose.ui.text.AnnotatedString
+import org.junit.Before
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+
+class DateVisualTransformationTest {
+    private lateinit var dateVisualTransformation: DateVisualTransformation
+
+    @Before
+    fun setUp() {
+        dateVisualTransformation = DateVisualTransformation()
+    }
+
+    @Test
+    fun `4자리 입력 시 슬래시가 가운데 삽입된다`() {
+        // given:
+        val input = AnnotatedString("0201")
+
+        // when:
+        val result = dateVisualTransformation.filter(input)
+
+        // then:
+        assertEquals("02/01", result.text.text)
+    }
+
+    @Test
+    fun `4자리 초과 입력 시 잘린 후 슬래시 삽입`() {
+        // given:
+        val input = AnnotatedString("020123")
+
+        // when:
+        val result = dateVisualTransformation.filter(input)
+
+        // then:
+        assertEquals("02/01", result.text.text)
+    }
+}
