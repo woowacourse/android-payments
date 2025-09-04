b8c1761bbadc7b6dd535343d57df0ba937de58c1
DoableTuple <doabletuple@gmail.com>
2025-09-04 15:38:24 +0900

    refactor: 카드 추가 Activity 이름 변경

--- app/src/main/java/woowacourse/payments/ui/newcard/NewCardActivity.kt
+++ app/src/main/java/woowacourse/payments/ui/newcard/NewCardActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 
-class MainActivity : ComponentActivity() {
+class NewCardActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
