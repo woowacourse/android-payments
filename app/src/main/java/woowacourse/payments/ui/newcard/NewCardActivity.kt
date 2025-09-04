e23f6402f9644496bdbdcf9b6ae3cd36811c7c45
hwannow <vnfdlvgid061@naver.com>
2025-09-04 15:54:33 +0900

    refactor: 불필요한 import 제거

--- app/src/main/java/woowacourse/payments/ui/newcard/NewCardActivity.kt
+++ app/src/main/java/woowacourse/payments/ui/newcard/NewCardActivity.kt
@@ -6,9 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import woowacourse.payments.ui.newcard.components.NewCardTopBar
 import woowacourse.payments.ui.theme.AndroidpaymentsTheme
 
--- app/src/main/java/woowacourse/payments/ui/newcard/components/Name.kt
+++ app/src/main/java/woowacourse/payments/ui/newcard/components/Name.kt
@@ -5,10 +5,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
