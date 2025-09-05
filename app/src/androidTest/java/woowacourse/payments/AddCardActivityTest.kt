c144776ad31b9ad03d50231ec57d486586dc11ce
gahyunkim <mapledt001@naver.com>
2025-09-05 15:34:28 +0900

    test: AddCardActivityTest 테스트 코드 추가

--- app/src/androidTest/java/woowacourse/payments/AddCardActivityTest.kt
+++ app/src/androidTest/java/woowacourse/payments/AddCardActivityTest.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performTextInput
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -12,10 +13,13 @@ class AddCardActivityTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
+    @Before
+    fun setup() {
+        composeTestRule.setContent { AddCardScreen() }
+    }
+
     @Test
     fun 카드를_추가하기_위한_필드들이_보인다() {
-        composeTestRule.setContent { AddCardScreen() }
-
         composeTestRule
             .onNodeWithText("카드 번호")
             .assertIsDisplayed()
@@ -31,9 +35,7 @@ class AddCardActivityTest {
     }
 
     @Test
-    fun 카드번호는_숫자만_허용하고_4자리마다_하이픈으로_자동_포맷된다() {
-        composeTestRule.setContent { AddCardScreen() }
-
+    fun 카드번호는_4자리마다_하이픈으로_자동_포맷된다() {
         val cardNumberField = composeTestRule.onNodeWithText("카드 번호")
 
         cardNumberField.performTextInput("1234123412341234")
@@ -44,9 +46,18 @@ class AddCardActivityTest {
     }
 
     @Test
-    fun 만료일은_숫자만_허용하고_2자리마다_슬래시로_자동_포맷된다() {
-        composeTestRule.setContent { AddCardScreen() }
+    fun 카드번호는_숫자만_허용한다() {
+        val cardNumberField = composeTestRule.onNodeWithText("카드 번호")
 
+        cardNumberField.performTextInput("abcd123")
+
+        composeTestRule
+            .onNodeWithText("123")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun 만료일은_2자리마다_슬래시로_자동_포맷된다() {
         val expirationDateField = composeTestRule.onNodeWithText("만료일")
 
         expirationDateField.performTextInput("1226")
@@ -57,9 +68,34 @@ class AddCardActivityTest {
     }
 
     @Test
-    fun 사용자_이름을_입력하면_입력값과_글자수가_표시된다() {
-        composeTestRule.setContent { AddCardScreen() }
+    fun 만료일은_숫자만_허용한다() {
+        val expirationDateField = composeTestRule.onNodeWithText("만료일")
 
+        expirationDateField.performTextInput("aabb1")
+
+        composeTestRule
+            .onNodeWithText("1")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun 카드_소유자_이름은_정확히_30자까지_입력_되고_카운터가_30_30을_표시한다() {
+        val userNameField = composeTestRule.onNodeWithText("카드 소유자 이름(선택)")
+        val userName = "a".repeat(30)
+
+        userNameField.performTextInput(userName)
+
+        composeTestRule
+            .onNodeWithText(userName)
+            .assertIsDisplayed()
+
+        composeTestRule
+            .onNodeWithText("30 / 30")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun 카드_소유자_이름을_입력하면_입력값과_글자수가_표시된다() {
         val userNameField = composeTestRule.onNodeWithText("카드 소유자 이름(선택)")
 
         userNameField.performTextInput("kimgahyun")
@@ -75,8 +111,6 @@ class AddCardActivityTest {
 
     @Test
     fun 비밀번호는_마스킹되어_표시된다() {
-        composeTestRule.setContent { AddCardScreen() }
-
         val passwordField = composeTestRule.onNodeWithText("비밀번호")
         passwordField.performTextInput("1234")
 
