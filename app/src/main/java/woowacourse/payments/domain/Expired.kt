58063bcbff53068c8fedd7dbc2e4a93061123cb2
Ham BeomJoon <hbj0802@naver.com>
2025-09-05 15:18:38 +0900

    refactor: 만료일 유효성 검사 로직 수정

--- app/src/main/java/woowacourse/payments/domain/Expired.kt
+++ app/src/main/java/woowacourse/payments/domain/Expired.kt
@@ -5,11 +5,5 @@ value class Expired(
     val value: String,
 ) {
     val isValid: Boolean
-        get() {
-            if (value.length != 4) {
-                return false
-            }
-            val mm = value.substring(0, 2)
-            return mm.matches(Regex("(0[1-9]|1[0-2])"))
-        }
+        get() = value.length == 4 && value.take(2).toIntOrNull() in 1..12
 }
