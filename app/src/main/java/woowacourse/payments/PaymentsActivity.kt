8975085d18fec1799577b446ef3c178e2805ce3e
Ham BeomJoon <hbj0802@naver.com>
2025-09-05 16:28:28 +0900

    chore: Activity 이름 변경

--- app/src/main/java/woowacourse/payments/PaymentsActivity.kt
+++ app/src/main/java/woowacourse/payments/PaymentsActivity.kt
@@ -7,7 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import woowacourse.payments.ui.screen.AddCardScreen
 import woowacourse.payments.ui.theme.AndroidpaymentsTheme
 
-class MainActivity : ComponentActivity() {
+class PaymentsActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
