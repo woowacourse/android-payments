3254b491663800a1bdfcb7eb43a1af7048b26329
JunHyuk Im <imjh0417@gmail.com>
2025-09-05 14:58:00 +0900

    refactor: 현재 연도를 기준으로 유효 기간 검사하도록 수정

--- app/src/main/java/woowacourse/payments/domain/ExpiredDate.kt
+++ app/src/main/java/woowacourse/payments/domain/ExpiredDate.kt
@@ -1,18 +1,19 @@
 package woowacourse.payments.domain
 
+import java.time.LocalDate
+
 class ExpiredDate private constructor(
     val month: Int,
     val year: Int
 ) {
     companion object {
-        fun of(month: Int, year: Int): ExpiredDate? {
-            if (month in MIN_MONTH..MAX_MONTH && year >= THIS_YEAR)
+        fun of(month: Int, year: Int, now: LocalDate = LocalDate.now()): ExpiredDate? {
+            if (month in MIN_MONTH..MAX_MONTH && year >= now.year)
                 return ExpiredDate(month, year)
             return null
         }
 
         private const val MIN_MONTH = 1
         private const val MAX_MONTH = 12
-        private const val THIS_YEAR = 25
     }
 }
