dfa980c4e7981fdd7956d169df9aad463fa44cb0
cucumber99 <dlsguq1@gmail.com>
2025-09-05 15:12:43 +0900

    test: 비밀번호 테스트 개선

--- app/src/androidTest/java/woowacourse/payments/ui/PasswordTextFieldTest.kt
+++ app/src/androidTest/java/woowacourse/payments/ui/PasswordTextFieldTest.kt
@@ -4,9 +4,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.test.assert
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performTextInput
 import org.junit.Before
 import org.junit.Rule
@@ -20,23 +24,25 @@ class PasswordTextFieldTest {
     fun setUp() {
         composeRule.setContent {
             var value by remember { mutableStateOf("") }
-            PasswordTextField(value = value, onPasswordChange = { value = it })
+            PasswordTextField(
+                value = value, 
+                onPasswordChange = { value = it },
+                modifier = Modifier.testTag(TEST_TAG)
+            )
         }
     }
 
     @Test
     fun `입력한_비밀번호는_가려진다`() {
-        // given
-        val expected = "\u2022".repeat(4)
-
         // when
         composeRule
-            .onNodeWithText("")
+            .onNodeWithTag(TEST_TAG)
             .performTextInput("1234")
 
         // then
         composeRule
-            .onNodeWithText(expected)
+            .onNodeWithTag(TEST_TAG)
+            .assert(hasText("\u2022".repeat(4)))
             .assertIsDisplayed()
     }
 
@@ -44,12 +50,17 @@ class PasswordTextFieldTest {
     fun 비밀번호는_4글자이다() {
         // when
         composeRule
-            .onNodeWithText("")
+            .onNodeWithTag(TEST_TAG)
             .performTextInput("12345678")
 
         // then
         composeRule
-            .onNodeWithText("\u2022".repeat(4))
+            .onNodeWithTag(TEST_TAG)
+            .assert(hasText("\u2022".repeat(4)))
             .assertIsDisplayed()
+    }
+    
+    companion object {
+        private const val TEST_TAG = "TEST_TAG"
     }
 }
