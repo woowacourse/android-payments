ff62880f12b9f7aa8581c548309f1169e4eebabb
jiyunlee <02leejiyun@naver.com>
2025-09-05 15:18:52 +0900

    refactor: 카드 비밀번호 도메인 유효성 검사 수정

--- app/src/main/java/woowacourse/payments/domain/CardPassword.kt
+++ app/src/main/java/woowacourse/payments/domain/CardPassword.kt
@@ -1,14 +1,16 @@
 package woowacourse.payments.domain
 
 class CardPassword(
-    val password: List<Int>,
+    val password: String,
 ) {
     init {
-        require(password.size == CARD_PASSWORD_LENGTH) { ERROR_INVALID_CARD_PASSWORD_LENGTH }
+        require(password.length == CARD_PASSWORD_LENGTH) { ERROR_INVALID_CARD_PASSWORD_LENGTH }
+        require(password.all(Char::isDigit)) { ERROR_INVALID_CARD_PASSWORD_FORMAT }
     }
 
     companion object {
         const val CARD_PASSWORD_LENGTH = 4
         private const val ERROR_INVALID_CARD_PASSWORD_LENGTH = "카드 비밀번호는 4자리여야 합니다."
+        private const val ERROR_INVALID_CARD_PASSWORD_FORMAT = "카드 비밀번호는 숫자로만 이루어져야 합니다."
     }
 }
