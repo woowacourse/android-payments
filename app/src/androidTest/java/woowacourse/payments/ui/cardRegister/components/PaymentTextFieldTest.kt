30dabee55b783aed0f82c74fc44fe2415f542ec1
m6z1 <sonmyungj1zz@gmail.com>
2025-09-06 23:55:30 +0900

    test: 텍스트 필드 검증부 테스트 추가

--- app/src/androidTest/java/woowacourse/payments/ui/cardRegister/components/PaymentTextFieldTest.kt
+++ app/src/androidTest/java/woowacourse/payments/ui/cardRegister/components/PaymentTextFieldTest.kt
@@ -1,9 +1,14 @@
 package woowacourse.payments.ui.cardRegister.components
 
 import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.test.hasSetTextAction
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performTextInput
+import androidx.compose.ui.test.performTextReplacement
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import org.junit.Rule
 import org.junit.Test
@@ -17,27 +22,30 @@ class PaymentTextFieldTest {
     @Test
     fun `매개변수onlyDigits를_true_값으로_전달하면_숫자만_입력이_된다`() {
         // given:
-        val text = "1234뭉"
+        var inputText by mutableStateOf("")
 
         // when:
         composeTestRule.setContent {
             PaymentTextField(
                 label = "카드 번호",
                 placeholder = "0000-0000-0000-0000",
-                maxLength = 4,
-                text = text,
+                maxLength = 10,
+                text = inputText,
                 onlyDigits = true,
-                onValueChanged = { "" },
+                onValueChanged = { inputText = it },
             )
         }
 
+        composeTestRule.onNodeWithText("카드 번호").performTextInput("1234뭉abc5678")
+
         // then:
-        composeTestRule.onNodeWithText("카드 번호").performTextInput("1234")
+        assert(inputText == "12345678")
     }
 
     @Test
     fun 매개변수maxLength를_6자로_전달하면_6자까지만_입력이_된다() {
         // given:
+        var inputText by mutableStateOf("")
         val maxLength = 6
 
         // when:
@@ -46,13 +54,16 @@ class PaymentTextFieldTest {
                 label = "닉네임",
                 placeholder = "뭉치즈~",
                 maxLength = maxLength,
-                text = "123456789",
-                onValueChanged = { "" },
+                text = inputText,
+                onValueChanged = { inputText = it },
             )
         }
 
+        composeTestRule.onNodeWithText("닉네임").performTextInput("123456789")
+
         // then:
-        composeTestRule.onNodeWithText("닉네임").performTextInput("123456")
+        assert(inputText == "123456")
+        assert(inputText.length == maxLength)
     }
 
     @Test
@@ -81,57 +92,71 @@ class PaymentTextFieldTest {
     @Test
     fun 매개변수VisualTransformation을_PasswordVisualTransformation으로_설정하면_text가_숨겨져_보인다() {
         // given:
+        val password = "123456789"
+
         // when:
         composeTestRule.setContent {
             PaymentTextField(
-                label = "닉네임",
-                placeholder = "뭉치즈~",
+                label = "비밀번호",
+                placeholder = "비밀번호를 입력하세요",
                 maxLength = 10,
-                text = "123456789",
+                text = password,
                 onValueChanged = {},
                 visualTransformation = PasswordVisualTransformation(),
             )
         }
 
         // then:
-        composeTestRule.onNodeWithText("닉네임").performTextInput("•••••••••")
+        val maskedText = "•".repeat(password.length)
+        composeTestRule.onNodeWithText(maskedText).assertExists()
     }
 
     @Test
     fun 매개변수CreditCardVisualTransformation으로_설정하면_4자마다_하이픈이_추가되어_보인다() {
         // given:
+        var inputText by mutableStateOf("")
+
         // when:
         composeTestRule.setContent {
             PaymentTextField(
                 label = "카드번호",
-                placeholder = "뭉치즈~",
-                maxLength = 10,
-                text = "123456789",
-                onValueChanged = {},
+                placeholder = "0000-0000-0000-0000",
+                maxLength = 16,
+                text = inputText,
+                onValueChanged = { inputText = it },
                 visualTransformation = CreditCardVisualTransformation(),
             )
         }
+        composeTestRule
+            .onNode(hasSetTextAction())
+            .performTextReplacement("1234567890123456")
 
         // then:
-        composeTestRule.onNodeWithText("카드번호").performTextInput("1234-5678-9")
+        composeTestRule.onNodeWithText("1234 - 5678 - 9012 - 3456").assertExists()
     }
 
     @Test
     fun 매개변수DateVisualTransformation으로_설정하면_2자마다_슬래시가_추가되어_보인다() {
         // given:
+        var inputText by mutableStateOf("0130")
+
         // when:
         composeTestRule.setContent {
             PaymentTextField(
                 label = "만료일",
                 placeholder = "MM/YY",
                 maxLength = 4,
-                text = "0130",
-                onValueChanged = {},
+                text = inputText,
+                onValueChanged = { inputText = it },
                 visualTransformation = DateVisualTransformation(),
             )
         }
 
+        composeTestRule
+            .onNode(hasSetTextAction())
+            .performTextReplacement("0130")
+
         // then:
-        composeTestRule.onNodeWithText("만료일").performTextInput("01/30")
+        composeTestRule.onNodeWithText("01 / 30").assertExists()
     }
 }
