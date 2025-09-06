4feec773592823377043d043a7adf4e1eb113818
chanho0908 <uni070@naver.com>
2025-09-07 03:19:08 +0900

    feat: 직렬화 카드 모델 추가

---
+++ app/src/main/java/woowacourse/payments/serialization/SerializationCard.kt
@@ -0,0 +1,27 @@
+package woowacourse.payments.serialization
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import woowacourse.payments.domain.Card
+
+@Parcelize
+data class SerializationCard(
+    val number: String,
+    val expireDate: String,
+    val ownerName: String,
+    val password: String,
+) : Parcelable {
+    fun toDomain(): Card = Card(
+        number = number,
+        expireDate = expireDate,
+        ownerName = ownerName,
+        password = password,
+    )
+}
+
+fun Card.toSerializationCard(): SerializationCard = SerializationCard(
+    number = number,
+    expireDate = expireDate,
+    ownerName = ownerName,
+    password = password,
+)
