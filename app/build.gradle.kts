1f1da3a0959b9339df9bad0cef7915b8ce94ce57
지원 <cheerfulboys12@gmail.com>
2025-09-06 18:52:06 +0900

    chore: AssertJ 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation(libs.androidx.material3)
     testImplementation(libs.junit.jupiter)
     testImplementation(libs.kotest.runner.junit5)
+    testImplementation(libs.assertj.core)
     androidTestImplementation(libs.androidx.ui.test.junit4)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
