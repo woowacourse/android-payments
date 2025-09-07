6e62fd918a480ecf2a24c5f28647b05c4e17749b
JunHyuk Im <imjh0417@gmail.com>
2025-09-07 15:39:43 +0900

    feat: 카드 목록 화면 TopBar 추가

---
+++ app/src/main/java/woowacourse/payments/cards/component/CardsTopBar.kt
@@ -0,0 +1,37 @@
+package woowacourse.payments.cards.component
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun CardsTopBar(
+    modifier: Modifier = Modifier,
+    onAddClick: () -> Unit = {},
+) {
+    TopAppBar(
+        title = {
+            Text(
+                "Payments",
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth(),
+            )
+        },
+        actions = {
+            TextButton(onClick = { onAddClick() }) {
+                Text("추가", fontSize = 18.sp, fontWeight = FontWeight.Bold)
+            }
+        },
+        modifier = modifier
+    )
+}
