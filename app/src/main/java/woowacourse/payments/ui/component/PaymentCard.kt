570d868c927630e3abecf1febc84b4d314418f5e
Ham BeomJoon <hbj0802@naver.com>
2025-09-07 22:47:30 +0900

    feat: PaymentCard에 입력값 (카드번호, 만료일, 소유자) 표시

--- app/src/main/java/woowacourse/payments/ui/component/PaymentCard.kt
+++ app/src/main/java/woowacourse/payments/ui/component/PaymentCard.kt
@@ -1,21 +1,38 @@
 package woowacourse.payments.ui.component
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import woowacourse.payments.domain.Card
+import woowacourse.payments.domain.CardNumber
+import woowacourse.payments.domain.CardOwner
+import woowacourse.payments.domain.Expired
+import woowacourse.payments.domain.Password
+import woowacourse.payments.ui.CardUiModel
 import woowacourse.payments.ui.theme.AndroidpaymentsTheme
+import woowacourse.payments.ui.toPresentation
 
 @Composable
-fun PaymentCard(modifier: Modifier = Modifier) {
+fun PaymentCard(
+    card: CardUiModel,
+    modifier: Modifier = Modifier,
+) {
     Box(
         contentAlignment = Alignment.CenterStart,
         modifier =
@@ -30,13 +47,47 @@ fun PaymentCard(modifier: Modifier = Modifier) {
         Box(
             modifier =
                 Modifier
-                    .padding(start = 14.dp, bottom = 10.dp)
+                    .padding(start = 14.dp, bottom = 12.dp)
                     .size(width = 40.dp, height = 26.dp)
                     .background(
                         color = Color(0xFFCBBA64),
                         shape = RoundedCornerShape(4.dp),
                     ),
         )
+
+        Column(
+            modifier =
+                Modifier
+                    .align(Alignment.BottomStart)
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp, vertical = 8.dp),
+        ) {
+            Text(
+                text = card.number,
+                color = Color.White,
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Medium,
+            )
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                Text(
+                    text = card.owner,
+                    color = Color.White,
+                    fontSize = 10.sp,
+                    fontWeight = FontWeight.Medium,
+                )
+
+                Text(
+                    text = card.expired,
+                    color = Color.White,
+                    fontSize = 10.sp,
+                    fontWeight = FontWeight.Medium,
+                )
+            }
+        }
     }
 }
 
@@ -44,6 +95,14 @@ fun PaymentCard(modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 fun PaymentPreview() {
     AndroidpaymentsTheme {
-        PaymentCard()
+        PaymentCard(
+            card =
+                Card(
+                    number = CardNumber("1234567887654321"),
+                    expired = Expired("0826"),
+                    owner = CardOwner("으어 글씨가 너무 크다."),
+                    password = Password("1234"),
+                ).toPresentation(),
+        )
     }
 }
