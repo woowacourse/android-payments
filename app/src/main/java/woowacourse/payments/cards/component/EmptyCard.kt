76dba94a5265b6abeb458159d14f731720ab84ae
JunHyuk Im <imjh0417@gmail.com>
2025-09-07 15:14:37 +0900

    feat: 빈 카드 컴포넌트 추가

---
+++ app/src/main/java/woowacourse/payments/cards/component/EmptyCard.kt
@@ -0,0 +1,39 @@
+package woowacourse.payments.cards.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import woowacourse.payments.R
+
+@Preview
+@Composable
+fun EmptyCard(modifier: Modifier = Modifier) {
+    Box(
+        contentAlignment = Alignment.CenterStart,
+        modifier =
+            modifier
+                .size(width = 208.dp, height = 124.dp)
+                .background(
+                    color = Color(0xFFE5E5E5),
+                    shape = RoundedCornerShape(5.dp),
+                ),
+    ) {
+        Icon(
+            imageVector = Icons.Default.Add,
+            contentDescription = stringResource(R.string.ic_plus),
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
