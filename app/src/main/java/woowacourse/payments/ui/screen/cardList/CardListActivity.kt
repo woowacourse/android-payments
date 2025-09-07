3e3ab8ec84b0e42aa99f5c0104085443e88a686a
Ham BeomJoon <hbj0802@naver.com>
2025-09-07 22:56:43 +0900

    feat: 새로운 카드 추가시 Toast 메시지 출력

--- app/src/main/java/woowacourse/payments/ui/screen/cardList/CardListActivity.kt
+++ app/src/main/java/woowacourse/payments/ui/screen/cardList/CardListActivity.kt
@@ -3,6 +3,7 @@ package woowacourse.payments.ui.screen.cardList
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
@@ -11,6 +12,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
 import woowacourse.payments.ui.CardUiModel
 import woowacourse.payments.ui.getParcelableExtraCompat
 import woowacourse.payments.ui.screen.addCard.AddCardActivity
@@ -24,6 +26,7 @@ class CardListActivity : ComponentActivity() {
         setContent {
             AndroidpaymentsTheme {
                 val cardList = remember { mutableStateListOf<CardUiModel>() }
+                val context = LocalContext.current
 
                 val addCardLauncher =
                     rememberLauncherForActivityResult(
@@ -32,7 +35,10 @@ class CardListActivity : ComponentActivity() {
                         if (result.resultCode == RESULT_OK) {
                             val newCard =
                                 result.data?.getParcelableExtraCompat<CardUiModel>("new_card")
-                            newCard?.let { cardList.add(it) }
+                            newCard?.let {
+                                cardList.add(it)
+                                Toast.makeText(context, "새 카드가 추가되었습니다", Toast.LENGTH_SHORT).show()
+                            }
                         }
                     }
 
