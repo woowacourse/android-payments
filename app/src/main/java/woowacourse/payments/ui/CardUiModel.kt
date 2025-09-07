f1c28fe292581656ac6e135f49387bfbfabd6e30
Ham BeomJoon <hbj0802@naver.com>
2025-09-07 22:31:16 +0900

    feat: CardUiModel Parcelable 추가

---
+++ app/src/main/java/woowacourse/payments/ui/CardUiModel.kt
@@ -0,0 +1,19 @@
+package woowacourse.payments.ui
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import woowacourse.payments.domain.Card
+
+@Parcelize
+data class CardUiModel(
+    val number: String,
+    val expired: String,
+    val owner: String,
+) : Parcelable
+
+fun Card.toPresentation(): CardUiModel =
+    CardUiModel(
+        number = formatCardNumber(this.number),
+        expired = formatExpired(this.expired),
+        owner = this.owner?.value ?: "",
+    )
