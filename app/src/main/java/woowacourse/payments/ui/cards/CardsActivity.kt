78b0b2165718ced170283e0994b177120ff6b73d
hwannow <vnfdlvgid061@naver.com>
2025-09-07 17:47:25 +0900

    feat: 카드 목록 화면 추가

--- app/src/main/java/woowacourse/payments/ui/cards/CardsActivity.kt
+++ app/src/main/java/woowacourse/payments/ui/cards/CardsActivity.kt
@@ -4,21 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
+import androidx.compose.material3.ExperimentalMaterial3Api
 import woowacourse.payments.ui.theme.AndroidpaymentsTheme
 
 class CardsActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             AndroidpaymentsTheme {
-                Scaffold(
-                    modifier = Modifier.fillMaxSize(),
-                ) { innerPadding ->
-                }
+                CardsScreen()
             }
         }
     }
--- app/src/main/java/woowacourse/payments/ui/cards/CardsScreen.kt
+++ app/src/main/java/woowacourse/payments/ui/cards/CardsScreen.kt
@@ -5,45 +5,76 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import woowacourse.payments.ui.cards.components.AddCardImage
 import woowacourse.payments.ui.cards.components.CardItem
 import woowacourse.payments.ui.model.PaymentCardUiModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CardsScreen(innerPadding: PaddingValues) {
+fun CardsScreen() {
     val cardList =
         rememberSaveable { mutableStateListOf<PaymentCardUiModel>() }
 
-    Column(
-        modifier =
-            Modifier
-                .padding(innerPadding)
-                .padding(top = 12.dp)
-                .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(36.dp),
-    ) {
-        cardList.forEach { card ->
-            CardItem(
-                paymentCard = card,
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = "Payments",
+                    )
+                },
+                actions = {
+                    if (cardList.size > 1) {
+                        Text(
+                            modifier =
+                                Modifier
+                                    .padding(end = 20.dp),
+                            text = "추가",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 18.sp,
+                        )
+                    }
+                },
             )
-        }
-
-        if (cardList.size <= 1) {
-            if (cardList.isEmpty()) {
-                Text(
-                    text = "새로운 카드를 등록해주세요",
+        },
+    ) { innerPadding ->
+        Column(
+            modifier =
+                Modifier
+                    .padding(innerPadding)
+                    .padding(top = 12.dp)
+                    .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(36.dp),
+        ) {
+            cardList.forEach { card ->
+                CardItem(
+                    paymentCard = card,
                 )
             }
-            AddCardImage()
+
+            if (cardList.size <= 1) {
+                if (cardList.isEmpty()) {
+                    Text(
+                        text = "새로운 카드를 등록해주세요",
+                    )
+                }
+                AddCardImage()
+            }
         }
     }
 }
@@ -51,5 +82,5 @@ fun CardsScreen(innerPadding: PaddingValues) {
 @Preview(showBackground = true)
 @Composable
 private fun CardScreenPreview() {
-    CardsScreen(innerPadding = PaddingValues())
+    CardsScreen()
 }
