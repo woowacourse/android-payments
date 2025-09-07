cb615c08453e0ed092e8394866d6cf06536f7194
JunHyuk Im <imjh0417@gmail.com>
2025-09-07 22:05:11 +0900

    feat: 카드 정보(번호, 이름, 만료일) 표시 추가

--- app/src/main/java/woowacourse/payments/newcard/component/PaymentCard.kt
+++ app/src/main/java/woowacourse/payments/newcard/component/PaymentCard.kt
@@ -1,21 +1,37 @@
 package woowacourse.payments.newcard.component
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import woowacourse.payments.domain.Card
+import woowacourse.payments.domain.CardNumber
+import woowacourse.payments.domain.ExpiredDate
+import woowacourse.payments.domain.OwnerName
 
-@Preview
 @Composable
-fun PaymentCard(modifier: Modifier = Modifier) {
+fun PaymentCard(
+    modifier: Modifier = Modifier,
+    card: Card? = null,
+) {
     Box(
         contentAlignment = Alignment.CenterStart,
         modifier =
@@ -37,5 +53,74 @@ fun PaymentCard(modifier: Modifier = Modifier) {
                         shape = RoundedCornerShape(4.dp),
                     ),
         )
+
+        Column(
+            verticalArrangement = Arrangement.Bottom,
+            modifier =
+                Modifier
+                    .fillMaxHeight()
+                    .padding(bottom = 16.dp),
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                if (card != null) {
+                    Text(
+                        text = card.cardNumber.toMaskCardNumber(),
+                        style = cardTextStyle,
+                        letterSpacing = 4.sp,
+                        modifier = modifier.fillMaxWidth(),
+                    )
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier =
+                            Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 12.dp),
+                    ) {
+                        Text(text = card.ownerName.name, style = cardTextStyle)
+                        Text(
+                            text = card.expiredDate.toFormattedString(),
+                            style = cardTextStyle,
+                        )
+                    }
+                }
+            }
+        }
     }
 }
+
+@Preview
+@Composable
+private fun PaymentCardPreview() {
+    PaymentCard(
+        card =
+            Card(
+                cardNumber = CardNumber("1234567812345678"),
+                expiredDate = ExpiredDate.of(1, 26)!!,
+                ownerName = OwnerName("크림"),
+                password = "1234",
+            ),
+    )
+}
+
+private fun CardNumber.toMaskCardNumber(): String {
+    val maskCardNumber = this.numbers.take(8) + "*".repeat(8)
+    return maskCardNumber.chunked(4).joinToString("-")
+}
+
+private fun ExpiredDate.toFormattedString(): String {
+    val mm: String = if (month < 10) "0$month" else "$month"
+    val yy: String = if (year < 10) "0$year" else "$year"
+    return "$mm / $yy"
+}
+
+private val cardTextStyle =
+    TextStyle(
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color.White,
+        letterSpacing = 1.sp,
+        textAlign = TextAlign.Center,
+    )
