138fb573a4a9ceebd34dbded0d6f48ccee2de070
Ham BeomJoon <hbj0802@naver.com>
2025-09-07 15:03:41 +0900

    chore: ktlint 적용

--- app/src/main/java/woowacourse/payments/ui/component/CardListTopBar.kt
+++ app/src/main/java/woowacourse/payments/ui/component/CardListTopBar.kt
@@ -3,7 +3,6 @@ package woowacourse.payments.ui.component
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
