7f52ef477fd9460a3dbed8a3cb7d5e1c838c1611
jiyunlee <02leejiyun@naver.com>
2025-09-08 21:10:39 +0900

    refactor: 카드 번호 입력 시 숫자만 필터링하도록 수정

--- app/src/main/java/woowacourse/payments/domain/CardNumber.kt
+++ app/src/main/java/woowacourse/payments/domain/CardNumber.kt
@@ -11,11 +11,10 @@ value class CardNumber(
     companion object {
         const val CARD_NUMBER_LENGTH = 16
         private const val ERROR_INVALID_CARD_NUMBER_LENGTH = "카드 번호는 16자리여야 합니다."
-        private const val ERROR_INVALID_CARD_NUMBER_FORMAT = "카드 번호는 숫자로만 이루어져야 합니다."
 
         fun from(numbers: String): CardNumber {
-            require(numbers.all(Char::isDigit)) { ERROR_INVALID_CARD_NUMBER_FORMAT }
-            return CardNumber(numbers.map { CardDigit(it.digitToInt()) })
+            val digits = numbers.filter(Char::isDigit).map { CardDigit(it.digitToInt()) }
+            return CardNumber(digits)
         }
     }
 }
