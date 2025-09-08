f62653604ba4684cd9fb9251c9e58f2a0a839d8e
jiyunlee <02leejiyun@naver.com>
2025-09-08 16:48:56 +0900

    feat: 카드 목록이 없을 때 안내 메시지 표시

--- app/src/main/java/woowacourse/payments/ui/cardlist/CardListScreen.kt
+++ app/src/main/java/woowacourse/payments/ui/cardlist/CardListScreen.kt
@@ -3,16 +3,27 @@ package woowacourse.payments.ui.cardlist
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import woowacourse.payments.domain.Card
+import woowacourse.payments.domain.CardDigit
+import woowacourse.payments.domain.CardExpirationDate
+import woowacourse.payments.domain.CardHolderName
+import woowacourse.payments.domain.CardNumber
+import woowacourse.payments.domain.CardPassword
 import woowacourse.payments.ui.components.PaymentCard
+import java.time.YearMonth
 
 @Composable
 fun CardListScreen(
@@ -30,13 +41,49 @@ fun CardListScreen(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(36.dp),
         ) {
-            PaymentCard()
+            if (cards.isEmpty()) {
+                Spacer(modifier = Modifier.height(20.dp))
+                Text(
+                    text = "새로운 카드를 등록해주세요",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+            cards.forEach { card: Card ->
+                PaymentCard(card = card)
+            }
         }
     }
 }
 
-@Preview
+@Preview(name = "카드 0개")
 @Composable
-private fun CardListScreenPreview() {
+private fun CardListScreenPreview1() {
+    CardListScreen()
+}
+
+@Preview(name = "카드 1개")
+@Composable
+private fun CardListScreenPreview2() {
+    CardListScreen(
+        cards =
+            listOf(
+                Card(
+                    number =
+                        CardNumber(
+                            listOf(1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4)
+                                .map(::CardDigit),
+                        ),
+                    expirationDate = CardExpirationDate(YearMonth.of(2025, 9)),
+                    password = CardPassword("0000"),
+                    holderName = CardHolderName("CREW"),
+                ),
+            ),
+    )
+}
+
+@Preview(name = "카드 n개")
+@Composable
+private fun CardListScreenPreview3() {
     CardListScreen()
 }
