df298902b002b81467a52e4c6b28bf8539f271f5
jerry8282 <ka06196@naver.com>
2025-09-08 21:46:59 +0900

    feat: 카드 추가 버튼 UI 구현

---
+++ app/src/main/java/woowacourse/payments/ui/catalog/component/AddCardButtonField.kt
@@ -0,0 +1,51 @@
+package woowacourse.payments.ui.catalog.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import woowacourse.payments.ui.theme.AndroidpaymentsTheme
+
+@Composable
+fun AddCardButton(onClick: () -> Unit,modifier: Modifier = Modifier) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier =
+            modifier
+                .shadow(8.dp)
+                .size(width = 208.dp, height = 124.dp)
+                .background(
+                    color = Color.LightGray,
+                    shape = RoundedCornerShape(5.dp),
+                ).clickable(onClick = onClick)
+    ) {
+        Text(
+            text = "+",
+            fontSize = 17.sp,
+            color = Color.DarkGray,
+            modifier = Modifier.padding(12.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AddCardButtonPreview() {
+    Column(modifier = Modifier.padding(12.dp)) {
+        AndroidpaymentsTheme {
+            AddCardButton(onClick = {})
+        }
+    }
+}
