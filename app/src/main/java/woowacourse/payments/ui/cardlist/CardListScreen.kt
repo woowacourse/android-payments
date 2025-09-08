2334c4f123020f0d8568fc006080c0b3ee6aee70
jiyunlee <02leejiyun@naver.com>
2025-09-08 15:32:16 +0900

    feat: 카드 목록 화면 UI 구현

---
+++ app/src/main/java/woowacourse/payments/ui/cardlist/CardListScreen.kt
@@ -0,0 +1,42 @@
+package woowacourse.payments.ui.cardlist
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import woowacourse.payments.domain.Card
+import woowacourse.payments.ui.newcard.components.PaymentCard
+
+@Composable
+fun CardListScreen(
+    cards: List<Card> = emptyList(),
+    onAddClick: () -> Unit = {},
+) {
+    Scaffold(
+        topBar = { CardListTopBar(onAddClick = onAddClick) },
+    ) { innerPadding: PaddingValues ->
+        Column(
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .padding(top = innerPadding.calculateTopPadding() + 12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(36.dp),
+        ) {
+            PaymentCard()
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun CardListScreenPreview() {
+    CardListScreen()
+}
