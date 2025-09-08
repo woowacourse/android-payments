1cacbf9446fd7cd49450ab3bd10d8802090cf805
DoableTuple <doabletuple@gmail.com>
2025-09-09 00:17:12 +0900

    feat: 카드 Composable에 카드 번호, 소유자 이름 및 만료일 추가

--- app/src/main/java/woowacourse/payments/ui/newcard/PaymentCard.kt
+++ app/src/main/java/woowacourse/payments/ui/newcard/PaymentCard.kt
@@ -1,39 +1,98 @@
 package woowacourse.payments.ui.newcard
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import woowacourse.payments.ui.theme.AndroidpaymentsTheme
 
 @Suppress("ktlint:standard:function-naming")
 @Composable
-fun PaymentCard(modifier: Modifier = Modifier) {
+fun PaymentCard(
+    modifier: Modifier = Modifier,
+    cardNumber: String = "",
+    cardholderName: String = "",
+    expirationDate: String = "",
+) {
     Box(
-        contentAlignment = Alignment.CenterStart,
-        modifier =
-            modifier
-                .shadow(8.dp)
-                .size(width = 208.dp, height = 124.dp)
-                .background(
-                    color = Color(0xFF333333),
-                    shape = RoundedCornerShape(5.dp),
-                ),
+        modifier
+            .shadow(8.dp)
+            .size(width = 208.dp, height = 124.dp)
+            .background(
+                color = Color(0xFF333333),
+                shape = RoundedCornerShape(5.dp),
+            ),
     ) {
-        Box(
+        Column(
             Modifier
-                .padding(start = 14.dp, bottom = 10.dp)
-                .size(width = 40.dp, height = 26.dp)
-                .background(
-                    color = Color(0xFFCBBA64),
-                    shape = RoundedCornerShape(4.dp),
-                ),
-        )
+                .fillMaxWidth()
+                .padding(start = 16.dp, end = 16.dp, top = 44.dp),
+        ) {
+            Box(
+                Modifier
+                    .size(width = 40.dp, height = 26.dp)
+                    .background(
+                        color = Color(0xFFCBBA64),
+                        shape = RoundedCornerShape(4.dp),
+                    ),
+            )
+            Text(
+                text = cardNumber,
+                color = Color.White,
+                fontSize = 12.sp,
+            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                Text(
+                    text = cardholderName,
+                    color = Color.White,
+                    fontSize = 12.sp,
+                )
+                Text(
+                    text = expirationDate,
+                    color = Color.White,
+                    fontSize = 12.sp,
+                    textAlign = TextAlign.End,
+                )
+            }
+        }
+    }
+}
+
+@Suppress("ktlint:standard:function-naming")
+@Preview(showBackground = true)
+@Composable
+fun BlankPaymentCardPreview() {
+    AndroidpaymentsTheme {
+        PaymentCard()
+    }
+}
+
+@Suppress("ktlint:standard:function-naming")
+@Preview(showBackground = true)
+@Composable
+fun PaymentCardWithDetailPreview() {
+    AndroidpaymentsTheme {
+        PaymentCard(
+            cardNumber = "1111 - 2222 - **** - ****",
+            cardholderName = "CREW",
+            expirationDate = "04 / 21"
+            )
     }
 }
