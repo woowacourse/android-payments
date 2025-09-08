bde79a10e57785a5216dcc1c01199fea07a8b1bb
cucumber99 <dlsguq1@gmail.com>
2025-09-08 16:18:41 +0900

    feat: 카드 목록 화면 앱 바 UI 구현

---
+++ app/src/main/java/woowacourse/payments/ui/screen/cards/component/CardsTopBar.kt
@@ -0,0 +1,45 @@
+package woowacourse.payments.ui.screen.cards.component
+
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import woowacourse.payments.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CardsTopBar(
+    modifier: Modifier = Modifier,
+    isAddButtonVisible: Boolean = false,
+    onAddClick: () -> Unit = {},
+) {
+    CenterAlignedTopAppBar(
+        title = { Text(text = stringResource(R.string.cards_top_bar_title)) },
+        modifier = modifier,
+        actions = {
+            if (isAddButtonVisible) {
+                TextButton(onClick = onAddClick) {
+                    Text(
+                        text = stringResource(R.string.cards_top_bar_add_button),
+                        color = Color.Black,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                    )
+                }
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun CardsTopBarPreview() {
+    CardsTopBar(isAddButtonVisible = true)
+}
