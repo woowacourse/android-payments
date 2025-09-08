c1d5dc53f3c7cb6a72a790fb28108cab3adb08db
jiyunlee <02leejiyun@naver.com>
2025-09-08 14:03:14 +0900

    refactor: MainActivity를 NewCardActivity로 변경

--- app/src/main/java/woowacourse/payments/ui/newcard/NewCardActivity.kt
+++ app/src/main/java/woowacourse/payments/ui/newcard/NewCardActivity.kt
@@ -1,13 +1,12 @@
-package woowacourse.payments
+package woowacourse.payments.ui.newcard
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import woowacourse.payments.ui.newcard.NewCardScreen
 import woowacourse.payments.ui.theme.AndroidpaymentsTheme
 
-class MainActivity : ComponentActivity() {
+class NewCardActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
