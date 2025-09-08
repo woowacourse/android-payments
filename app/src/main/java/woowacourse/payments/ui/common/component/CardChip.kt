e4c888a6bc0b4e0fd9b6aa715c594376bc405388
cucumber99 <dlsguq1@gmail.com>
2025-09-08 17:12:35 +0900

    chore: 패키지 이동

--- app/src/main/java/woowacourse/payments/ui/common/component/CardChip.kt
+++ app/src/main/java/woowacourse/payments/ui/common/component/CardChip.kt
@@ -1,4 +1,4 @@
-package woowacourse.payments.ui.common
+package woowacourse.payments.ui.common.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
--- app/src/main/java/woowacourse/payments/ui/screen/cardAddition/CardNumberVisualTransformation.kt
+++ app/src/main/java/woowacourse/payments/ui/screen/cardAddition/CardNumberVisualTransformation.kt
@@ -1,4 +1,4 @@
-package woowacourse.payments.ui
+package woowacourse.payments.ui.screen.cardAddition
 
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.input.OffsetMapping
