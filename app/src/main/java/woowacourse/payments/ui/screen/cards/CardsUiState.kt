c289df30822bc22b7d0504972ae5104ab88f3cee
cucumber99 <dlsguq1@gmail.com>
2025-09-08 22:15:04 +0900

    feat: 카드 UI 상태 정의

---
+++ app/src/main/java/woowacourse/payments/ui/screen/cards/CardsUiState.kt
@@ -0,0 +1,9 @@
+package woowacourse.payments.ui.screen.cards
+
+import woowacourse.payments.ui.model.CardUiModel
+
+sealed interface CardsUiState {
+    data object Empty : CardsUiState
+    data class SingleCard(val card: CardUiModel) : CardsUiState
+    data class MultipleCards(val cards: List<CardUiModel>) : CardsUiState
+}
