75c5fdece3834c22e757deb369bd1c90540ad853
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-09-08 15:18:11 +0900

    feat: MultipleCardContent 구현

---
+++ app/src/main/java/woowacourse/payments/ui/MultipleCardContent.kt
@@ -0,0 +1,54 @@
+package woowacourse.payments.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MultipleCardContent(
+    cards: List<Card>,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Spacer(modifier = Modifier.padding(vertical = 12.dp))
+        cards.forEach { card: Card ->
+            PaymentCard(
+                detail = card,
+                modifier = Modifier.padding(bottom = 36.dp)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun MultipleCardContentPreview() {
+    MultipleCardContent(
+        cards =
+            listOf(
+                Card(
+                    number = "1234".repeat(4),
+                    owner = "CREW",
+                    expiredDate = "0421",
+                ),
+                Card(
+                    number = "1234".repeat(4),
+                    owner = "CREW",
+                    expiredDate = "0421",
+                ),
+                Card(
+                    number = "1234".repeat(4),
+                    owner = "CREW",
+                    expiredDate = "0421",
+                ),
+            ),
+    )
+}
