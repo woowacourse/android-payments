07aff318cb42b566185e12f7e1f22a2175f0e359
JunHyuk Im <imjh0417@gmail.com>
2025-09-08 16:40:22 +0900

    refactor: ktlint 적용

--- app/src/main/java/woowacourse/payments/domain/ExpiredDate.kt
+++ app/src/main/java/woowacourse/payments/domain/ExpiredDate.kt
@@ -4,15 +4,20 @@ import java.time.LocalDate
 
 class ExpiredDate private constructor(
     val month: Int,
-    val year: Int
+    val year: Int,
 ) {
     companion object {
-        fun of(month: Int, year: Int, now: LocalDate = LocalDate.now()): ExpiredDate? {
+        fun of(
+            month: Int,
+            year: Int,
+            now: LocalDate = LocalDate.now(),
+        ): ExpiredDate? {
             val currentYear = now.year - YEAR_TWO_THOUSAND
             val currentMonth = now.monthValue
 
-            if ((year == currentYear && month in currentMonth..MAX_MONTH) || (year > currentYear && month in MIN_MONTH..MAX_MONTH))
+            if ((year == currentYear && month in currentMonth..MAX_MONTH) || (year > currentYear && month in MIN_MONTH..MAX_MONTH)) {
                 return ExpiredDate(month, year)
+            }
             return null
         }
 
