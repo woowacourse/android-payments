8b3086e6f24faf754974b602d2f7a660c951ae9a
JunHyuk Im <imjh0417@gmail.com>
2025-09-08 16:43:22 +0900

    feat: CardParcelable 구현 및 Intent 확장 함수 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.android.junit5)
+    id("kotlin-parcelize")
 }
 
 android {
@@ -19,7 +20,6 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunnerArguments["runnerBuilder"] =
             "de.mannodermaus.junit5.AndroidJUnit5Builder"
-
     }
     packaging {
         resources {
@@ -31,7 +31,7 @@ android {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                "proguard-rules.pro",
             )
         }
     }
---
+++ app/src/main/java/woowacourse/payments/cards/CardParcelable.kt
@@ -0,0 +1,41 @@
+package woowacourse.payments.cards
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import woowacourse.payments.domain.Card
+import woowacourse.payments.domain.CardNumber
+import woowacourse.payments.domain.ExpiredDate
+import woowacourse.payments.domain.OwnerName
+
+@Parcelize
+data class CardParcelable(
+    val cardNumber: String,
+    val expiredMonth: Int,
+    val expiredYear: Int,
+    val ownerName: String,
+    val password: String,
+) : Parcelable {
+    fun toDomainOrNull(): Card? {
+        val expiredDate =
+            ExpiredDate.of(
+                month = expiredMonth,
+                year = expiredYear,
+            ) ?: return null
+
+        return Card(
+            cardNumber = CardNumber(cardNumber),
+            expiredDate = expiredDate,
+            ownerName = OwnerName(ownerName),
+            password = password,
+        )
+    }
+}
+
+fun Card.toParcelable(): CardParcelable =
+    CardParcelable(
+        cardNumber = cardNumber.numbers,
+        expiredMonth = expiredDate.month,
+        expiredYear = expiredDate.year,
+        ownerName = ownerName.name,
+        password = password,
+    )
