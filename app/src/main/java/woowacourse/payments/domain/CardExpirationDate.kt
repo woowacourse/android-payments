1ec7b3ca12c5f6e0181b0be57f156b59377aa715
jiyunlee <02leejiyun@naver.com>
2025-09-08 21:07:40 +0900

    feat: 카드 만료일 유효성 검사 추가

--- app/src/main/java/woowacourse/payments/domain/CardExpirationDate.kt
+++ app/src/main/java/woowacourse/payments/domain/CardExpirationDate.kt
@@ -1,9 +1,30 @@
 package woowacourse.payments.domain
 
 import java.time.YearMonth
+import java.time.format.DateTimeFormatter
 
-data class CardExpirationDate(
+@JvmInline
+value class CardExpirationDate(
     val date: YearMonth,
 ) {
+    init {
+        require(!isExpired()) { ERROR_EXPIRED_CARD_EXPIRATION_DATE }
+    }
+
     fun isExpired(now: YearMonth = YearMonth.now()): Boolean = date.isBefore(now)
+
+    companion object {
+        private const val ERROR_EXPIRED_CARD_EXPIRATION_DATE = "만료된 카드입니다."
+
+        fun from(
+            date: String,
+            formatter: DateTimeFormatter,
+        ): CardExpirationDate =
+            CardExpirationDate(
+                YearMonth.parse(
+                    date.replace(" ", ""),
+                    formatter,
+                ),
+            )
+    }
 }
