f14d0bcf3c0afdde08f4dedf57ef3a2a2f73e14e
cucumber99 <dlsguq1@gmail.com>
2025-09-08 16:19:07 +0900

    feat: CardsActivity 임시 구현

---
+++ app/src/main/java/woowacourse/payments/ui/screen/cards/CardsActivity.kt
@@ -0,0 +1,14 @@
+package woowacourse.payments.ui.screen.cards
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+
+class CardsActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {  }
+    }
+}
