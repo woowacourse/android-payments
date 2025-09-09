0e70d9a4db01f2c23947b6c2527ffde23c488e03
hwannow <vnfdlvgid061@naver.com>
2025-09-09 09:44:14 +0900

    refactor: 불필요한 import 정리

--- app/src/main/java/woowacourse/payments/ui/cards/CardsScreen.kt
+++ app/src/main/java/woowacourse/payments/ui/cards/CardsScreen.kt
@@ -3,13 +3,10 @@ package woowacourse.payments.ui.cards
 import android.app.Activity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,10 +16,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import woowacourse.payments.R
 import woowacourse.payments.ui.cards.components.AddCardImage
 import woowacourse.payments.ui.cards.components.CardItem
