1181803412eed49d10e798def0b9236d2610ce89
rosemin928 <rosemin928@naver.com>
2025-09-09 15:21:10 +0900

    test: PaymentCardsContent에 대한 테스트 추가

---
+++ app/src/androidTest/java/woowacourse/payments/Tags.kt
@@ -0,0 +1,8 @@
+package woowacourse.payments
+
+object Tags {
+    const val EMPTY_CARD = "tag_empty_card"
+    const val SINGLE_CARD = "tag_single_card"
+    const val MULTI_CARDS = "tag_multi_cards"
+    const val TOPBAR_ADD = "tag_topbar_add"
+}
