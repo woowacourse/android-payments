4242eb89c03df9ffb8e9485ab2e1b32220f28621
지원 <cheerfulboys12@gmail.com>
2025-09-10 00:10:41 +0900

    feat: Card 객체에 Parcelable 구현

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.android.junit5)
     id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
+    id("kotlin-parcelize")
 }
 
 android {
--- app/src/main/java/woowacourse/payments/domain/Card.kt
+++ app/src/main/java/woowacourse/payments/domain/Card.kt
@@ -1,8 +1,12 @@
 package woowacourse.payments.domain
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Card(
     val number: String,
     val expiry: String,
     val password: String,
     val name: String? = null,
-)
+): Parcelable
