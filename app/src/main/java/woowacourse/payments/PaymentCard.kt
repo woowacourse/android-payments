fccce67dc48d37b64ed7d54fb54482f458c33ae4
지원 <cheerfulboys12@gmail.com>
2025-09-10 00:18:55 +0900

    feat: 카드 UI에 카드 정보 표시

--- app/src/main/java/woowacourse/payments/PaymentCard.kt
+++ app/src/main/java/woowacourse/payments/PaymentCard.kt
@@ -1,21 +1,33 @@
 package woowacourse.payments
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import woowacourse.payments.domain.Card
 import woowacourse.payments.ui.theme.AndroidpaymentsTheme
 
 @Composable
-fun PaymentCard(modifier: Modifier = Modifier) {
+fun PaymentCard(
+    card: Card? = null,
+    modifier: Modifier = Modifier,
+) {
     Box(
         contentAlignment = Alignment.CenterStart,
         modifier =
@@ -27,16 +39,47 @@ fun PaymentCard(modifier: Modifier = Modifier) {
                     shape = RoundedCornerShape(5.dp),
                 ),
     ) {
-        Box(
-            modifier =
-                modifier
-                    .padding(start = 14.dp, bottom = 10.dp)
-                    .size(40.dp, 28.dp)
-                    .background(
-                        color = Color(0xFFCBBA64),
-                        shape = RoundedCornerShape(5.dp),
-                    ),
-        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .padding(top = 28.dp)
+        ) {
+            Box(
+                modifier =
+                    modifier
+                        .size(40.dp, 28.dp)
+                        .background(
+                            color = Color(0xFFCBBA64),
+                            shape = RoundedCornerShape(5.dp),
+                        ),
+            )
+
+            if (card != null) {
+                Text(
+                    text = card.number,
+                    color = Color.White,
+                    fontSize = 12.sp,
+//                    letterSpacing = 2.04.sp,
+                )
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = card.name ?: "",
+                        color = Color.White,
+                        fontSize = 12.sp
+                    )
+                    Text(
+                        text = card.expiry,
+                        color = Color.White,
+                        fontSize = 12.sp,
+                    )
+                }
+            }
+        }
     }
 }
 
@@ -44,6 +87,6 @@ fun PaymentCard(modifier: Modifier = Modifier) {
 @Composable
 private fun PaymentCardPreview() {
     AndroidpaymentsTheme {
-        PaymentCard()
+        PaymentCard(Card("0000 - 0000 - 0000 - 0000", "10 / 25", "1234", "CREW"))
     }
 }
