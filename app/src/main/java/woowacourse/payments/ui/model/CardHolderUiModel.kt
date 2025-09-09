a7f0908e7405ef8d3dcb08438ca1c7cf279b971e
hwannow <vnfdlvgid061@naver.com>
2025-09-09 19:41:11 +0900

    fix: 카드 소유자 이름 최소 길이 제한 제거

--- app/src/main/java/woowacourse/payments/ui/model/CardHolderUiModel.kt
+++ app/src/main/java/woowacourse/payments/ui/model/CardHolderUiModel.kt
@@ -8,14 +8,11 @@ data class CardHolderUiModel(
     val value: String,
 ) : Parcelable {
     init {
-        require(
-            value.length in CARD_HOLDER_MIN_LENGTH..CARD_HOLDER_MAX_LENGTH,
-        ) { throw IllegalArgumentException(MESSAGE_INVALID_NAME_LENGTH) }
+        require(value.length < CARD_HOLDER_MAX_LENGTH) { throw IllegalArgumentException(MESSAGE_INVALID_NAME_LENGTH) }
     }
 
     companion object {
         private const val MESSAGE_INVALID_NAME_LENGTH: String = "INVALID_NAME_LENGTH"
-        private const val CARD_HOLDER_MIN_LENGTH: Int = 1
         const val CARD_HOLDER_MAX_LENGTH: Int = 30
     }
 }
