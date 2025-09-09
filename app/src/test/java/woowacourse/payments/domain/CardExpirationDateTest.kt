6ccfd9f10ff67db9d2e56f3020f9d34c75215d8a
jiyunlee <02leejiyun@naver.com>
2025-09-09 21:00:02 +0900

    test: 카드 만료일 유효성 검사 테스트 추가

---
+++ app/src/test/java/woowacourse/payments/domain/CardExpirationDateTest.kt
@@ -0,0 +1,70 @@
+package woowacourse.payments.domain
+
+import io.kotest.assertions.throwables.shouldThrowAny
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import java.time.YearMonth
+import java.time.format.DateTimeFormatter
+
+class CardExpirationDateTest {
+    private val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("MMyy")
+
+    @Test
+    fun `과거 날짜일 경우 만료되었다`() {
+        // given
+        val pastDate = CardExpirationDate(YearMonth.of(2020, 12))
+
+        // when
+        val result = pastDate.isExpired(YearMonth.of(2025, 9))
+
+        // then
+        result shouldBe true
+    }
+
+    @Test
+    fun `미래 날짜일 경우 만료되지 않았다`() {
+        // given
+        val futureDate = CardExpirationDate(YearMonth.of(2030, 1))
+
+        // when
+        val result = futureDate.isExpired(YearMonth.of(2025, 9))
+
+        // then
+        result shouldBe false
+    }
+
+    @Test
+    fun `현재 날짜와 같을 경우 만료되지 않았다`() {
+        // given
+        val currentDate = CardExpirationDate(YearMonth.of(2025, 9))
+
+        // when
+        val result = currentDate.isExpired(YearMonth.of(2025, 9))
+
+        // then
+        result shouldBe false
+    }
+
+    @Test
+    fun `올바른 형식의 문자열은 YearMonth로 변환된다`() {
+        // given
+        val dateString = "1225"
+
+        // when
+        val cardExpirationDate = CardExpirationDate.from(dateString, formatter)
+
+        // then
+        cardExpirationDate.date shouldBe YearMonth.of(2025, 12)
+    }
+
+    @Test
+    fun `잘못된 날짜 문자열을 입력하면 예외가 발생한다`() {
+        // given
+        val dateString = "0000"
+
+        // when & then
+        shouldThrowAny {
+            CardExpirationDate.from(dateString, formatter)
+        }
+    }
+}
