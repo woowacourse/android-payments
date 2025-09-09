529124725475a0fd8abbd39744b0e67e78a8d1a9
medAndro <med4ndro@gmail.com>
2025-09-09 11:39:33 +0900

    feat: 카드 비밀번호 입력 시 4자리 미만일 경우 에러 메시지 표시

--- app/src/main/java/woowacourse/payments/ui/features/addcard/components/CardPasswordField.kt
+++ app/src/main/java/woowacourse/payments/ui/features/addcard/components/CardPasswordField.kt
@@ -2,12 +2,15 @@ package woowacourse.payments.ui.features.addcard.components
 
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -24,6 +27,10 @@ fun CardPasswordField(
     value: String,
     onValueChange: (String) -> Unit,
 ) {
+    var isFocused by remember { mutableStateOf(false) }
+    val isIncomplete = value.isNotEmpty() && value.length < MAX_LENGTH_PASSWORD
+    val showError = isIncomplete && !isFocused
+
     val visualTransformation = remember { PasswordVisualTransformation() }
     AppTextField(
         value = value,
@@ -31,10 +38,23 @@ fun CardPasswordField(
             val filteredValue = newValue.filter { it in '0'..'9' }.take(MAX_LENGTH_PASSWORD)
             onValueChange(filteredValue)
         },
-        modifier = modifier,
+        modifier =
+            modifier.onFocusChanged { focusState ->
+                isFocused = focusState.isFocused
+            },
         labelText = stringResource(R.string.add_card_password_field_title),
         placeholderText = stringResource(R.string.add_card_password_field_hint),
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        isError = showError,
+        supportingText = {
+            if (showError) {
+                Text(
+                    modifier = Modifier,
+                    text = stringResource(id = R.string.add_card_password_incomplete_error_message),
+                    color = MaterialTheme.colorScheme.error,
+                )
+            }
+        },
         visualTransformation = visualTransformation,
     )
 }
