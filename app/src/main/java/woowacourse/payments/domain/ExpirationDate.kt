56dbea2225be90fae62dd28d1d01b8b4844f5932
Dongjoo Seo <83579348+etama123@users.noreply.github.com>
2025-09-09 10:34:41 +0900

    feat: 만료일 입력 형식 변경 (MM/YY -> MMYY)

--- app/src/main/java/woowacourse/payments/domain/ExpirationDate.kt
+++ app/src/main/java/woowacourse/payments/domain/ExpirationDate.kt
@@ -8,15 +8,17 @@ value class ExpirationDate(
     val expirationDate: String,
 ) {
     init {
-        val regex = Regex("^(0[1-9]|1[0-2])/([0-9]{2})$")
-        require(expirationDate.matches(regex)) {
-            "만료일은 'MM/YY' 형식이어야 합니다."
+        require(expirationDate.length == 4 && expirationDate.all { it.isDigit() }) {
+            "만료일은 4자리 숫자여야 합니다."
         }
+
+        val month = expirationDate.substring(0, 2)
+        val year = expirationDate.substring(2, 4)
 
         val expirationYearMonth =
             runCatching {
                 val formatter = DateTimeFormatter.ofPattern("MM/yy")
-                YearMonth.parse(expirationDate, formatter)
+                YearMonth.parse("$month/$year", formatter)
             }.getOrElse {
                 throw IllegalArgumentException("유효하지 않은 날짜 형식입니다.", it)
             }
