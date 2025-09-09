42133bba646fc2f3174b3ccb40c7f10875e452d4
devfeijoa <devfeijoa@gmail.com>
2025-09-09 21:54:59 +0900

    feat: 빈 카드 UI 추가

--- app/src/main/java/woowacourse/payments/CardWalletScreen.kt
+++ app/src/main/java/woowacourse/payments/CardWalletScreen.kt
@@ -1,12 +1,21 @@
 package woowacourse.payments
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import woowacourse.payments.component.CardWalletTopBar
+import woowacourse.payments.component.EmptyCard
 
 @Composable
 fun CardWalletScreen(
@@ -20,9 +29,14 @@ fun CardWalletScreen(
         }
     ) { innerPadding ->
         Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.padding(innerPadding)
         ) {
+            Spacer(modifier = Modifier.height(32.dp).fillMaxSize())
+            Text("새로운 카드를 등록해주세요", fontSize = 18.sp, fontWeight = FontWeight.W700)
 
+            Spacer(modifier = Modifier.height(32.dp).fillMaxSize())
+            EmptyCard({} )
         }
     }
 }
--- app/src/main/java/woowacourse/payments/ui/theme/Color.kt
+++ app/src/main/java/woowacourse/payments/ui/theme/Color.kt
@@ -11,3 +11,5 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 val Grey10 = Color(0xFFAAAAAA)
+
+val Grey20 = Color(0xFFE5E5E5)
