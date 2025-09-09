54fc84506b783e6a37521553ccb9200afb88c890
yrsel <xxknu88@gmail.com>
2025-09-09 11:54:13 +0900

    feat: 카드 목록 화면 액티비티 추가

---
+++ app/src/main/java/woowacourse/payments/ui/payments/PaymentCardsActivity.kt
@@ -0,0 +1,18 @@
+package woowacourse.payments.ui.payments
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import woowacourse.payments.ui.theme.AndroidpaymentsTheme
+
+class PaymentCardsActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndroidpaymentsTheme {
+            }
+        }
+    }
+}
