11e168f2cb35e1979cb732ddabc45d833b3bba34
지원 <cheerfulboys12@gmail.com>
2025-09-10 00:10:30 +0900

    feat: 카드 목록 화면 UI 구현 및 NewCardActivity으로 이동하는 로직 구현

--- app/src/main/java/woowacourse/payments/list/ListActivity.kt
+++ app/src/main/java/woowacourse/payments/list/ListActivity.kt
@@ -1,23 +1,47 @@
 package woowacourse.payments.list
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import woowacourse.payments.NewCardActivity
+import woowacourse.payments.PaymentCard
+import woowacourse.payments.domain.Card
 import woowacourse.payments.ui.theme.AndroidpaymentsTheme
 
 class ListActivity : ComponentActivity() {
@@ -26,22 +50,27 @@ class ListActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AndroidpaymentsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize(),
-                    topBar = {
-                        CardListTopBar(
-                            onAddClick = {},
-                        )
-                    }
-                ) { innerPadding ->
-                    Column(
-                        modifier =
-                            Modifier
-                                .fillMaxSize()
-                                .padding(innerPadding),
-                    ) {
+                var cards by remember { mutableStateOf(CardUiState()) }
+                val context = LocalContext.current
 
+                val launcher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.StartActivityForResult()
+                ) { result ->
+                    if (result.resultCode == RESULT_OK) {
+                        val newCard = result.data?.getParcelableExtra<Card>("card")
+                        newCard?.let {
+                            cards = CardUiState(cards.cards + it)
+                        }
                     }
                 }
+
+                CardListScreen(
+                    cards = cards.cards,
+                    onAddClick = {
+                        val intent = Intent(context, NewCardActivity::class.java)
+                        launcher.launch(intent)
+                    }
+                )
             }
         }
     }
@@ -50,33 +79,121 @@ class ListActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CardListTopBar(
-    onAddClick: () -> Unit,
     modifier: Modifier = Modifier,
+    actions: @Composable (RowScope.() -> Unit) = {}
 ) {
     CenterAlignedTopAppBar(
         title = { Text(text = "Payments") },
-        actions = {
-            TextButton (onClick = { onAddClick() },
-                content = {
-                    Text(
-                        text = "추가",
-                        fontSize = 18.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color.Black
-                        )
-                }
-            )
-        },
+        actions = actions,
         modifier = modifier,
     )
+}
+
+@Composable
+fun CardListScreen(
+    cards: List<Card>,
+    onAddClick: () -> Unit,
+) {
+    Scaffold(
+        topBar = {
+            CardListTopBar(
+                actions = {
+                    if (cards.size >= 2) {
+                        TextButton(
+                            onClick = onAddClick,
+                            content = { Text(
+                            text = "추가",
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.Black
+                        ) } )
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(18.dp)
+        ) {
+            when (cards.size) {
+                0 -> {
+                    AddNewCardText()
+                    AddNewCard(
+                        onAddClick = onAddClick,
+                    )
+                }
+
+                1 -> {
+                    CardList(cards)
+                    AddNewCard(
+                        onAddClick = onAddClick,
+                    )
+                }
+
+                else -> {
+                    CardList(cards)
+                }
+            }
+        }
+    }
+
+}
+
+@Composable
+fun CardList(
+    cards: List<Card>,
+) {
+    cards.forEach { card: Card -> PaymentCard(card) }
+}
+
+@Composable
+fun AddNewCard(
+    onAddClick: () -> Unit,
+) {
+    Box(
+        modifier = Modifier
+            .height(124.dp)
+            .width(208.dp)
+            .clickable(onClick = onAddClick)
+            .background(color = Color(0xFFE5E5E5)),
+        content = {
+            Icon(
+                imageVector = Icons.Filled.Add,
+                contentDescription = "새로운 카드 추가",
+                modifier = Modifier.align(alignment = Alignment.Center)
+            )
+        })
+}
+
+@Composable
+fun AddNewCardText() {
+    Text(text = "새로운 카드를 등록해주세요", fontSize = 18.sp, fontWeight = FontWeight.Bold)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AndroidpaymentsTheme {
-        CardListTopBar(
-            onAddClick = {}
-        )
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+        ) { innerPadding ->
+            Column(
+                modifier =
+                    Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(18.dp)
+            ) {
+                CardListScreen(
+                    emptyList(),
+                    onAddClick = {}
+                )
+            }
+        }
     }
 }
