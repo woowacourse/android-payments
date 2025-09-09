8b5b53d9b21a37b56677e21b5f742df9f1083a16
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-09-09 12:02:04 +0900

    feat: 새로운 카드를 추가하면 "카드가 추가되었습니다" 토스트가 노출

--- app/src/main/java/woowacourse/payments/ui/screen/PaymentScreen.kt
+++ app/src/main/java/woowacourse/payments/ui/screen/PaymentScreen.kt
@@ -1,11 +1,18 @@
 package woowacourse.payments.ui.screen
 
+import android.app.Activity
 import android.content.Intent
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import woowacourse.payments.domain.PaymentCard
 import woowacourse.payments.ui.component.payments.PaymentsColumn
@@ -18,9 +25,26 @@ fun PaymentScreen(
     cards: List<PaymentCard>,
     onAddNewCardClick: () -> Unit
 ) {
+    val context = LocalContext.current
+    val cardList = remember { mutableStateListOf<PaymentCard>() }
+    val screenType = remember { mutableStateListOf<PaymentCardCount>() }
+    val cardAddLauncher =
+        rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) { activityResult ->
+            if (activityResult.resultCode == Activity.RESULT_OK) {
+                // 카드 추가 성공!
+
+
+            }
+        }
+
     Scaffold(
         modifier = modifier,
-        topBar = { PaymentsTopBar(onAddNewCardClick = { onAddNewCardClick() }) }
+        topBar = {
+            PaymentsTopBar(onAddNewCardClick = {
+                onAddNewCardClick()
+                Toast.makeText(context, "카드가 추가되었습니다", Toast.LENGTH_SHORT).show()
+            })
+        }
     ) { paddingValues: PaddingValues ->
         PaymentsColumn(
             onClickAddCard = onAddNewCardClick,
