69645e2976bf1f79d6d932a68bc72f77442625c3
yrsel <xxknu88@gmail.com>
2025-09-09 13:23:56 +0900

    test: 카드 목록 앱 바 구현

---
+++ app/src/androidTest/java/woowacourse/payments/ui/payments/PaymentCardsTopAppBarTest.kt
@@ -0,0 +1,47 @@
+package woowacourse.payments.ui.payments
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotDisplayed
+import androidx.compose.ui.test.hasContentDescription
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Rule
+import org.junit.Test
+
+class PaymentCardsTopAppBarTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun isVisibleRegistrationButton이_true면_추가_버튼이_활성화된다() {
+        // given && when
+        val isVisibleRegistrationButton = true
+        composeTestRule.setContent {
+            PaymentCardsTopAppBar(
+                onRegistrationClick = {},
+                isVisibleRegistrationButton = isVisibleRegistrationButton,
+            )
+        }
+
+        // then
+        composeTestRule
+            .onNode(hasContentDescription("카드 목록 앱 바 추가 버튼"))
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun isVisibleRegistrationButton이_false면_추가_버튼이_비활성화된다() {
+        // given && when
+        val isVisibleRegistrationButton = false
+        composeTestRule.setContent {
+            PaymentCardsTopAppBar(
+                onRegistrationClick = {},
+                isVisibleRegistrationButton = isVisibleRegistrationButton,
+            )
+        }
+
+        // then
+        composeTestRule
+            .onNode(hasContentDescription("카드 목록 앱 바 추가 버튼"))
+            .assertIsNotDisplayed()
+    }
+}
---
+++ app/src/main/java/woowacourse/payments/ui/payments/PaymentCardsTopAppBar.kt
@@ -0,0 +1,84 @@
+package woowacourse.payments.ui.payments
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import woowacourse.payments.R
+
+@Composable
+fun PaymentCardsTopAppBar(
+    onRegistrationClick: () -> Unit,
+    isVisibleRegistrationButton: Boolean,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier =
+            modifier
+                .fillMaxWidth()
+                .statusBarsPadding()
+                .height(64.dp),
+    ) {
+        Text(
+            text = stringResource(R.string.app_name),
+            modifier = Modifier.align(Alignment.Center),
+            fontWeight = FontWeight.W400,
+            lineHeight = 28.sp,
+            fontSize = 22.sp,
+            textAlign = TextAlign.Center,
+            maxLines = 1,
+        )
+
+        if (isVisibleRegistrationButton) ShowRegistrationButton(onRegistrationClick)
+    }
+}
+
+@Composable
+private fun BoxScope.ShowRegistrationButton(onRegistrationClick: () -> Unit) {
+    TextButton(
+        onClick = onRegistrationClick,
+        modifier =
+            Modifier
+                .semantics { contentDescription = "카드 목록 앱 바 추가 버튼" }
+                .align(Alignment.CenterEnd),
+    ) {
+        Text(
+            text = "추가",
+            lineHeight = 28.sp,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.W700,
+        )
+    }
+}
+
+@Preview(showBackground = true, name = "메인 화면 앱 바 추가 버튼 비활성화")
+@Composable
+private fun InvisibleRegistrationButtonPreview() {
+    PaymentCardsTopAppBar(
+        onRegistrationClick = { },
+        isVisibleRegistrationButton = false,
+    )
+}
+
+@Preview(showBackground = true, name = "메인 화면 앱 바 추가 버튼 활성화")
+@Composable
+private fun VisibleRegistrationButtonPreview() {
+    PaymentCardsTopAppBar(
+        onRegistrationClick = { },
+        isVisibleRegistrationButton = true,
+    )
+}
