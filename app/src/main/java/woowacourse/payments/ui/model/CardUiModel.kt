e6bdf8d1016d06b5e688ae2be39d867aaafaeefe
devfeijoa <devfeijoa@gmail.com>
2025-09-10 17:33:29 +0900

    feat: `CardUiModel`에 카드 비밀번호 필드 추가 및 Parcelize 구현

--- app/src/main/java/woowacourse/payments/ui/model/CardUiModel.kt
+++ app/src/main/java/woowacourse/payments/ui/model/CardUiModel.kt
@@ -1,10 +1,15 @@
 package woowacourse.payments.ui.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class CardUiModel(
     val cardNumber: String,
     val cardHolderName: String,
     val cardExpiryDate: String,
-) {
+    val cardPassword: String
+) : Parcelable {
     fun maskedCardNumber(): String {
         val visibleText = cardNumber.take(8)
         val maskedText = "*".repeat(8)
@@ -15,7 +20,8 @@ data class CardUiModel(
 
     fun formattedExpiryDate(): String {
         return if (cardExpiryDate.length == 4) {
-            cardExpiryDate.substring(0, 2) + " / " + cardExpiryDate.substring(2)
+//            cardExpiryDate.substring(0, 2) + " / " + cardExpiryDate.substring(2)
+            "${cardExpiryDate.substring(0,2)} / ${cardExpiryDate.substring(2,4)}"
         } else {
             cardExpiryDate
         }
