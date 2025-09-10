86c775744b13f80c958314fa685eeef6bccbfd95
devfeijoa <devfeijoa@gmail.com>
2025-09-10 17:35:31 +0900

    feat: CardWalletTopBar에 카드 추가 버튼 구현
    - `isAddable` 파라미터 추가하여 버튼 표시 여부 제어
    - `onAddCardClick` 콜백 함수를 통해 카드 추가 화면으로 이동하는 로직 연결

--- app/src/main/java/woowacourse/payments/component/CardWalletTopBar.kt
+++ app/src/main/java/woowacourse/payments/component/CardWalletTopBar.kt
@@ -1,24 +1,57 @@
 package woowacourse.payments.component
 
+import android.content.Intent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import woowacourse.payments.NewCardScreenActivity
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CardWalletTopBar(
+    isAddable: Boolean = false,
+    onAddCardClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    val context = LocalContext.current
+
     CenterAlignedTopAppBar(
-        title = { Text("Payments") },
+        title = {
+            Text("Payments")
+        },
+        actions = {
+            if (isAddable) {
+                Text(
+                    modifier =
+                        modifier
+                            .clickable {
+                                onAddCardClick()
+                            }
+                            .padding(end = 20.dp),
+                    text = "추가",
+                    fontWeight = FontWeight.W700,
+                    fontSize = 18.sp,
+                )
+            }
+        },
     )
 }
 
 @Composable
 @Preview(showBackground = true)
 fun CardWalletTopBarPreview() {
-    CardWalletTopBar()
+    CardWalletTopBar(
+        isAddable = true,
+        onAddCardClick = {}
+    )
 }
