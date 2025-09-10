04527e688c3a220d6c4d2b815721b9f7281eda1b
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-09-10 14:29:57 +0900

    chore: ktlint 적용

--- app/src/main/java/woowacourse/payments/cardaddition/component/CardAdditionScreen.kt
+++ app/src/main/java/woowacourse/payments/cardaddition/component/CardAdditionScreen.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.app.Activity.RESULT_OK
 import android.content.Intent
 import androidx.activity.compose.LocalActivity
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,7 +14,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
