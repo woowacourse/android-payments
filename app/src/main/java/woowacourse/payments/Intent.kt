66dd41a4a95850af8c90a97ed0268d6c2f8e9a6e
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-09-10 16:13:37 +0900

    refactor: getParcelableCompat의 타입 파라미터를 Parcelable로 한정

--- app/src/main/java/woowacourse/payments/Intent.kt
+++ app/src/main/java/woowacourse/payments/Intent.kt
@@ -2,11 +2,13 @@ package woowacourse.payments
 
 import android.content.Intent
 import android.os.Build
+import android.os.Parcelable
 
-inline fun <reified T> Intent.getParcelableCompat(key: String): T? =
+inline fun <reified T : Parcelable> Intent.getParcelableCompat(key: String): T? =
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
         getParcelableExtra(key, T::class.java)
     } else {
+        @Suppress("DEPRECATION")
         getParcelableExtra(key)
     }
 
