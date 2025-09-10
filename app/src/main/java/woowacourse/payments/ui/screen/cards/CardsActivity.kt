2b3bb341c15dd76738539a99e0261c6c6fed5a5a
cucumber99 <dlsguq1@gmail.com>
2025-09-10 15:39:14 +0900

    refactor: 불필요한 import 제거

--- app/src/main/java/woowacourse/payments/ui/screen/cards/CardsActivity.kt
+++ app/src/main/java/woowacourse/payments/ui/screen/cards/CardsActivity.kt
@@ -3,19 +3,10 @@ package woowacourse.payments.ui.screen.cards
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.platform.LocalContext
-import woowacourse.payments.R
-import woowacourse.payments.ui.common.extension.getParcelableCompat
 import woowacourse.payments.ui.model.CardUiModel
-import woowacourse.payments.ui.screen.cardAddition.CardAdditionActivity
 import woowacourse.payments.ui.screen.cards.component.CardsScreen
 import woowacourse.payments.ui.theme.AndroidpaymentsTheme
 
