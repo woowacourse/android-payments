311327cbc7a514024ffb673c1d5ac9b44ade90cc
yrsel <xxknu88@gmail.com>
2025-09-10 15:03:01 +0900

    refactor: 인텐트 버전 분기 함수명 수정

--- app/src/main/java/woowacourse/payments/ui/common/IntentExtenstions.kt
+++ app/src/main/java/woowacourse/payments/ui/common/IntentExtenstions.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import android.os.Build
 import android.os.Parcelable
 
-inline fun <reified T : Parcelable> Intent.getParcelableCompat(key: String): T? =
+inline fun <reified T : Parcelable> Intent.getParcelableExtraCompat(key: String): T? =
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
         getParcelableExtra(key, T::class.java)
     } else {
