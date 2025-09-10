4e82f32d04e5df33aeca41b369ce17533dfbb1b2
DoableTuple <doabletuple@gmail.com>
2025-09-10 15:48:54 +0900

    refactor: CardListTopBar를 CenterAlignedTopAppBar로 변경

--- app/src/main/java/woowacourse/payments/ui/cardlist/CardListTopBar.kt
+++ app/src/main/java/woowacourse/payments/ui/cardlist/CardListTopBar.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -33,12 +34,11 @@ fun CardListTopBar(
     cards: SnapshotStateList<CardUiModel>,
     onAddCard: () -> Unit,
 ) {
-    TopAppBar(
+    CenterAlignedTopAppBar(
         title = {
             Text(
                 text = stringResource(R.string.card_list_title),
                 textAlign = TextAlign.Center,
-                modifier = Modifier.fillMaxWidth(),
             )
         },
         actions = {
