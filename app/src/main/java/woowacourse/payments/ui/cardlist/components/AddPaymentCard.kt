5f6245b29cc9decc27499657706f70a1101ae551
jiyunlee <02leejiyun@naver.com>
2025-09-10 10:52:37 +0900

    feat: 카드 추가 버튼에 semantics 속성 추가

--- app/src/main/java/woowacourse/payments/ui/cardlist/components/AddPaymentCard.kt
+++ app/src/main/java/woowacourse/payments/ui/cardlist/components/AddPaymentCard.kt
@@ -13,6 +13,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import woowacourse.payments.R
@@ -22,6 +24,8 @@ fun AddPaymentCard(
     modifier: Modifier = Modifier,
     onAddClick: () -> Unit = {},
 ) {
+    val addCardDescription: String = stringResource(R.string.add_card_button_description)
+
     Box(
         contentAlignment = Alignment.Center,
         modifier =
@@ -31,11 +35,12 @@ fun AddPaymentCard(
                     color = Color.LightGray,
                     shape = RoundedCornerShape(5.dp),
                 )
-                .clickable { onAddClick() },
+                .clickable { onAddClick() }
+                .semantics { contentDescription = addCardDescription },
     ) {
         Icon(
             imageVector = Icons.Filled.Add,
-            contentDescription = stringResource(R.string.add_card_button_description),
+            contentDescription = null,
             tint = Color.DarkGray,
         )
     }
