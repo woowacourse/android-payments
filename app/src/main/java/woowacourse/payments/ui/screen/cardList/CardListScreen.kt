d97ec56a8cc29f0c74e7e246fb707386c0d2d080
Ham BeomJoon <hbj0802@naver.com>
2025-09-10 14:16:46 +0900

    chore: ktlint 적용

--- app/src/main/java/woowacourse/payments/ui/screen/cardList/CardListScreen.kt
+++ app/src/main/java/woowacourse/payments/ui/screen/cardList/CardListScreen.kt
@@ -26,8 +26,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.contentDescription
-import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
