8056b9efdd0ef589d4c001943ed7b503cf8356e3
DoableTuple <doabletuple@gmail.com>
2025-09-10 13:54:14 +0900

    test: CardListContentsTest 추가

--- app/src/androidTest/java/woowacourse/payments/ui/cardlist/CardListContentsTest.kt
+++ app/src/androidTest/java/woowacourse/payments/ui/cardlist/CardListContentsTest.kt
@@ -7,30 +7,18 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import org.junit.Rule
 import org.junit.Test
-import woowacourse.payments.domain.Card
-import woowacourse.payments.domain.CardNumber
-import woowacourse.payments.domain.CardholderName
-import woowacourse.payments.domain.ExpirationDate
-import woowacourse.payments.domain.Passcode
-import java.time.YearMonth
+import woowacourse.payments.CARD_FIXTURE
+import woowacourse.payments.ui.model.CardUiModel
 
 @Suppress("ktlint:standard:function-naming")
 class CardListContentsTest {
-    val fixture: Card =
-        Card(
-            CardNumber("1234123412341234"),
-            ExpirationDate(YearMonth.of(2034, 12)),
-            CardholderName("디랙"),
-            Passcode("1234"),
-        )
-
     @get:Rule
     val composeTestRule = createComposeRule()
 
     @Test
     fun 카드가_하나도_없을_때는_카드_추가_안내_문구가_표시된다() {
         // given
-        val cards = mutableStateListOf<Card>()
+        val cards = mutableStateListOf<CardUiModel>()
 
         // when
         composeTestRule.setContent { CardListContents(cards) {} }
@@ -42,7 +30,7 @@ class CardListContentsTest {
     @Test
     fun 카드가_한_개_이상일_때는_카드_추가_안내_문구가_표시되지_않는다() {
         // given
-        val cards = mutableStateListOf(fixture)
+        val cards = mutableStateListOf(CARD_FIXTURE)
 
         // when
         composeTestRule.setContent { CardListContents(cards) {} }
@@ -52,9 +40,22 @@ class CardListContentsTest {
     }
 
     @Test
-    fun 카드가_한_개_이하일_때는_카드_추가_버튼이_기호로_표시된다() {
+    fun 카드가_한_개_이하일_때는_카드_추가_버튼이_기호로_표시된다_1() {
         // given
-        val cards = mutableStateListOf(fixture)
+        val cards = mutableStateListOf<CardUiModel>()
+
+        // when
+        composeTestRule.setContent { CardListContents(cards) {} }
+
+        // then
+        composeTestRule.onNodeWithText("+").assertIsDisplayed()
+        composeTestRule.onNodeWithText("추가").assertIsNotDisplayed()
+    }
+
+    @Test
+    fun 카드가_한_개_이하일_때는_카드_추가_버튼이_기호로_표시된다_2() {
+        // given
+        val cards = mutableStateListOf(CARD_FIXTURE)
 
         // when
         composeTestRule.setContent { CardListContents(cards) {} }
@@ -67,7 +68,7 @@ class CardListContentsTest {
     @Test
     fun 카드가_두_개_이상일_때는_카드_추가_버튼이_문구로_표시된다() {
         // given
-        val cards = mutableStateListOf(fixture, fixture)
+        val cards = mutableStateListOf(CARD_FIXTURE, CARD_FIXTURE)
 
         // when
         composeTestRule.setContent { CardListContents(cards) {} }
