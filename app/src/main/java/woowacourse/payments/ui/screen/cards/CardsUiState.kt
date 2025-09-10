0faa4a2089fae0689a35d79cb1d4ee88e0e322cd
cucumber99 <dlsguq1@gmail.com>
2025-09-10 13:30:12 +0900

    refactor: CardsUiState 내부에서 상태가 전이되도록 개선

--- app/src/main/java/woowacourse/payments/ui/screen/cards/CardsUiState.kt
+++ app/src/main/java/woowacourse/payments/ui/screen/cards/CardsUiState.kt
@@ -1,8 +1,18 @@
 package woowacourse.payments.ui.screen.cards
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import woowacourse.payments.ui.model.CardUiModel
 
-sealed interface CardsUiState {
+@Parcelize
+sealed interface CardsUiState : Parcelable {
+    fun addCard(newCard: CardUiModel): CardsUiState =
+        when (this) {
+            is Empty -> SingleCard(newCard)
+            is SingleCard -> MultipleCards(listOf(card, newCard))
+            is MultipleCards -> MultipleCards(cards + newCard)
+        }
+
     data object Empty : CardsUiState
 
     data class SingleCard(
