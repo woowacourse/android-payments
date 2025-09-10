d8665fe407d592147378292469140511d69c1b7e
윤동현 <donghyunyoon81@gmail.com>
2025-09-10 11:06:13 +0900

    feat: 카드 추가 시 토스트 메시지 표시

--- app/src/main/java/woowacourse/payments/ui/cards/CardsActivity.kt
+++ app/src/main/java/woowacourse/payments/ui/cards/CardsActivity.kt
@@ -1,9 +1,10 @@
 package woowacourse.payments.ui.cards
 
-import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
@@ -14,16 +15,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import woowacourse.payments.R
 import woowacourse.payments.ui.cardcreate.CreateCardActivity
 import woowacourse.payments.ui.cards.model.CardsUiState
 import woowacourse.payments.ui.model.PaymentCardUiModel
-import woowacourse.payments.ui.model.paymentCardUiModelSamples
 import woowacourse.payments.ui.theme.AndroidpaymentsTheme
 
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
 class CardsActivity : ComponentActivity() {
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -32,32 +35,20 @@ class CardsActivity : ComponentActivity() {
                 val cardsStateHolder = rememberSaveable(saver = CardsStateHolderSaver()) {
                     CardsStateHolder(CardsUiState.of(emptyList()))
                 }
-                val cardAddLauncher =
-                    rememberLauncherForActivityResult(
-                        contract = ActivityResultContracts.StartActivityForResult()
-                    ) { activityResult ->
-                        if (activityResult.resultCode == RESULT_OK) {
-                            val data = activityResult.data
-                            val cardUiModel = data?.getParcelableExtra(
-                                NEW_CARD_KEY,
-                                PaymentCardUiModel::class.java
-                            ) ?: return@rememberLauncherForActivityResult
-                            cardsStateHolder.addCard(cardUiModel)
-                        }
-                    }
+                val cardAddLauncher = cardAddLauncher(cardsStateHolder, LocalContext.current)
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
                     topBar = {
                         CardsTopBar(
-                            cardsStateHolder.cardsUiState,
+                            cardsUiState = cardsStateHolder.cardsUiState,
                             onAddClick = {
                                 val intent = CreateCardActivity.instance(this)
                                 cardAddLauncher.launch(intent)
                             })
                     }) { innerPadding ->
                     CardsScreen(
-                        cardsStateHolder.cardsUiState,
-                        {
+                        cardsUiState = cardsStateHolder.cardsUiState,
+                        onAddClick = {
                             val intent = CreateCardActivity.instance(this)
                             cardAddLauncher.launch(intent)
                         },
@@ -69,6 +60,22 @@ class CardsActivity : ComponentActivity() {
             }
         }
     }
+
+    @Composable
+    fun cardAddLauncher(cardsStateHolder: CardsStateHolder, context: Context) =
+        rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.StartActivityForResult()
+        ) { activityResult ->
+            if (activityResult.resultCode == RESULT_OK) {
+                val data = activityResult.data
+                val cardUiModel = data?.getParcelableExtra(
+                    NEW_CARD_KEY,
+                    PaymentCardUiModel::class.java
+                ) ?: return@rememberLauncherForActivityResult
+                cardsStateHolder.addCard(cardUiModel)
+                Toast.makeText(context, getString(R.string.created_card_message), Toast.LENGTH_SHORT).show()
+            }
+        }
 
     companion object {
         private const val NEW_CARD_KEY = "new_card_key"
