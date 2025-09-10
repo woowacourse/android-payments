cb0a6418f2769e761bba27b44be07b4e011db075
hwannow <vnfdlvgid061@naver.com>
2025-09-10 18:01:45 +0900

    refactor: 불필요한 어노테이션 삭제

--- app/src/main/java/woowacourse/payments/ui/cards/CardsActivity.kt
+++ app/src/main/java/woowacourse/payments/ui/cards/CardsActivity.kt
@@ -4,11 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.ExperimentalMaterial3Api
 import woowacourse.payments.ui.theme.AndroidpaymentsTheme
 
 class CardsActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
