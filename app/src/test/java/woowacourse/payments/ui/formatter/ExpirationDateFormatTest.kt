bc2f8d1365506688e08176ad64ce8b32636a59fb
DoableTuple <doabletuple@gmail.com>
2025-09-10 13:29:37 +0900

    test: ExpirationDateFormatTest 추가

---
+++ app/src/test/java/woowacourse/payments/ui/formatter/ExpirationDateFormatTest.kt
@@ -0,0 +1,21 @@
+package woowacourse.payments.ui.formatter
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import woowacourse.payments.domain.ExpirationDate
+import java.time.YearMonth
+
+class ExpirationDateFormatTest {
+    @Test
+    fun `만료일은 4자리씩 나누어 표시되고, 마지막 8자는 마스킹된다`() {
+        // given
+        val expirationDate = ExpirationDate(YearMonth.of(2099, 12))
+
+        // when
+        val actual: String = ExpirationDateFormat.formattedExpirationDate(expirationDate)
+
+        // then
+        val expected = "12 / 99"
+        assertThat(actual).isEqualTo(expected)
+    }
+}
