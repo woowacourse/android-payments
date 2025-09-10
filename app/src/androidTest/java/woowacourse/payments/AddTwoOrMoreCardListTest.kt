8b1da03ec686692501dc70d3ec83ce57785aea99
지원 <cheerfulboys12@gmail.com>
2025-09-10 15:03:05 +0900

    test: 카드 2개 이상 추가 시 카드 목록 UI 테스트 추가

---
+++ app/src/androidTest/java/woowacourse/payments/AddTwoOrMoreCardListTest.kt
@@ -0,0 +1,91 @@
+package woowacourse.payments
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.hasContentDescription
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import woowacourse.payments.domain.Card
+import woowacourse.payments.list.CardListScreen
+
+class AddTwoOrMoreCardListTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Before
+    fun setUp() {
+        composeTestRule.setContent {
+            CardListScreen(
+                cards = listOf(
+                    Card(
+                        "1234123412341234",
+                        "1234",
+                        "9999",
+                        "PARK JIWON"
+                    ),
+                    Card(
+                        "7890789078907890",
+                        "0999",
+                        "1234",
+                        "TOMATO BASIL ADE"
+                    )
+                ), onAddClick = {})
+        }
+    }
+
+    @Test
+    fun 추가된_카드가_두_개_이상이면_카드_추가_안내가_안_보인다() {
+        composeTestRule
+            .onNodeWithText("새로운 카드를 등록해주세요")
+            .assertDoesNotExist()
+    }
+
+    @Test
+    fun 추가된_카드가_두_개_이상이면_탑바에_추가_버튼이_보인다() {
+        composeTestRule
+            .onNodeWithText("추가")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun 추가된_카드가_두_개_이상이면_추가된_카드들의_일부가_마스킹된_번호가_보인다() {
+        composeTestRule
+            .onNodeWithText("1234 - 1234 - **** - ****")
+            .assertIsDisplayed()
+
+        composeTestRule
+            .onNodeWithText("7890 - 7890 - **** - ****")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun 추가된_카드가_두_개_이상이면_추가된_카드들의_만료일이_보인다() {
+        composeTestRule
+            .onNodeWithText("12 / 34")
+            .assertIsDisplayed()
+
+        composeTestRule
+            .onNodeWithText("09 / 99")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun 추가된_카드가_두_개_이상이면_추가된_카드들의_소유자명이_보인다() {
+        composeTestRule
+            .onNodeWithText("PARK JIWON")
+            .assertIsDisplayed()
+
+        composeTestRule
+            .onNodeWithText("TOMATO BASIL ADE")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun 추가된_카드가_두_개_이상이면_카드_추가_뷰가_안_보인다() {
+        composeTestRule
+            .onNode(hasContentDescription("새로운 카드 추가"))
+            .assertDoesNotExist()
+    }
+}
