88f6ff05d7c8f9025912656df8d69e7e8bf65efd
oungsi2000 <oungsi1000@gmail.com>
2025-09-11 19:02:20 +0900

    fix: 스크롤 가능하게 변경

--- app/src/main/java/woowacourse/payments/ui/allcards/AllCardsScreen.kt
+++ app/src/main/java/woowacourse/payments/ui/allcards/AllCardsScreen.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -34,8 +36,9 @@ fun AllCardsScreen(
     modifier: Modifier = Modifier,
     onPlusCardClick: () -> Unit = {},
 ) {
+    val scrollState = rememberScrollState()
     Column(
-        modifier = modifier,
+        modifier = modifier.verticalScroll(scrollState),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         when (allCards.viewType) {
