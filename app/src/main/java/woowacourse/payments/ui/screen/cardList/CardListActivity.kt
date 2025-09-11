0e48912f40f3ea3b773a31b535a556ecc94e3788
Ham BeomJoon <hbj0802@naver.com>
2025-09-11 20:23:44 +0900

    refactor: Activity에서 LocalContext 대신 this로 컨텍스트 사용

--- app/src/main/java/woowacourse/payments/ui/screen/cardList/CardListActivity.kt
+++ app/src/main/java/woowacourse/payments/ui/screen/cardList/CardListActivity.kt
@@ -10,7 +10,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.platform.LocalContext
 import woowacourse.payments.R
 import woowacourse.payments.ui.CardUiModel
 import woowacourse.payments.ui.screen.addCard.AddCardActivity
@@ -24,7 +23,6 @@ class CardListActivity : ComponentActivity() {
 
         setContent {
             AndroidpaymentsTheme {
-                val context = LocalContext.current
                 val cards = rememberSaveable { mutableStateListOf<CardUiModel>() }
 
                 val addCardLauncher =
@@ -38,7 +36,7 @@ class CardListActivity : ComponentActivity() {
                                 cards.add(it)
                                 Toast
                                     .makeText(
-                                        context,
+                                        this,
                                         R.string.card_list_add_card_toast,
                                         Toast.LENGTH_SHORT,
                                     ).show()
