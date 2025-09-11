954edb75b5caf9cca80a17fd15950334006ba25d
jiyunlee <02leejiyun@naver.com>
2025-09-11 18:10:14 +0900

    refactor: 카드 소유자 이름 유효성 검사 메시지 수정

--- app/src/main/java/woowacourse/payments/domain/CardHolderName.kt
+++ app/src/main/java/woowacourse/payments/domain/CardHolderName.kt
@@ -11,6 +11,6 @@ value class CardHolderName(
     companion object {
         const val MIN_NAME_LENGTH = 1
         const val MAX_NAME_LENGTH = 30
-        private const val ERROR_INVALID_NAME_LENGTH = "카드 소유자 이름은 30자 이하여야 합니다."
+        private const val ERROR_INVALID_NAME_LENGTH = "카드 소유자 이름은 1자 이상 30자 이하여야 합니다."
     }
 }
