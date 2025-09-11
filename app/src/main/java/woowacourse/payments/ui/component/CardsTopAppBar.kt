dd120efb057d81cf9478cd4c0f9247a41ef66a64
yrsel <xxknu88@gmail.com>
2025-09-11 15:44:49 +0900

    refactor: 카드 목록 화면 컴포즈제공 AppBar 활용하는 방식으로 수정

--- app/src/main/java/woowacourse/payments/ui/component/CardsTopAppBar.kt
+++ app/src/main/java/woowacourse/payments/ui/component/CardsTopAppBar.kt
@@ -1,68 +1,58 @@
 package woowacourse.payments.ui.component
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxScope
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import woowacourse.payments.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CardsTopAppBar(
     onRegistrationClick: () -> Unit,
     isVisibleRegistrationButton: Boolean,
     modifier: Modifier = Modifier,
 ) {
-    Box(
-        modifier =
-            modifier
-                .fillMaxWidth()
-                .statusBarsPadding()
-                .height(64.dp),
-    ) {
-        Text(
-            text = stringResource(R.string.app_name),
-            modifier = Modifier.align(Alignment.Center),
-            fontWeight = FontWeight.W400,
-            lineHeight = 28.sp,
-            fontSize = 22.sp,
-            textAlign = TextAlign.Center,
-            maxLines = 1,
-        )
-
-        if (isVisibleRegistrationButton) ShowRegistrationButton(onRegistrationClick)
-    }
-}
-
-@Composable
-private fun BoxScope.ShowRegistrationButton(onRegistrationClick: () -> Unit) {
-    TextButton(
-        onClick = onRegistrationClick,
-        modifier =
-            Modifier
-                .semantics { contentDescription = "카드 목록 앱 바 추가 버튼" }
-                .align(Alignment.CenterEnd),
-    ) {
-        Text(
-            text = stringResource(R.string.cards_top_app_bar_add),
-            lineHeight = 28.sp,
-            fontSize = 18.sp,
-            fontWeight = FontWeight.W700,
-        )
-    }
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(R.string.app_name),
+                fontWeight = FontWeight.W400,
+                color = Color.Black,
+                lineHeight = 28.sp,
+                fontSize = 22.sp,
+                textAlign = TextAlign.Center,
+                maxLines = 1,
+            )
+        },
+        actions = {
+            if (isVisibleRegistrationButton) {
+                TextButton(
+                    onClick = onRegistrationClick,
+                    modifier = Modifier.semantics { contentDescription = "카드 목록 앱 바 추가 버튼" },
+                ) {
+                    Text(
+                        text = stringResource(R.string.cards_top_app_bar_add),
+                        color = Color.Black,
+                        lineHeight = 28.sp,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.W700,
+                    )
+                }
+            }
+        },
+        modifier = modifier,
+    )
 }
 
 @Preview(showBackground = true, name = "메인 화면 앱 바 추가 버튼 비활성화")
