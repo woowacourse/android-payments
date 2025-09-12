88f81028dca140c244f68f8b83d9b103a7f071b3
gahyunkim <mapledt001@naver.com>
2025-09-12 16:53:37 +0900

    refactor: 카드 번호 포맷터 로직 변경 및 TextFormatter 삭제

--- app/src/main/java/woowacourse/payments/ui/text/CardNumberFormatter.kt
+++ app/src/main/java/woowacourse/payments/ui/text/CardNumberFormatter.kt
@@ -5,10 +5,23 @@ import woowacourse.payments.domain.model.CardNumber
 object CardNumberFormatter {
     fun formatAndMask(cardNumber: CardNumber): String {
         val rawValue = cardNumber.value
-        val maskedValue = rawValue.replaceRange(8, 16, "********")
+
+        val maskLength = END_MASK_INDEX - START_MASK_INDEX
+        val dynamicMask = MASK_CHARACTER.repeat(maskLength)
+
+        val maskedValue =
+            rawValue.replaceRange(
+                START_MASK_INDEX,
+                END_MASK_INDEX,
+                dynamicMask,
+            )
         return maskedValue.chunked(CARD_NUMBER_CHUNK_SIZE).joinToString(CARD_NUMBER_SEPARATOR)
     }
 
     private const val CARD_NUMBER_CHUNK_SIZE = 4
     private const val CARD_NUMBER_SEPARATOR = " - "
+
+    private const val START_MASK_INDEX = 8
+    private const val END_MASK_INDEX = 16
+    private const val MASK_CHARACTER = "*"
 }
