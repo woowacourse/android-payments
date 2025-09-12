2161648282e9b57982c91aacd59ba136789ac7a6
hwannow <vnfdlvgid061@naver.com>
2025-09-12 14:24:21 +0900

    refactor: 불필요한 import 제거

--- app/src/main/java/woowacourse/payments/ui/cards/components/CardsTopBar.kt
+++ app/src/main/java/woowacourse/payments/ui/cards/components/CardsTopBar.kt
@@ -1,7 +1,5 @@
 package woowacourse.payments.ui.cards.components
 
-import android.content.Intent
-import androidx.activity.result.ActivityResultLauncher
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -9,14 +7,11 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import woowacourse.payments.R
-import woowacourse.payments.ui.model.PaymentCardUiModel
-import woowacourse.payments.ui.newcard.NewCardActivity
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
