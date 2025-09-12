8bb76609a6f6ad19c5a0b44b8648e636ad25bac7
jerry8282 <ka06196@naver.com>
2025-09-12 14:48:35 +0900

    refactor: CardUiStateHolder를 ViewModel로 변경 및 ktlint 적용

--- app/src/main/java/woowacourse/payments/ui/mapper/Mapper.kt
+++ app/src/main/java/woowacourse/payments/ui/mapper/Mapper.kt
@@ -4,12 +4,11 @@ import woowacourse.payments.domain.PaymentCard
 import woowacourse.payments.ui.model.PaymentCardUiModel
 import java.time.format.DateTimeFormatter
 
-fun PaymentCardUiModel.toDomain(): PaymentCard =
-    PaymentCard(number, expirationDate, cardholderName)
+fun PaymentCardUiModel.toDomain(): PaymentCard = PaymentCard(number, expirationDate, cardholderName)
 
 fun PaymentCard.toUiModel(): PaymentCardUiModel =
     PaymentCardUiModel(
         number = number.toString(),
         expirationDate = expirationDate.format(DateTimeFormatter.ofPattern("MMyy")),
-        cardholderName = cardholderName
+        cardholderName = cardholderName,
     )
