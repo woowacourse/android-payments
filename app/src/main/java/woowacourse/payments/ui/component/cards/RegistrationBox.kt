f71dabc26e0ea18c4fec2a02f52a1814e7698686
yrsel <xxknu88@gmail.com>
2025-09-12 15:20:51 +0900

    test: 카드 목록 화면 테스트

--- app/src/main/java/woowacourse/payments/ui/component/cards/RegistrationBox.kt
+++ app/src/main/java/woowacourse/payments/ui/component/cards/RegistrationBox.kt
@@ -12,6 +12,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import woowacourse.payments.R
@@ -22,9 +24,12 @@ fun RegistrationBox(
     onClickRegistration: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    val registrationBoxAreaDescription =
+        stringResource(R.string.registration_box_registration_box_description)
     Box(
         modifier =
             modifier
+                .semantics { contentDescription = registrationBoxAreaDescription }
                 .size(width = 208.dp, height = 124.dp)
                 .background(Color.LightGray)
                 .clickable { onClickRegistration() },
