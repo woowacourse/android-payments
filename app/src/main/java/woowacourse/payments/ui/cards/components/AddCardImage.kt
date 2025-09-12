2f677aeef54c94086b5904c5fe823259c1234a90
hwannow <vnfdlvgid061@naver.com>
2025-09-12 09:53:57 +0900

    feat: AddCardImage에서 Image를 Icon으로 변경

--- app/src/main/java/woowacourse/payments/ui/cards/components/AddCardImage.kt
+++ app/src/main/java/woowacourse/payments/ui/cards/components/AddCardImage.kt
@@ -1,6 +1,5 @@
 package woowacourse.payments.ui.cards.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -8,6 +7,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -36,7 +36,7 @@ fun AddCardImage(
                     onClick()
                 },
     ) {
-        Image(
+        Icon(
             imageVector = Icons.Default.Add,
             contentDescription = stringResource(R.string.cards_add_card_description),
         )
