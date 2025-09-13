bf81ceb8156a762ce2a42fb8e8b325607cd97602
지원 <cheerfulboys12@gmail.com>
2025-09-13 20:51:54 +0900

    refactor: `Card` 데이터 클래스 Parcelable 제거

--- app/src/main/java/woowacourse/payments/domain/Card.kt
+++ app/src/main/java/woowacourse/payments/domain/Card.kt
@@ -1,12 +1,8 @@
 package woowacourse.payments.domain
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
 data class Card(
     val number: String,
     val expiry: String,
     val password: String,
     val name: String? = null,
-) : Parcelable
+)
