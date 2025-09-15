c2b1e6af1e7503e676bc3148d5010dcb08bdf823
chanho0908 <uni070@naver.com>
2025-09-15 16:44:14 +0900

    feat: 카드사별 색상 추가

--- app/src/main/java/woowacourse/payments/ui/theme/Color.kt
+++ app/src/main/java/woowacourse/payments/ui/theme/Color.kt
@@ -17,3 +17,13 @@ val GrayE5 = Color(0xFFE5E5E5)
 
 val Black49 = Color(0xFF49454F)
 val Black33 = Color(0xFF333333)
+val Black52 = Color(0xFF525252)
+
+val SignatureBcRed = Color(0xFFE04C4C)
+val SignatureShinhanBlue = Color(0xFF1E4BF5)
+val SignatureKakaoYellow = Color(0xFFFEE500)
+val SignatureHyundaiBlack = Color(0xFF000000)
+val SignatureWooriBlueWhite = Color(0xFFC6D0F6)
+val SignatureLotteRed = Color(0xFFE53935)
+val SignatureHanaTeal = Color(0xFF3AAFA9)
+val SignatureKbBrown = Color(0xFF7A6C5A)
