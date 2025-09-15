8450331fea23bf6fda0edee1035dcbe4b3c01d9b
medAndro <med4ndro@gmail.com>
2025-09-15 16:48:13 +0900

    test: 카드사 아이템 UI 테스트 추가

---
+++ app/src/androidTest/java/woowacourse/payments/ui/features/addcard/components/bottomsheet/BottomSheetCardCompanyItemTest.kt
@@ -0,0 +1,42 @@
+package woowacourse.payments.ui.features.addcard.components.bottomsheet
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Rule
+import org.junit.Test
+import woowacourse.payments.R
+import woowacourse.payments.ui.model.CardCompanyUiModel
+import woowacourse.payments.ui.theme.AndroidpaymentsTheme
+
+class BottomSheetCardCompanyItemTest {
+    @get:Rule
+    val compose = createComposeRule()
+
+    @Test
+    fun 카드사_아이템에_아이콘_이미지와_카드사_명이_존재한다() {
+        // given
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+
+        val kakaoUiModel = CardCompanyUiModel.KAKAO
+        compose.setContent {
+            AndroidpaymentsTheme(dynamicColor = false) {
+                BottomSheetCardCompanyItem(
+                    value = kakaoUiModel,
+                )
+            }
+        }
+
+        // then
+        compose
+            .onNodeWithContentDescription(
+                context.getString(
+                    R.string.add_card_bottom_sheet_card_company_icon_description,
+                    kakaoUiModel.companyNameResId,
+                ),
+            ).assertIsDisplayed()
+        compose.onNodeWithText(context.getString(kakaoUiModel.companyNameResId)).assertIsDisplayed()
+    }
+}
