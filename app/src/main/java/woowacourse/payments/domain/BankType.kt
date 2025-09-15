35c60b3adef1002362d00391ea10940e386a140e
Ham BeomJoon <hbj0802@naver.com>
2025-09-15 17:55:17 +0900

    feat: 카드사별 Enum Class에 displayName 추가

--- app/src/main/java/woowacourse/payments/domain/BankType.kt
+++ app/src/main/java/woowacourse/payments/domain/BankType.kt
@@ -1,13 +1,15 @@
 package woowacourse.payments.domain
 
-enum class BankType {
-    NOT_SELECTED,
-    BC,
-    SHINHAN,
-    KAKAOBANK,
-    HYUNDAI,
-    WOORI,
-    LOTTE,
-    HANA,
-    KB,
+enum class BankType(
+    val displayName: String,
+) {
+    BC("BC카드"),
+    SHINHAN("신한카드"),
+    KAKAOBANK("카카오뱅크"),
+    HYUNDAI("현대카드"),
+    WOORI("우리카드"),
+    LOTTE("롯데카드"),
+    HANA("하나카드"),
+    KB("국민카드"),
+    NOT_SELECTED(""),
 }
