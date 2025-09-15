2bc3322935e115864db842cb40f6f196bd90e247
DoableTuple <doabletuple@gmail.com>
2025-09-15 22:23:49 +0900

    feat: CardCompanyUiModel 추가

---
+++ app/src/main/java/woowacourse/payments/ui/model/CardCompanies.kt
@@ -0,0 +1,14 @@
+package woowacourse.payments.ui.model
+
+import woowacourse.payments.R
+
+object CardCompanies {
+    val bcCard = CardCompanyUiModel("BC카드", R.drawable.icon_bc_card)
+    val shinhanCard = CardCompanyUiModel("신한카드", R.drawable.icon_shinhan_card)
+    val kakaoBank = CardCompanyUiModel("카카오뱅크", R.drawable.icon_kakao_bank)
+    val hyundaiCard = CardCompanyUiModel("현대카드", R.drawable.icon_hyundai_card)
+    val wooriCard = CardCompanyUiModel("우리카드", R.drawable.icon_woori_card)
+    val lotteCard = CardCompanyUiModel("롯데카드", R.drawable.icon_lotte_card)
+    val hanaCard = CardCompanyUiModel("하나카드", R.drawable.icon_hana_card)
+    val kbCard = CardCompanyUiModel("국민카드", R.drawable.icon_kb_card)
+}
---
+++ app/src/main/java/woowacourse/payments/ui/model/CardCompanyUiModel.kt
@@ -0,0 +1,8 @@
+package woowacourse.payments.ui.model
+
+import androidx.annotation.DrawableRes
+
+data class CardCompanyUiModel(
+    val name: String,
+    @DrawableRes val logoId: Int,
+)
