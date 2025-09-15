98be612a121dcf967cc29276d94e5672a89713ae
DoableTuple <doabletuple@gmail.com>
2025-09-16 00:57:21 +0900

    refactor: CardCompanyuiModel에 Parcelize 적용

--- app/src/main/java/woowacourse/payments/ui/model/CardCompanyUiModel.kt
+++ app/src/main/java/woowacourse/payments/ui/model/CardCompanyUiModel.kt
@@ -1,15 +1,22 @@
 package woowacourse.payments.ui.model
 
+import android.os.Parcelable
 import androidx.annotation.DrawableRes
-import androidx.compose.ui.graphics.Color
+import kotlinx.parcelize.Parcelize
 import woowacourse.payments.R
 import woowacourse.payments.domain.CardCompany
 
+@Parcelize
 data class CardCompanyUiModel(
     val name: String,
-    @DrawableRes val logoId: Int,
-    val cardColor: Color,
-)
+    @DrawableRes val logo: Int,
+    val cardColor: Long,
+) : Parcelable
+
+fun CardCompanyUiModel.toCardCompanyOrNull(): CardCompany? =
+    CardCompany.entries.find { cardCompany: CardCompany ->
+        cardCompany.companyName == name
+    }
 
 fun CardCompany.toUiModel(): CardCompanyUiModel =
     when (this) {
@@ -17,55 +24,55 @@ fun CardCompany.toUiModel(): CardCompanyUiModel =
             CardCompanyUiModel(
                 companyName,
                 R.drawable.icon_bc_card,
-                Color(0xFFF04651),
+                0xFFF04651,
             )
 
         CardCompany.SHINHAN_CARD ->
             CardCompanyUiModel(
                 companyName,
                 R.drawable.icon_shinhan_card,
-                Color(0xFF0046FF),
+                0xFF0046FF,
             )
 
         CardCompany.KAKAO_BANK ->
             CardCompanyUiModel(
                 companyName,
                 R.drawable.icon_kakao_bank,
-                Color(0xFFFFE600),
+                0xFFFFE600,
             )
 
         CardCompany.HYUNDAI_CARD ->
             CardCompanyUiModel(
                 companyName,
                 R.drawable.icon_hyundai_card,
-                Color(0xFF000000),
+                0xFF000000,
             )
 
         CardCompany.WOORI_CARD ->
             CardCompanyUiModel(
                 companyName,
                 R.drawable.icon_woori_card,
-                Color(0xFF027BC8),
+                0xFF027BC8,
             )
 
         CardCompany.LOTTE_CARD ->
             CardCompanyUiModel(
                 companyName,
                 R.drawable.icon_lotte_card,
-                Color(0xFFED1C25),
+                0xFFED1C25,
             )
 
         CardCompany.HANA_CARD ->
             CardCompanyUiModel(
                 companyName,
                 R.drawable.icon_hana_card,
-                Color(0xFF019490),
+                0xFF019490,
             )
 
         CardCompany.KB_CARD ->
             CardCompanyUiModel(
                 companyName,
                 R.drawable.icon_kb_card,
-                Color(0xFF554E45),
+                0xFF554E45,
             )
     }
