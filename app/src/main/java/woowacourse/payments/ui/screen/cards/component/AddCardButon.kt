4308cbb4a7e65c227080ddc94fb2fac8b28b37f3
cucumber99 <dlsguq1@gmail.com>
2025-09-15 19:27:16 +0900

    refactor: PaymentCard를 사용하지 않고 카드 추가 버튼 구현

--- app/src/main/java/woowacourse/payments/ui/screen/cards/component/AddCardButon.kt
+++ app/src/main/java/woowacourse/payments/ui/screen/cards/component/AddCardButon.kt
@@ -1,19 +1,22 @@
 package woowacourse.payments.ui.screen.cards.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import woowacourse.payments.R
-import woowacourse.payments.ui.screen.cardAddition.component.PaymentCard
 import woowacourse.payments.ui.theme.Gray80
 
 @Composable
@@ -21,22 +24,28 @@ fun AddCardButton(
     modifier: Modifier = Modifier,
     onClick: () -> Unit = {},
 ) {
-    PaymentCard(
-        modifier = modifier.clickable(onClick = onClick),
-        backgroundColor = Gray80,
-        cardContent = {
-            Icon(
-                imageVector = Icons.Default.Add,
-                contentDescription =
-                    stringResource(R.string.cards_card_addition_button_description),
-                modifier =
-                    Modifier
-                        .size(36.dp)
-                        .align(Alignment.Center),
-                tint = Color.Black,
-            )
-        },
-    )
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier =
+            modifier
+                .clickable(onClick = onClick)
+                .shadow(8.dp)
+                .size(width = 208.dp, height = 124.dp)
+                .background(
+                    color = Gray80,
+                    shape = RoundedCornerShape(5.dp),
+                ),
+    ) {
+        Icon(
+            imageVector = Icons.Default.Add,
+            contentDescription = stringResource(R.string.cards_card_addition_button_description),
+            modifier =
+                Modifier
+                    .size(36.dp)
+                    .align(Alignment.Center),
+            tint = Color.Black,
+        )
+    }
 }
 
 @Preview
