80b171dae507f0562348f97fa5361f2397338a3a
지원 <cheerfulboys12@gmail.com>
2025-09-15 23:51:22 +0900

    feat: 카드사 도메인 추가

--- app/src/main/java/woowacourse/payments/domain/Card.kt
+++ app/src/main/java/woowacourse/payments/domain/Card.kt
@@ -1,6 +1,7 @@
 package woowacourse.payments.domain
 
 data class Card(
+    val company: CardCompany = CardCompany.NOT_SELECTED,
     val number: CardNumber,
     val expiry: CardExpiry,
     val password: CardPassword,
