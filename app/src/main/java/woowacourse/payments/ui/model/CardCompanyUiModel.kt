ba1455b1a4da5e1b63cf31f49cc62677cc4b9a64
DoableTuple <doabletuple@gmail.com>
2025-09-16 00:00:16 +0900

    feat: CardCompany -> CardCompanyUiModel 매핑 로직 추가

--- app/src/main/java/woowacourse/payments/ui/model/CardCompanyUiModel.kt
+++ app/src/main/java/woowacourse/payments/ui/model/CardCompanyUiModel.kt
@@ -1,8 +1,23 @@
 package woowacourse.payments.ui.model
 
 import androidx.annotation.DrawableRes
+import woowacourse.payments.R
+import woowacourse.payments.domain.CardCompany
 
 data class CardCompanyUiModel(
     val name: String,
     @DrawableRes val logoId: Int,
 )
+
+fun CardCompany.toUiModelOrNull(): CardCompanyUiModel? =
+    when (name) {
+        "BC카드" -> CardCompanyUiModel(name, R.drawable.icon_bc_card)
+        "신한카드" -> CardCompanyUiModel(name, R.drawable.icon_shinhan_card)
+        "카카오뱅크" -> CardCompanyUiModel(name, R.drawable.icon_kakao_bank)
+        "현대카드" -> CardCompanyUiModel(name, R.drawable.icon_hyundai_card)
+        "우리카드" -> CardCompanyUiModel(name, R.drawable.icon_woori_card)
+        "롯데카드" -> CardCompanyUiModel(name, R.drawable.icon_lotte_card)
+        "하나카드" -> CardCompanyUiModel(name, R.drawable.icon_hana_card)
+        "국민카드" -> CardCompanyUiModel(name, R.drawable.icon_kb_card)
+        else -> null
+    }
