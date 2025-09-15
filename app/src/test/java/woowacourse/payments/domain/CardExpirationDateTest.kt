090a67423d20481f58832c6a20c3c0456e28f3b2
jiyunlee <02leejiyun@naver.com>
2025-09-15 18:41:38 +0900

    test: 카드 만료일 테스트 리팩터링

--- app/src/test/java/woowacourse/payments/domain/CardExpirationDateTest.kt
+++ app/src/test/java/woowacourse/payments/domain/CardExpirationDateTest.kt
@@ -10,42 +10,6 @@ class CardExpirationDateTest {
     private val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("MMyy")
 
     @Test
-    fun `과거 날짜일 경우 만료되었다`() {
-        // given
-        val pastDate = CardExpirationDate(YearMonth.of(2020, 12))
-
-        // when
-        val result = pastDate.isExpired(YearMonth.of(2025, 9))
-
-        // then
-        result shouldBe true
-    }
-
-    @Test
-    fun `미래 날짜일 경우 만료되지 않았다`() {
-        // given
-        val futureDate = CardExpirationDate(YearMonth.of(2030, 1))
-
-        // when
-        val result = futureDate.isExpired(YearMonth.of(2025, 9))
-
-        // then
-        result shouldBe false
-    }
-
-    @Test
-    fun `현재 날짜와 같을 경우 만료되지 않았다`() {
-        // given
-        val currentDate = CardExpirationDate(YearMonth.of(2025, 9))
-
-        // when
-        val result = currentDate.isExpired(YearMonth.of(2025, 9))
-
-        // then
-        result shouldBe false
-    }
-
-    @Test
     fun `올바른 날짜이면 정상적으로 변환된다`() {
         // given
         val dateString = "1225"
@@ -66,5 +30,44 @@ class CardExpirationDateTest {
         shouldThrowAny {
             CardExpirationDate.from(dateString, formatter)
         }
+    }
+
+    @Test
+    fun `과거 날짜일 경우 만료되었다`() {
+        // given
+        val dateString = "1220"
+        val pastDate = CardExpirationDate.from(dateString, formatter)
+
+        // when
+        val result = pastDate.isExpired(YearMonth.of(2025, 9))
+
+        // then
+        result shouldBe true
+    }
+
+    @Test
+    fun `미래 날짜일 경우 만료되지 않았다`() {
+        // given
+        val dateString = "0130"
+        val futureDate = CardExpirationDate.from(dateString, formatter)
+
+        // when
+        val result = futureDate.isExpired(YearMonth.of(2025, 9))
+
+        // then
+        result shouldBe false
+    }
+
+    @Test
+    fun `현재 날짜와 같을 경우 만료되지 않았다`() {
+        // given
+        val dateString = "0925"
+        val currentDate = CardExpirationDate.from(dateString, formatter)
+
+        // when
+        val result = currentDate.isExpired(YearMonth.of(2025, 9))
+
+        // then
+        result shouldBe false
     }
 }
