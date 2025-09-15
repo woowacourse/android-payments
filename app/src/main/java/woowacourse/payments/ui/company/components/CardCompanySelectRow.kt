0725967f359adebece7ea7900101a1ffd86854b2
jiyunlee <02leejiyun@naver.com>
2025-09-15 16:07:18 +0900

    feat: 카드사 선택 목록 컴포넌트 구현

--- app/src/main/java/woowacourse/payments/ui/company/components/CardCompanySelectRow.kt
+++ app/src/main/java/woowacourse/payments/ui/company/components/CardCompanySelectRow.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
@@ -23,8 +25,31 @@ import woowacourse.payments.domain.CardCompany
 import woowacourse.payments.ui.company.model.CompanyUiModel
 import woowacourse.payments.ui.company.model.toUiModel
 
+private const val COLUMN_COUNT = 4
+
 @Composable
-fun CompanySelectButton(
+fun CompanySelectRow(
+    companies: List<CompanyUiModel>,
+    onCompanySelected: (CompanyUiModel) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    FlowRow(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalArrangement = Arrangement.spacedBy(20.dp),
+        maxItemsInEachRow = COLUMN_COUNT,
+    ) {
+        companies.forEach { company: CompanyUiModel ->
+            CardCompanyItem(
+                company = company,
+                onClick = onCompanySelected,
+            )
+        }
+    }
+}
+
+@Composable
+private fun CardCompanyItem(
     company: CompanyUiModel,
     onClick: (CompanyUiModel) -> Unit,
     modifier: Modifier = Modifier,
@@ -53,11 +78,11 @@ fun CompanySelectButton(
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
-private fun CompanySelectButtonPreview() {
-    CompanySelectButton(
-        company = CardCompany.KAKAO.toUiModel(),
-        onClick = {},
+private fun CompanySelectRowPreview() {
+    CompanySelectRow(
+        companies = CardCompany.entries.map { it.toUiModel() },
+        onCompanySelected = {},
     )
 }
