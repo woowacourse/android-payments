d100defc7b454e99f5d8d971d4e4ad1428afdb43
cucumber99 <dlsguq1@gmail.com>
2025-09-16 16:14:50 +0900

    refactor: 불필요한 import 제거

--- app/src/main/java/woowacourse/payments/ui/screen/cardAddition/component/PaymentCard.kt
+++ app/src/main/java/woowacourse/payments/ui/screen/cardAddition/component/PaymentCard.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
