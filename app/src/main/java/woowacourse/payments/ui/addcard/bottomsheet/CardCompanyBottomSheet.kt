284fc0eee24d57f1b834bdc53a8193e63101201c
DoableTuple <doabletuple@gmail.com>
2025-09-16 09:58:19 +0900

    refactor: 카드사 선택 시 바텀 시트가 내려가는 애니메이션이 표시되도록 변경

--- app/src/main/java/woowacourse/payments/ui/addcard/bottomsheet/CardCompanyBottomSheet.kt
+++ app/src/main/java/woowacourse/payments/ui/addcard/bottomsheet/CardCompanyBottomSheet.kt
@@ -6,15 +6,19 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetState
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import woowacourse.payments.domain.CardCompany
 import woowacourse.payments.ui.model.CardCompanyUiModel
 import woowacourse.payments.ui.model.toUiModel
@@ -28,11 +32,13 @@ fun CardCompanyBottomSheet(
     modifier: Modifier = Modifier,
 ) {
     val showBottomSheet: MutableState<Boolean> = remember { mutableStateOf(true) }
+    val sheetState: SheetState = rememberModalBottomSheetState { false }
+    val coroutineScope: CoroutineScope = rememberCoroutineScope()
 
     if (showBottomSheet.value) {
         ModalBottomSheet(
             modifier = modifier,
-            sheetState = rememberModalBottomSheetState { false },
+            sheetState = sheetState,
             onDismissRequest = onDismissed,
         ) {
             FlowRow(
@@ -49,7 +55,12 @@ fun CardCompanyBottomSheet(
                         company,
                         {
                             onCompanySelected(company)
-                            showBottomSheet.value = false
+                            coroutineScope
+                                .launch {
+                                    sheetState.hide()
+                                }.invokeOnCompletion {
+                                    showBottomSheet.value = false
+                                }
                         },
                     )
                 }
