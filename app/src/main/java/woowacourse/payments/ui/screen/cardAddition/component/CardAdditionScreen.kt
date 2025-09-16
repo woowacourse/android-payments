6f26fa999c06d111c8ddbbd0e649f0ad2654a1b5
cucumber99 <dlsguq1@gmail.com>
2025-09-16 10:49:48 +0900

    feat: 카드 정보 입력창 Content Description 추가

--- app/src/main/java/woowacourse/payments/ui/screen/cardAddition/component/CardAdditionScreen.kt
+++ app/src/main/java/woowacourse/payments/ui/screen/cardAddition/component/CardAdditionScreen.kt
@@ -13,8 +13,11 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import woowacourse.payments.R
@@ -33,6 +36,7 @@ fun CardAdditionScreen(
         rememberSaveable(saver = CardAdditionUiStateHolder.Saver) { CardAdditionUiStateHolder() }
     val isCompletable = remember { derivedStateOf { stateHolder.uiState.isValidCard } }
     val focusManager = LocalFocusManager.current
+    val context = LocalContext.current
 
     Scaffold(
         modifier = modifier,
@@ -44,12 +48,24 @@ fun CardAdditionScreen(
             )
         },
     ) { paddingValues: PaddingValues ->
+        if (!stateHolder.hasShownSheet) {
+            BankSelectBottomSheet(
+                onBankSelected = { issuingBank ->
+                    stateHolder.updateIssuingBank(issuingBank)
+                },
+                onDismissRequest = { stateHolder.updateSheetVisible() },
+            )
+        }
         Column(
             modifier =
                 Modifier
                     .fillMaxSize()
                     .padding(paddingValues)
-                    .padding(horizontal = 24.dp),
+                    .padding(horizontal = 24.dp)
+                    .semantics {
+                        contentDescription =
+                            context.getString(R.string.card_addition_fields_description)
+                    },
         ) {
             PaymentCard(
                 modifier =
@@ -114,14 +130,6 @@ fun CardAdditionScreen(
                 onComplete = { focusManager.clearFocus() },
                 onKeyboardActionClick = { focusManager.clearFocus() },
             )
-            if (!stateHolder.hasShownSheet) {
-                BankSelectBottomSheet(
-                    onBankSelected = { issuingBank ->
-                        stateHolder.updateIssuingBank(issuingBank)
-                    },
-                    onDismissRequest = { stateHolder.updateSheetVisible() },
-                )
-            }
         }
     }
 }
