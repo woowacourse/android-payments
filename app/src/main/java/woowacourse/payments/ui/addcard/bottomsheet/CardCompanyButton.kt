085ca3d88bb5f0ae8ecef4187de7a473cf0220be
DoableTuple <doabletuple@gmail.com>
2025-09-16 23:09:24 +0900

    refactor: contentDescription 수정

--- app/src/main/java/woowacourse/payments/ui/addcard/bottomsheet/CardCompanyButton.kt
+++ app/src/main/java/woowacourse/payments/ui/addcard/bottomsheet/CardCompanyButton.kt
@@ -12,8 +12,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import woowacourse.payments.R
 import woowacourse.payments.domain.CardCompany
 import woowacourse.payments.ui.model.CardCompanyUiModel
 import woowacourse.payments.ui.model.toUiModel
@@ -34,7 +36,7 @@ fun CardCompanyButton(
     ) {
         Image(
             painter = painterResource(company.logo),
-            contentDescription = "",
+            contentDescription = stringResource(R.string.add_card_company_icon_description),
             modifier = Modifier.size(37.dp),
         )
         Spacer(Modifier.height(9.dp))
