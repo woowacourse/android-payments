e00cb3c226b7180beefbd6650e275be719cf3c8c
moondev03 <moondev03@gmail.com>
2025-09-16 16:22:40 +0900

    feat: 카드사 선택 바텀시트 UI 구현
    - 카드사 로고와 이름을 표시하는 `BankSelectRow` 컴포저블 추가
    - `BankSelectRow`를 포함하고 `ModalBottomSheet`를 사용하여 은행 선택 UI를 제공하는 `BankSelectBottomSheet` 컴포저블 추가

---
+++ app/src/main/java/woowacourse/payments/ui/component/BankSelectorBottomSheet.kt
@@ -0,0 +1,50 @@
+package woowacourse.payments.ui.component
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import woowacourse.payments.ui.model.BankTypeUiModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BankSelectBottomSheet(
+    onBankSelected: (BankTypeUiModel) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val sheetState = rememberModalBottomSheetState(confirmValueChange = { false })
+    var selectedBank by remember { mutableStateOf(BankTypeUiModel.NOT_SELECTED) }
+
+    LaunchedEffect(selectedBank) {
+        if (selectedBank != BankTypeUiModel.NOT_SELECTED) sheetState.hide()
+    }
+
+    ModalBottomSheet(
+        modifier = modifier,
+        sheetState = sheetState,
+        onDismissRequest = {},
+        containerColor = Color.White,
+    ) {
+        BankSelectRow(
+            onBankSelected = { bankType ->
+                selectedBank = bankType
+                onBankSelected(bankType)
+            },
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun BankSelectBottomSheetPreview() {
+    BankSelectBottomSheet(onBankSelected = { })
+}
