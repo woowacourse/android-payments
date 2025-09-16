9bdbd30bb323b90b72adb1e0e3f54f07c4051eb9
jerry8282 <ka06196@naver.com>
2025-09-16 17:15:46 +0900

    feat: 은행 선택 UI 컴포넌트 구현

---
+++ app/src/main/java/woowacourse/payments/ui/payments/component/BankSelectRow.kt
@@ -0,0 +1,55 @@
+package woowacourse.payments.ui.payments.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import woowacourse.payments.ui.payments.model.BankUiModel
+import woowacourse.payments.ui.theme.AndroidpaymentsTheme
+
+private const val COLUMN_COUNT = 4
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun BankSelectRow(
+    onClick: (BankUiModel) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    FlowRow(
+        modifier =
+            modifier
+                .padding(4.dp)
+                .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalArrangement = Arrangement.spacedBy(23.dp),
+        maxItemsInEachRow = COLUMN_COUNT,
+    ) {
+        val selectableBanks = BankUiModel.entries.filter { it != BankUiModel.NOT_SELECTED }
+
+        selectableBanks.forEach { bankUiModel ->
+            BankButton(
+                bankUiModel = bankUiModel,
+                modifier = Modifier
+                    .clickable { onClick(bankUiModel) }
+                    .weight(1f),
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PreViewSomeThing() {
+    AndroidpaymentsTheme {
+        Scaffold { innerPadding ->
+            BankSelectRow({}, modifier = Modifier.padding(innerPadding))
+        }
+    }
+}
