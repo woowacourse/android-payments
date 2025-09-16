2ae43a69e52499781b5dc7bf61983bff088930c2
Ham BeomJoon <hbj0802@naver.com>
2025-09-16 10:55:04 +0900

    refactor: BankType enum에서 displayName 속성 제거
    `BankType` enum 클래스에서 각 은행 타입의 표시 이름을 나타내는 `displayName` 속성을 제거했습니다.

--- app/src/main/java/woowacourse/payments/domain/BankType.kt
+++ app/src/main/java/woowacourse/payments/domain/BankType.kt
@@ -1,15 +1,13 @@
 package woowacourse.payments.domain
 
-enum class BankType(
-    val displayName: String,
-) {
-    BC("BC카드"),
-    SHINHAN("신한카드"),
-    KAKAOBANK("카카오뱅크"),
-    HYUNDAI("현대카드"),
-    WOORI("우리카드"),
-    LOTTE("롯데카드"),
-    HANA("하나카드"),
-    KB("국민카드"),
-    NOT_SELECTED(""),
+enum class BankType {
+    BC,
+    SHINHAN,
+    KAKAOBANK,
+    HYUNDAI,
+    WOORI,
+    LOTTE,
+    HANA,
+    KB,
+    NOT_SELECTED,
 }
