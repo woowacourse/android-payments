d4a177ccfbbc58296740fd8ecfb1892f986755b6
Ham BeomJoon <hbj0802@naver.com>
2025-09-16 10:56:31 +0900

    feat: AddCardScreen에 은행 선택 Bottom Sheet 추가

--- app/src/main/java/woowacourse/payments/ui/screen/addCard/AddCardScreen.kt
+++ app/src/main/java/woowacourse/payments/ui/screen/addCard/AddCardScreen.kt
@@ -10,11 +10,17 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import woowacourse.payments.domain.BankType
+import woowacourse.payments.ui.component.BankSelectBottomSheet
 import woowacourse.payments.ui.component.CardNumberInputField
 import woowacourse.payments.ui.component.CardOwnerInputField
 import woowacourse.payments.ui.component.ExpiredInputField
@@ -23,6 +29,7 @@ import woowacourse.payments.ui.component.PasswordInputField
 import woowacourse.payments.ui.component.PaymentCard
 import woowacourse.payments.ui.model.CardUiModel
 import woowacourse.payments.ui.theme.AndroidpaymentsTheme
+import woowacourse.payments.ui.util.toCardCompanyUiModel
 
 @Composable
 fun AddCardScreen(
@@ -32,6 +39,8 @@ fun AddCardScreen(
     val stateHolder =
         rememberSaveable(saver = AddCardStateHolder.saver) { AddCardStateHolder() }
     val scrollState = rememberScrollState()
+    var isBankSheetVisible by remember { mutableStateOf(true) }
+    var selectedBank by remember { mutableStateOf(BankType.NOT_SELECTED) }
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -94,6 +103,21 @@ fun AddCardScreen(
             )
         }
     }
+
+    BankSelectBottomSheet(
+        isVisible = isBankSheetVisible,
+        selectedBank = selectedBank,
+        onBankSelected = { bank ->
+            selectedBank = bank
+            isBankSheetVisible = false
+
+            val cardCompany = bank.toCardCompanyUiModel()
+            stateHolder.updateCardCompany(cardCompany)
+        },
+        onDismiss = {
+            isBankSheetVisible = false
+        },
+    )
 }
 
 @Composable
