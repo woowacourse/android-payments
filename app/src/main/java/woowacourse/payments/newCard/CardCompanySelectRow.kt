0a46e723bc283094bbd617a56d12e622e34f8242
지원 <cheerfulboys12@gmail.com>
2025-09-17 13:23:37 +0900

    refactor: 프리뷰 함수 이름 수정

--- app/src/main/java/woowacourse/payments/newCard/CardCompanySelectRow.kt
+++ app/src/main/java/woowacourse/payments/newCard/CardCompanySelectRow.kt
@@ -39,7 +39,7 @@ fun CardCompanySelectRow(
 
 @Preview
 @Composable
-private fun CardCompanySelectRow() {
+private fun CardCompanySelectRowPreview() {
     AndroidpaymentsTheme {
         CardCompanySelectRow(
             onClick = {}
