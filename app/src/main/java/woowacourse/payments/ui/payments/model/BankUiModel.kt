81e63f72d6f18c21001fff15b088fd6d164f46f0
jerry8282 <ka06196@naver.com>
2025-09-17 16:31:16 +0900

    feat: 카드사 색상 변경

--- app/src/main/java/woowacourse/payments/ui/payments/model/BankUiModel.kt
+++ app/src/main/java/woowacourse/payments/ui/payments/model/BankUiModel.kt
@@ -15,15 +15,23 @@ enum class BankUiModel(
 ) : Parcelable {
     NOT_SELECTED(
         R.string.bank_ui_model_not_selected_bank_name,
-        R.color.black,
+        R.color.no_selected_color,
         R.drawable.ic_launcher_background,
     ),
-    BC(R.string.bank_ui_model_bc_bank_name, R.color.teal_200, R.drawable.ic_bc_bank),
-    SHINHAN(R.string.bank_ui_model_shinhan_bank_name, R.color.teal_200, R.drawable.ic_shinhan_bank),
-    KAKAO(R.string.bank_ui_model_kakao_bank_name, R.color.teal_200, R.drawable.ic_kakao_bank),
-    HYUNDAI(R.string.bank_ui_model_hyundai_bank_name, R.color.teal_200, R.drawable.ic_hyundai_bank),
-    WOORI(R.string.bank_ui_model_woori_bank_name, R.color.teal_200, R.drawable.ic_woori_bank),
-    LOTTE(R.string.bank_ui_model_lotte_bank_name, R.color.teal_200, R.drawable.ic_lotte_bank),
-    HANA(R.string.bank_ui_model_hana_bank_name, R.color.teal_200, R.drawable.ic_hana_bank),
-    KB(R.string.bank_ui_model_kb_bank_name, R.color.teal_200, R.drawable.ic_kb_bank),
+    BC(R.string.bank_ui_model_bc_bank_name, R.color.bc_color, R.drawable.ic_bc_bank),
+    SHINHAN(
+        R.string.bank_ui_model_shinhan_bank_name,
+        R.color.shinhan_color,
+        R.drawable.ic_shinhan_bank,
+    ),
+    KAKAO(R.string.bank_ui_model_kakao_bank_name, R.color.kakao_color, R.drawable.ic_kakao_bank),
+    HYUNDAI(
+        R.string.bank_ui_model_hyundai_bank_name,
+        R.color.hyundai_color,
+        R.drawable.ic_hyundai_bank,
+    ),
+    WOORI(R.string.bank_ui_model_woori_bank_name, R.color.woori_color, R.drawable.ic_woori_bank),
+    LOTTE(R.string.bank_ui_model_lotte_bank_name, R.color.lotte_color, R.drawable.ic_lotte_bank),
+    HANA(R.string.bank_ui_model_hana_bank_name, R.color.hana_color, R.drawable.ic_hana_bank),
+    KB(R.string.bank_ui_model_kb_bank_name, R.color.kb_color, R.drawable.ic_kb_bank),
 }
