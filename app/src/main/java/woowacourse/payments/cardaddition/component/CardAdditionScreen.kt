dce93db307b9a3d1937290eac109a373f048cad8
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-09-17 16:39:21 +0900

    refactor: 불필요한 코드 제거

--- app/src/main/java/woowacourse/payments/cardaddition/component/CardAdditionScreen.kt
+++ app/src/main/java/woowacourse/payments/cardaddition/component/CardAdditionScreen.kt
@@ -7,7 +7,6 @@ import androidx.activity.compose.LocalActivity
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -153,7 +152,6 @@ private fun CardAdditionScreenPreview(
 ) {
     CardAdditionScreen(
         stateHolder = CardAdditionStateHolder(state),
-        modifier = Modifier.fillMaxSize(),
     )
 }
 
