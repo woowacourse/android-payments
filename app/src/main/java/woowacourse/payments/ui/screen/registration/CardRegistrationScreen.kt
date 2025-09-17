59498602ee274b3e212e5a7158d2d0e807fa0d6a
yrsel <xxknu88@gmail.com>
2025-09-17 12:33:14 +0900

    refactor: 카드 등록 화면 수정

--- app/src/main/java/woowacourse/payments/ui/screen/registration/CardRegistrationScreen.kt
+++ app/src/main/java/woowacourse/payments/ui/screen/registration/CardRegistrationScreen.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package woowacourse.payments.ui.screen.registration
 
 import androidx.compose.foundation.layout.Column
@@ -7,8 +9,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -18,11 +24,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import woowacourse.payments.ui.component.PaymentCard
+import woowacourse.payments.ui.component.registration.CardCompanySelectBottomSheet
 import woowacourse.payments.ui.component.registration.CardExpirationDateTextField
 import woowacourse.payments.ui.component.registration.CardNumberTextField
 import woowacourse.payments.ui.component.registration.CardPasswordTextField
 import woowacourse.payments.ui.component.registration.CardRegistrationTopAppBar
 import woowacourse.payments.ui.component.registration.CardholderNameTextField
+import woowacourse.payments.ui.model.CardCompanyUiModel
 import woowacourse.payments.ui.model.CardExpirationDateUiModel
 import woowacourse.payments.ui.model.CardNumberUiModel
 import woowacourse.payments.ui.model.CardPasswordUiModel
@@ -37,6 +45,15 @@ fun CardRegistrationScreen(
     modifier: Modifier = Modifier,
 ) {
     var uiState by rememberSaveable { mutableStateOf(CardRegistrationScreenUiState()) }
+    val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+
+    LaunchedEffect(uiState.isBottomSheetOpen) {
+        if (uiState.isBottomSheetOpen) {
+            sheetState.show()
+        } else {
+            sheetState.hide()
+        }
+    }
 
     Scaffold(
         topBar = {
@@ -47,6 +64,7 @@ fun CardRegistrationScreen(
                 onSaveClick = {
                     onCardRegistered(
                         CardUiModel(
+                            cardCompanyUiModel = uiState.cardCompany,
                             cardholderNameUiModel = uiState.cardholderName,
                             cardNumberUiModel = uiState.cardNumber,
                             cardExpirationDateUiModel = uiState.cardExpirationDate,
@@ -58,8 +76,11 @@ fun CardRegistrationScreen(
         },
     ) { innerPadding ->
         CardRegistrationScreenContent(
-            modifier = modifier.padding(innerPadding),
             uiState = uiState,
+            sheetState = sheetState,
+            onCardClick = {
+                uiState = uiState.copy(isBottomSheetOpen = true)
+            },
             onCardNumberChanged = { newCardNumber ->
                 uiState = uiState.copy(cardNumber = newCardNumber)
             },
@@ -78,6 +99,13 @@ fun CardRegistrationScreen(
             onCardPasswordChanged = { newCardPassword ->
                 uiState = uiState.copy(cardPassword = newCardPassword)
             },
+            onCardCompanySelected = { newCardCompany ->
+                uiState = uiState.copy(isBottomSheetOpen = false, cardCompany = newCardCompany)
+            },
+            onBottomSheetDismissRequest = {
+                uiState = uiState.copy(isBottomSheetOpen = false)
+            },
+            modifier = modifier.padding(innerPadding),
         )
     }
 }
@@ -85,13 +113,29 @@ fun CardRegistrationScreen(
 @Composable
 private fun CardRegistrationScreenContent(
     uiState: CardRegistrationScreenUiState,
+    sheetState: SheetState,
+    onCardClick: () -> Unit,
     onCardNumberChanged: (CardNumberUiModel) -> Unit,
     onCardExpirationDateChanged: (CardExpirationDateUiModel) -> Unit,
     onCardExpirationDateErrorMessageChanged: (String?) -> Unit,
     onCardholderNameChanged: (CardholderNameUiModel) -> Unit,
     onCardPasswordChanged: (CardPasswordUiModel) -> Unit,
+    onCardCompanySelected: (CardCompanyUiModel) -> Unit,
+    onBottomSheetDismissRequest: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    if (uiState.isBottomSheetOpen) {
+        CardCompanySelectBottomSheet(
+            onCardCompanyClick = {
+                onCardCompanySelected(it)
+            },
+            onDismissRequest = {
+                onBottomSheetDismissRequest()
+            },
+            sheetState = sheetState,
+        )
+    }
+
     Column(
         modifier =
             modifier
@@ -101,7 +145,11 @@ private fun CardRegistrationScreenContent(
     ) {
         Spacer(modifier = Modifier.height(16.dp))
 
-        PaymentCard(modifier = Modifier.align(Alignment.CenterHorizontally))
+        PaymentCard(
+            onCardClick = onCardClick,
+            card = uiState.toCardUiModel(),
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+        )
 
         Spacer(modifier = Modifier.height(40.dp))
 
