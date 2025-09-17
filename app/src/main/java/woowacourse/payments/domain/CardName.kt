52710266281981ffb42c0f77935ce6b46bfbe1fc
지원 <cheerfulboys12@gmail.com>
2025-09-17 13:59:28 +0900

    refactor: 카드 소유자 이름 Null 입력 시 Empty String으로 처리하도록 수정

--- app/src/main/java/woowacourse/payments/domain/CardName.kt
+++ app/src/main/java/woowacourse/payments/domain/CardName.kt
@@ -1,17 +1,17 @@
 package woowacourse.payments.domain
 
 @JvmInline
-value class CardName private constructor(val value: String?) {
+value class CardName private constructor(val value: String) {
     init {
-        require(value == null || value.length <= MAXIMUM_LENGTH) { "카드 이름은 최대 ${MAXIMUM_LENGTH}를 초과할 수 없습니다." }
-        require(value == null || value.all { it.isEnglishLetterOrSpace() }) { "카드 이름은 영어와 공백으로만 이루어져 있습니다." }
+        require(value.length <= MAXIMUM_LENGTH) { "카드 이름은 최대 ${MAXIMUM_LENGTH}를 초과할 수 없습니다." }
+        require(value.all { it.isEnglishLetterOrSpace() }) { "카드 이름은 영어와 공백으로만 이루어져 있습니다." }
     }
 
     companion object {
         private const val MAXIMUM_LENGTH = 30
 
         operator fun invoke(raw: String?): CardName {
-            return CardName(raw?.uppercase())
+            return CardName(raw?.uppercase() ?: "")
         }
     }
 }
