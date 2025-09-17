2298a0c16833af7106b01f1a5a9a00d6e81730d0
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-09-17 12:36:34 +0900

    feat: 시스템 뒤로가기로 인한 바텀 시트 닫힘 방지

--- app/src/main/java/woowacourse/payments/cardaddition/component/BankSelectBottomSheet.kt
+++ app/src/main/java/woowacourse/payments/cardaddition/component/BankSelectBottomSheet.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.ModalBottomSheetProperties
 import androidx.compose.material3.SheetState
 import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Text
@@ -41,6 +42,7 @@ fun BankSelectBottomSheet(
         sheetState = modalBottomSheetState,
         onDismissRequest = {},
         modifier = modifier,
+        properties = ModalBottomSheetProperties(shouldDismissOnBackPress = false),
     ) {
         Banks(
             onSelectBank = onSelectBankType,
