fbdc3d0018bc6a37ceba56e234fe96f68bfb06f8
m6z1 <sonmyungj1zz@gmail.com>
2025-09-18 17:34:28 +0900

    refactor: 불필요한 어노테이션 제거

--- app/src/main/java/woowacourse/payments/ui/cardList/components/CardSelectionModal.kt
+++ app/src/main/java/woowacourse/payments/ui/cardList/components/CardSelectionModal.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -56,7 +55,6 @@ fun CardSelectionModal(
     }
 }
 
-@OptIn(ExperimentalLayoutApi::class)
 @Composable
 private fun CardCompanyGrid(
     companies: List<CardCompany>,
