05ecfad727e96640d3e30b528dc24bb5d7cc18d4
DoableTuple <doabletuple@gmail.com>
2025-09-19 15:12:53 +0900

    refactor: 불필요한 Annotation 제거

--- app/src/main/java/woowacourse/payments/ui/addcard/AddCardScreen.kt
+++ app/src/main/java/woowacourse/payments/ui/addcard/AddCardScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -25,7 +24,6 @@ import woowacourse.payments.ui.addcard.textfields.PasscodeTextField
 import woowacourse.payments.ui.common.composable.PaymentCard
 import woowacourse.payments.ui.model.CardUiModel
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddCardScreen(
     uiState: AddCardScreenUiStateHolder,
