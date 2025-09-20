7ca2f81ee7962ee555f34cf30c4be3d1c030470b
gahyunkim <mapledt001@naver.com>
2025-09-20 16:32:05 +0900

    refactor: 카드 추가 화면 관련 불필요한 파일 삭제

--- app/src/main/java/woowacourse/payments/ui/screen/AddCardScreenUiState.kt
+++
@@ -1,9 +0,0 @@
-package woowacourse.payments.ui.screen
-
-import woowacourse.payments.domain.model.CardCompanyType
-
-data class AddCardScreenUiState(
-    val formState: AddCardFormState = AddCardFormState(),
-    val showSheet: Boolean = true,
-    val selectedCardCompanyType: CardCompanyType = CardCompanyType.NOT_SELECTED,
-)
