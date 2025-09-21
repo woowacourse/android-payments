db4abf773c522d76d6942813ef0801b6cba95458
DoableTuple <doabletuple@gmail.com>
2025-09-21 19:19:44 +0900

    refactor: CardUiModel.toCardOrNull() 함수 구현

--- app/src/main/java/woowacourse/payments/ui/cardlist/CardListActivity.kt
+++ app/src/main/java/woowacourse/payments/ui/cardlist/CardListActivity.kt
@@ -11,18 +11,12 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import woowacourse.payments.domain.Card
-import woowacourse.payments.domain.CardNumber
-import woowacourse.payments.domain.CardholderName
-import woowacourse.payments.domain.ExpirationDate
-import woowacourse.payments.domain.Passcode
 import woowacourse.payments.ui.addcard.AddCardActivity
 import woowacourse.payments.ui.common.ExtraKeys
 import woowacourse.payments.ui.common.getParcelableExtraCompat
-import woowacourse.payments.ui.format.ExpirationDateFormat
 import woowacourse.payments.ui.model.CardUiModel
 import woowacourse.payments.ui.model.toUiModel
 import woowacourse.payments.ui.theme.AndroidpaymentsTheme
-import java.time.YearMonth
 
 class CardListActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,18 +41,7 @@ class CardListActivity : ComponentActivity() {
     }
 
     private fun Intent.toCardOrNull(): Card? =
-        runCatching {
-            getParcelableExtraCompat<CardUiModel>(ExtraKeys.CARD_KEY)?.let { card: CardUiModel ->
-                val yearMonth: YearMonth =
-                    YearMonth.parse(card.expirationDate, ExpirationDateFormat.formatPattern)
-
-                Card(
-                    CardNumber(card.cardNumber),
-                    ExpirationDate(yearMonth),
-                    CardholderName(card.cardholderName),
-                    Passcode(card.passcode),
-                    card.cardCompany.company,
-                )
-            }
-        }.getOrNull()
+        getParcelableExtraCompat<CardUiModel>(ExtraKeys.CARD_KEY)?.let { card: CardUiModel ->
+            card.toCardOrNull()
+        }
 }
--- app/src/main/java/woowacourse/payments/ui/model/CardUiModel.kt
+++ app/src/main/java/woowacourse/payments/ui/model/CardUiModel.kt
@@ -4,8 +4,13 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 import woowacourse.payments.domain.Card
 import woowacourse.payments.domain.CardCompany
+import woowacourse.payments.domain.CardNumber
+import woowacourse.payments.domain.CardholderName
+import woowacourse.payments.domain.ExpirationDate
+import woowacourse.payments.domain.Passcode
 import woowacourse.payments.ui.format.CardNumberFormat
 import woowacourse.payments.ui.format.ExpirationDateFormat
+import java.time.YearMonth
 
 @Parcelize
 data class CardUiModel(
@@ -15,6 +20,19 @@ data class CardUiModel(
     val passcode: String,
     val cardCompany: CardCompanyUiModel,
 ) : Parcelable {
+    fun toCardOrNull(): Card? =
+        runCatching {
+            val yearMonth: YearMonth =
+                YearMonth.parse(expirationDate, ExpirationDateFormat.formatPattern)
+            Card(
+                CardNumber(cardNumber),
+                ExpirationDate(yearMonth),
+                CardholderName(cardholderName),
+                Passcode(passcode),
+                cardCompany.company,
+            )
+        }.getOrNull()
+
     companion object {
         val EMPTY = CardUiModel("", "", "", "", CardCompany.NONE.toUiModel())
     }
