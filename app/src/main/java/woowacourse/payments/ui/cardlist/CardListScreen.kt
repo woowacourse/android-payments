3d44d28df2b0b65300e1175adb375ebcee335ef0
DoableTuple <doabletuple@gmail.com>
2025-09-21 22:11:26 +0900

    refactor: LazyColumn 적용

--- app/src/main/java/woowacourse/payments/ui/cardlist/CardListScreen.kt
+++ app/src/main/java/woowacourse/payments/ui/cardlist/CardListScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -54,11 +55,11 @@ fun CardListScreen(
                 )
             }
 
-            Column(
+            LazyColumn(
                 verticalArrangement = Arrangement.spacedBy(36.dp),
             ) {
-                cards.forEach { card: CardUiModel ->
-                    PaymentCard(card = card)
+                items(count = cards.size) { index: Int ->
+                    PaymentCard(card = cards[index])
                 }
             }
 
