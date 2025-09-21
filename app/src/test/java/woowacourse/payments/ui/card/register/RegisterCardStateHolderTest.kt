cde049ba33810e5d53b1f0864f6964444748ace9
Dongjoo Seo <83579348+etama123@users.noreply.github.com>
2025-09-21 16:09:31 +0900

    test: 카드 등록 stateHolder에 세부 테스트 추가
    - 초기 상태 검증
    - 각 필드 업데이트 로직 검증
    - 카드사 선택 여부에 따른 저장 로직 검증

--- app/src/test/java/woowacourse/payments/ui/card/register/RegisterCardStateHolderTest.kt
+++ app/src/test/java/woowacourse/payments/ui/card/register/RegisterCardStateHolderTest.kt
@@ -5,10 +5,75 @@ import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Test
 import woowacourse.payments.domain.Bank
 import woowacourse.payments.domain.BankType
+import kotlin.test.assertTrue
 
 class RegisterCardStateHolderTest {
     @Test
-    fun 카드사선택시_uiState가_업데이트되고_바텀시트가_닫힌다() {
+    fun `초기_상태는_모든_필드가_기본값으로_설정되어_있어야_한다`() {
+        // Given
+        val stateHolder = RegisterCardStateHolder {}
+        val uiState = stateHolder.uiState
+
+        // Then
+        assertEquals("", uiState.cardNumber)
+        assertEquals("", uiState.expirationDate)
+        assertEquals("", uiState.cardHolderName)
+        assertEquals("", uiState.password)
+        assertEquals(null, uiState.selectedBank)
+        assertTrue(uiState.showBottomSheet)
+        assertEquals(null, uiState.toastMessage)
+    }
+
+    @Test
+    fun `updateCardNumber_호출_시_uiState_cardNumber가_업데이트되어야_한다`() {
+        // Given
+        val stateHolder = RegisterCardStateHolder {}
+
+        // When
+        stateHolder.updateCardNumber("1234")
+
+        // Then
+        assertEquals("1234", stateHolder.uiState.cardNumber)
+    }
+
+    @Test
+    fun `updateExpirationDate_호출_시_uiState_expirationDate가_업데이트되어야_한다`() {
+        // Given
+        val stateHolder = RegisterCardStateHolder {}
+
+        // When
+        stateHolder.updateExpirationDate("1124")
+
+        // Then
+        assertEquals("1124", stateHolder.uiState.expirationDate)
+    }
+
+    @Test
+    fun `updateCardHolderName_호출_시_uiState_cardHolderName이_업데이트되어야_한다`() {
+        // Given
+        val stateHolder = RegisterCardStateHolder {}
+
+        // When
+        stateHolder.updateCardHolderName("TAMA")
+
+        // Then
+        assertEquals("TAMA", stateHolder.uiState.cardHolderName)
+    }
+
+    @Test
+    fun `updatePassword_호출_시_uiState_password가_업데이트되어야_한다`() {
+        // Given
+        val stateHolder = RegisterCardStateHolder {}
+
+        // When
+        stateHolder.updatePassword("12")
+
+        // Then
+        assertEquals("12", stateHolder.uiState.password)
+    }
+
+    @Test
+    fun `updateSelectedBank_호출_시_uiState_selectedBank가_업데이트된다`() {
         // Given
         val stateHolder = RegisterCardStateHolder {}
         val bank = Bank(BankType.KB, "국민카드")
@@ -18,11 +83,23 @@ class RegisterCardStateHolderTest {
 
         // Then
         assertEquals(bank, stateHolder.uiState.selectedBank)
+    }
+
+    @Test
+    fun `updateSelectedBank_호출_시_uiState_showBottomSheet가_false로_변경된다`() {
+        // Given
+        val stateHolder = RegisterCardStateHolder {}
+        val bank = Bank(BankType.KB, "국민카드")
+
+        // When
+        stateHolder.updateSelectedBank(bank)
+
+        // Then
         assertFalse(stateHolder.uiState.showBottomSheet)
     }
 
     @Test
-    fun 카드사선택이_없으면_onCardSaved가_호출되지_않을_수_있다() {
+    fun `카드사_선택이_없으면_saveCard_호출_시_onCardSaved가_호출되지_않아야_하고_toastMessage가_설정되어야_한다`() {
         // Given
         var savedCalled = false
         val stateHolder = RegisterCardStateHolder { savedCalled = true }
@@ -32,23 +109,6 @@ class RegisterCardStateHolderTest {
 
         // Then
         assertFalse(savedCalled)
-    }
-
-    @Test
-    fun 입력값에따라_uiState가_업데이트될_수_있다() {
-        // Given
-        val stateHolder = RegisterCardStateHolder {}
-
-        // When
-        stateHolder.updateCardNumber("1234")
-        stateHolder.updateExpirationDate("1124")
-        stateHolder.updateCardHolderName("TAMA")
-        stateHolder.updatePassword("12")
-
-        // Then
-        assertEquals("1234", stateHolder.uiState.cardNumber)
-        assertEquals("1124", stateHolder.uiState.expirationDate)
-        assertEquals("TAMA", stateHolder.uiState.cardHolderName)
-        assertEquals("12", stateHolder.uiState.password)
+        assertEquals("카드 생성에 실패했습니다.", stateHolder.uiState.toastMessage)
     }
 }
