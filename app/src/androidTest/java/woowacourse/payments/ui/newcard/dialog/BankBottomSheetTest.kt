a2baa703496e4763d43dfc4ec277e07659439ee2
hwannow <vnfdlvgid061@naver.com>
2025-09-21 15:45:58 +0900

    test: `BankBottomSheet` 클릭 이벤트 테스트 추가

--- app/src/androidTest/java/woowacourse/payments/ui/newcard/dialog/BankBottomSheetTest.kt
+++ app/src/androidTest/java/woowacourse/payments/ui/newcard/dialog/BankBottomSheetTest.kt
@@ -2,11 +2,14 @@ package woowacourse.payments.ui.newcard.dialog
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import org.junit.Rule
 import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
 import woowacourse.payments.R
 import woowacourse.payments.domain.model.Bank
 import woowacourse.payments.domain.model.BankType
@@ -18,6 +21,7 @@ class BankBottomSheetTest {
     @OptIn(ExperimentalMaterial3Api::class)
     @Test
     fun `은행_목록이_뜬다`() {
+        // given & when
         composeTestRule.setContent {
             BankBottomSheet(
                 sheetState = rememberModalBottomSheetState(),
@@ -27,10 +31,38 @@ class BankBottomSheetTest {
                         Bank(BankType.KB, R.drawable.ic_kb),
                     ),
                 onClick = {},
+                onDismiss = {},
             )
         }
 
+        // then
         composeTestRule.onNodeWithText("신한카드").assertIsDisplayed()
         composeTestRule.onNodeWithText("국민카드").assertIsDisplayed()
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Test
+    fun `은행을_클릭하면_onClick함수가_실행된다`() {
+        // given
+        val clickedBank = mutableStateOf<BankType?>(null)
+
+        composeTestRule.setContent {
+            BankBottomSheet(
+                sheetState = rememberModalBottomSheetState(),
+                banks =
+                    listOf(
+                        Bank(BankType.SHINHAN, R.drawable.ic_shinhan),
+                        Bank(BankType.KB, R.drawable.ic_kb),
+                    ),
+                onClick = { bank -> clickedBank.value = bank.type },
+                onDismiss = {},
+            )
+        }
+
+        // when
+        composeTestRule.onNodeWithText("신한카드").performClick()
+
+        // then
+        assertEquals(BankType.SHINHAN, clickedBank.value)
     }
 }
