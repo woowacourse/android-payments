3384365ef1d49faace1078c2c52b3889a7a91c37
devfeijoa <devfeijoa@gmail.com>
2025-09-21 20:58:13 +0900

    feat: 카드 회사 선택 바텀 시트 구현

---
+++ app/src/main/java/woowacourse/payments/ui/component/CardCompanySelectBottomSheet.kt
@@ -0,0 +1,48 @@
+package woowacourse.payments.ui.component
+
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import woowacourse.payments.ui.model.CardCompanyUiModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CardCompanySelectBottomSheet(
+    modifier: Modifier = Modifier,
+    onCompanyClick: (CardCompanyUiModel) -> Unit,
+    onDismissRequest: () -> Unit,
+) {
+    val sheetState = rememberModalBottomSheetState()
+    var selectedCompany by remember { mutableStateOf<CardCompanyUiModel?>(null) }
+
+    LaunchedEffect(selectedCompany) {
+        selectedCompany?.let {
+            sheetState.hide()
+            onDismissRequest()
+        }
+    }
+
+    ModalBottomSheet(
+        modifier = modifier,
+        sheetState = sheetState,
+        onDismissRequest = onDismissRequest,
+        containerColor = Color.White,
+    ) {
+        CardCompanySelectRow(
+            modifier = Modifier.navigationBarsPadding(),
+            onCompanyClick = { company ->
+                selectedCompany = company
+                onCompanyClick(company)
+            },
+        )
+    }
+}
