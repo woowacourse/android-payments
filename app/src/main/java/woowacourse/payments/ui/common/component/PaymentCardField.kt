28798d409b2decbc8b71d84dd4ae65f8e903ec05
jerry8282 <ka06196@naver.com>
2025-09-21 18:18:42 +0900

    fix: 카드 만료일 가운데 정렬 오류 수정

--- app/src/main/java/woowacourse/payments/ui/common/component/PaymentCardField.kt
+++ app/src/main/java/woowacourse/payments/ui/common/component/PaymentCardField.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
@@ -84,10 +85,12 @@ fun PaymentCardField(
                     fontSize = 13.sp,
                     letterSpacing = 3.5.sp,
                     lineHeight = 1.em,
+                    textAlign = TextAlign.End,
                     modifier =
                         Modifier
-                            .align(Alignment.CenterHorizontally)
-                            .padding(bottom = 2.dp),
+                            .align(Alignment.Start)
+                            .padding(bottom = 2.dp)
+                            .padding(horizontal = 14.dp),
                 )
 
                 Row(
