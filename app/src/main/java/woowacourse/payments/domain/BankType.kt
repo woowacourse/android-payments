1869cf8cfe37f30323ad2dda021801c345b19652
Sia <lyl8661@naver.com>
2025-09-22 16:53:27 +0900

    refactor: 우리카드 영문명 변경 (uri -> woori)

--- app/src/main/java/woowacourse/payments/domain/BankType.kt
+++ app/src/main/java/woowacourse/payments/domain/BankType.kt
@@ -18,7 +18,7 @@ enum class BankType(
     SINHAN(R.string.bank_name_sinhan, R.drawable.sinhan_card_img, 0xFF0046FF.toInt()),
     KAKAO(R.string.bank_name_kakao, R.drawable.kakao_card_img, 0xFFFFE300.toInt()),
     HYUNDE(R.string.bank_name_hyunde, R.drawable.hyunde_card_img, 0xFF000000.toInt()),
-    URI(R.string.bank_name_uri, R.drawable.uri_card_img, 0xFF007BC8.toInt()),
+    WOORI(R.string.bank_name_woori, R.drawable.woori_card_img, 0xFF007BC8.toInt()),
     LOTTE(R.string.bank_name_lotte, R.drawable.lotte_card_img, 0xFFED1C24.toInt()),
     HANA(R.string.bank_name_hana, R.drawable.hana_card_img, 0xFF009490.toInt()),
     KB(R.string.bank_name_kb, R.drawable.kb_card_img, 0xFF554E45.toInt()),
