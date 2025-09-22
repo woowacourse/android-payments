405ae9427d486e9a4969840a65a63b04002f5122
medAndro <med4ndro@gmail.com>
2025-09-23 02:38:53 +0900

    refactor: 카드사 컬려명 수정 및 import하도록 수정

--- app/src/main/java/woowacourse/payments/ui/model/CardCompanyUiModel.kt
+++ app/src/main/java/woowacourse/payments/ui/model/CardCompanyUiModel.kt
@@ -4,6 +4,15 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.ui.graphics.Color
 import woowacourse.payments.R
+import woowacourse.payments.ui.theme.BCColor
+import woowacourse.payments.ui.theme.HanaColor
+import woowacourse.payments.ui.theme.HyundaiColor
+import woowacourse.payments.ui.theme.KBColor
+import woowacourse.payments.ui.theme.KakaoColor
+import woowacourse.payments.ui.theme.LotteColor
+import woowacourse.payments.ui.theme.ShinhanColor
+import woowacourse.payments.ui.theme.UnknownCard
+import woowacourse.payments.ui.theme.WooriColor
 
 enum class CardCompanyUiModel(
     @StringRes
@@ -15,48 +24,48 @@ enum class CardCompanyUiModel(
 ) {
     UNKNOWN(
         companyNameResId = R.string.card_company_unknown,
-        plateColor = woowacourse.payments.ui.theme.UnknownCard,
+        plateColor = UnknownCard,
         iconResId = null,
     ),
     BC(
         companyNameResId = R.string.bc_card_company_name,
-        plateColor = woowacourse.payments.ui.theme.BC,
+        plateColor = BCColor,
         iconResId = R.drawable.ic_bc,
     ),
     SHINHAN(
         companyNameResId = R.string.shinhan_card_company_name,
-        plateColor = woowacourse.payments.ui.theme.SHINHAN,
+        plateColor = ShinhanColor,
         iconResId = R.drawable.ic_shinhan,
     ),
     KAKAO(
         companyNameResId = R.string.kakao_card_company_name,
-        plateColor = woowacourse.payments.ui.theme.KAKAO,
+        plateColor = KakaoColor,
         iconResId = R.drawable.ic_kakao,
         textColor = Color.Black,
     ),
     HYUNDAI(
         companyNameResId = R.string.hyundai_card_company_name,
-        plateColor = woowacourse.payments.ui.theme.HYUNDAI,
+        plateColor = HyundaiColor,
         iconResId = R.drawable.ic_hyundai,
     ),
     WOORI(
         companyNameResId = R.string.woori_card_company_name,
-        plateColor = woowacourse.payments.ui.theme.WOORI,
+        plateColor = WooriColor,
         iconResId = R.drawable.ic_woori,
     ),
     LOTTE(
         companyNameResId = R.string.lotte_card_company_name,
-        plateColor = woowacourse.payments.ui.theme.LOTTE,
+        plateColor = LotteColor,
         iconResId = R.drawable.ic_lotte,
     ),
     HANA(
         companyNameResId = R.string.hana_card_company_name,
-        plateColor = woowacourse.payments.ui.theme.HANA,
+        plateColor = HanaColor,
         iconResId = R.drawable.ic_hana,
     ),
     KB(
         companyNameResId = R.string.kb_card_company_name,
-        plateColor = woowacourse.payments.ui.theme.KB,
+        plateColor = KBColor,
         iconResId = R.drawable.ic_kb,
     ),
 }
