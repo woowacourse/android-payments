edc603526fe2e19e27c964029fbf13a0a8c6633b
Sia <lyl8661@naver.com>
2025-09-22 14:58:02 +0900

    refactor: 최대 라인 제한 삭제

--- app/src/main/java/woowacourse/payments/ui/addcard/BankSelectRow.kt
+++ app/src/main/java/woowacourse/payments/ui/addcard/BankSelectRow.kt
@@ -1,7 +1,6 @@
 package woowacourse.payments.ui.addcard
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -11,7 +10,6 @@ import androidx.compose.ui.unit.dp
 import woowacourse.payments.domain.BankType
 
 private const val COLUMN_COUNT = 4
-private const val ROW_COUNT = 2
 
 @Composable
 fun BankSelectRow(
@@ -25,7 +23,6 @@ fun BankSelectRow(
                 .padding(47.dp, 55.dp),
         horizontalArrangement = Arrangement.SpaceBetween,
         maxItemsInEachRow = COLUMN_COUNT,
-        maxLines = ROW_COUNT,
         verticalArrangement = Arrangement.spacedBy(23.dp),
     ) {
         val bankList = BankType.entries.filter { it != BankType.NOT_SELECTED }
