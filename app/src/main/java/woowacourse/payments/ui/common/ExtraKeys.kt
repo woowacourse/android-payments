b96e2fee582110f1bc6d613ffaad989154c64418
DoableTuple <doabletuple@gmail.com>
2025-09-23 20:31:03 +0900

    refactor: 사용되지 않는 상수 제거

--- app/src/main/java/woowacourse/payments/ui/common/ExtraKeys.kt
+++ app/src/main/java/woowacourse/payments/ui/common/ExtraKeys.kt
@@ -2,8 +2,4 @@ package woowacourse.payments.ui.common
 
 object ExtraKeys {
     const val CARD_KEY = "card_key"
-    const val CARD_NUMBER_KEY = "card_number_key"
-    const val CARDHOLDER_NAME_KEY = "cardholder_name_key"
-    const val CARD_EXPIRATION_DATE_KEY = "card_expiration_date_key"
-    const val CARD_PASSCODE_KEY = "card_passcode_key"
 }
