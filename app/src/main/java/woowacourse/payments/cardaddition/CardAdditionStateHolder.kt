d29c5c8370b2a77eb6a0d377d1612e5e61f3f1e0
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-09-23 19:30:06 +0900

    feat: UiState 내부에서 파생된 상태 생성

--- app/src/main/java/woowacourse/payments/cardaddition/CardAdditionStateHolder.kt
+++ app/src/main/java/woowacourse/payments/cardaddition/CardAdditionStateHolder.kt
@@ -7,8 +7,10 @@ import androidx.compose.runtime.saveable.Saver
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import woowacourse.payments.BankType
+import woowacourse.payments.cardaddition.CardAdditionUiState.Companion.CARD_NUMBER_LENGTH
+import woowacourse.payments.cardaddition.CardAdditionUiState.Companion.EXPIRED_DATE_LENGTH
+import woowacourse.payments.cardaddition.CardAdditionUiState.Companion.PASSWORD_LENGTH
 import java.lang.Character.isDigit
-import java.time.Month
 
 class CardAdditionStateHolder(
     initialState: CardAdditionUiState = CardAdditionUiState(),
@@ -19,75 +21,29 @@ class CardAdditionStateHolder(
     fun updateCardNumber(value: String) {
         val newCardNumber: String = value.filter(::isDigit).take(CARD_NUMBER_LENGTH)
 
-        uiState =
-            uiState.copy(
-                cardNumber = newCardNumber,
-                isValidCardNumber = newCardNumber.isValidCardNumber,
-                canAddCard = canAddCard(isValidCardNumber = newCardNumber.isValidCardNumber),
-            )
+        uiState = uiState.copy(cardNumber = newCardNumber)
     }
 
     fun updateExpiredDate(value: String) {
-        val newDate: String = value.filter(::isDigit).take(EXPIRED_DATE_LENGTH)
+        val newExpiredDate: String = value.filter(::isDigit).take(EXPIRED_DATE_LENGTH)
 
-        uiState =
-            uiState.copy(
-                expiredDate = newDate,
-                isValidExpiredDate = newDate.isValidExpiredDate,
-                canAddCard = canAddCard(isValidExpiredDate = newDate.isValidExpiredDate),
-            )
+        uiState = uiState.copy(expiredDate = newExpiredDate)
     }
 
     fun updateHolder(value: String) {
-        uiState = uiState.copy(holder = value.take(uiState.holderMaxLength).uppercase())
+        val holder: String = value.take(uiState.holderMaxLength).uppercase()
+
+        uiState = uiState.copy(holder = holder)
     }
 
     fun updatePassword(value: String) {
         val newPassword: String = value.filter(::isDigit).take(PASSWORD_LENGTH)
 
-        uiState =
-            uiState.copy(
-                password = newPassword,
-                isValidPassword = newPassword.isValidPassword,
-                canAddCard = canAddCard(isValidPassword = newPassword.isValidPassword),
-            )
+        uiState = uiState.copy(password = newPassword)
     }
 
-    fun updateBankType(value: BankType?) {
-        uiState =
-            uiState.copy(
-                bankType = value,
-                isBankSelected = value.isBankSelected,
-                canAddCard = canAddCard(isBankSelected = value.isBankSelected),
-            )
-    }
-
-    private val String.isValidCardNumber: Boolean get() = length == CARD_NUMBER_LENGTH
-
-    private val String.isValidExpiredDate: Boolean
-        get() {
-            if (length != EXPIRED_DATE_LENGTH) return false
-
-            val month: Int = take(2).toIntOrNull() ?: return false
-
-            return runCatching { Month.of(month) }.isSuccess
-        }
-
-    private val String.isValidPassword: Boolean get() = length == PASSWORD_LENGTH
-
-    private val BankType?.isBankSelected: Boolean get() = this != null
-
-    private fun canAddCard(
-        isValidCardNumber: Boolean = uiState.isValidCardNumber,
-        isValidExpiredDate: Boolean = uiState.isValidExpiredDate,
-        isValidPassword: Boolean = uiState.isValidPassword,
-        isBankSelected: Boolean = uiState.isBankSelected,
-    ) = isValidCardNumber && isValidExpiredDate && isValidPassword && isBankSelected
-
-    companion object {
-        private const val CARD_NUMBER_LENGTH: Int = 16
-        private const val EXPIRED_DATE_LENGTH: Int = 4
-        private const val PASSWORD_LENGTH: Int = 4
+    fun updateBankType(newBankType: BankType?) {
+        uiState = uiState.copy(bankType = newBankType)
     }
 }
 
--- app/src/main/java/woowacourse/payments/cardaddition/CardAdditionUiState.kt
+++ app/src/main/java/woowacourse/payments/cardaddition/CardAdditionUiState.kt
@@ -1,20 +1,46 @@
 package woowacourse.payments.cardaddition
 
 import android.os.Parcelable
+import kotlinx.parcelize.IgnoredOnParcel
 import kotlinx.parcelize.Parcelize
 import woowacourse.payments.BankType
+import java.time.Month
 
 @Parcelize
 data class CardAdditionUiState(
     val cardNumber: String = "",
-    val isValidCardNumber: Boolean = false,
     val expiredDate: String = "",
-    val isValidExpiredDate: Boolean = false,
     val holder: String = "",
     val holderMaxLength: Int = 30,
     val password: String = "",
-    val isValidPassword: Boolean = false,
     val bankType: BankType? = null,
-    val isBankSelected: Boolean = false,
-    val canAddCard: Boolean = false,
-) : Parcelable
+) : Parcelable {
+    @IgnoredOnParcel
+    val isValidCardNumber: Boolean = cardNumber.length == CARD_NUMBER_LENGTH
+
+    @IgnoredOnParcel
+    val isValidExpiredDate: Boolean =
+        run {
+            if (expiredDate.length != EXPIRED_DATE_LENGTH) return@run false
+
+            val month: Int = expiredDate.take(2).toIntOrNull() ?: return@run false
+
+            runCatching { Month.of(month) }.isSuccess
+        }
+
+    @IgnoredOnParcel
+    val isValidPassword: Boolean = password.length == PASSWORD_LENGTH
+
+    @IgnoredOnParcel
+    val isBankSelected: Boolean = bankType != null
+
+    @IgnoredOnParcel
+    val canAddCard: Boolean =
+        isValidCardNumber && isValidExpiredDate && isValidPassword && isBankSelected
+
+    companion object {
+        const val CARD_NUMBER_LENGTH: Int = 16
+        const val EXPIRED_DATE_LENGTH: Int = 4
+        const val PASSWORD_LENGTH: Int = 4
+    }
+}
