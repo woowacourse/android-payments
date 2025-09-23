56a671db89c3ab4bf17dec77db72bded3724a7b6
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-09-23 17:24:00 +0900

    feat: 카드사별 심볼 색상 추가

---
+++ app/src/main/java/woowacourse/payments/ui/theme/SymbolColor.kt
@@ -0,0 +1,21 @@
+package woowacourse.payments.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+object SymbolColor {
+    val KB = Color(0xFF695F54)
+
+    val BC = Color(0xFFF04651)
+
+    val HANA = Color(0xFF009490)
+
+    val KAKAO = Color(0xFFFFE600)
+
+    val WOORI = Color(0xFF027BC8)
+
+    val SHINHAN = Color(0xFF0046FF)
+
+    val LOTTE = Color(0xFFED1C23)
+
+    val HYUNDAE = Color(0xFF000000)
+}
