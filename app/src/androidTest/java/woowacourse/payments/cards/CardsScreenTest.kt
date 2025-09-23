eef202c8099ee332addb5590ea854c3f7c7f7c1c
박지민 <pjm5489@naver.com>
2025-09-23 11:28:10 +0900

    test: 카드 수정화면 UI 테스트 작성

--- app/src/androidTest/java/woowacourse/payments/cards/CardsScreenTest.kt
+++ app/src/androidTest/java/woowacourse/payments/cards/CardsScreenTest.kt
@@ -24,7 +24,7 @@ class CardsScreenTest {
     @Before
     fun setUp() {
         composeTestRule.setContent {
-            CardsScreen(stateHolder = stateHolder, onCardAddClick = { })
+            CardsScreen(stateHolder = stateHolder, onCardAddClick = { }, onCardClick = {})
         }
     }
 
---
+++ app/src/androidTest/java/woowacourse/payments/component/CardInputFieldsTest.kt
@@ -0,0 +1,88 @@
+package woowacourse.payments.component
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.hasSetTextAction
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import woowacourse.payments.domain.BankType
+import woowacourse.payments.domain.Card
+import woowacourse.payments.ui.component.CardInputFields
+import woowacourse.payments.ui.registercard.CardInputFieldStateHolder
+import java.time.YearMonth
+
+class CardInputFieldsTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    private lateinit var card: Card
+    private lateinit var stateHolder: CardInputFieldStateHolder
+
+    @Before
+    fun setUp() {
+        card =
+            Card
+                .create(
+                    cardNumber = "1234567812345678",
+                    expiryDate = YearMonth.of(2095, 12),
+                    cardOwner = "뭉치",
+                    password = "1234",
+                    bankType = BankType.BC,
+                ).getOrNull()!!
+
+        stateHolder = CardInputFieldStateHolder()
+
+        composeTestRule.setContent {
+            CardInputFields(stateHolder = stateHolder)
+        }
+
+        stateHolder.setupRegisteredCardInfo(card)
+    }
+
+    @Test
+    fun `선택한_카드의_카드번호가_입력필드에_자동입력된다`() {
+        // given
+        composeTestRule.onNode(hasText("카드 번호") and hasSetTextAction())
+
+        // when & then
+        composeTestRule
+            .onNodeWithText("1234-5678-1234-5678")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun `선택한_카드의_만료일이_입력필드에_자동입력된다`() {
+        // given
+        composeTestRule.onNode(hasText("만료일") and hasSetTextAction())
+
+        // when & then
+        composeTestRule
+            .onNodeWithText("12/95")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun `선택한_카드의_카드소유자_이름이_입력필드에_자동입력된다`() {
+        // given
+        composeTestRule.onNode(hasText("카드 소유자 이름(선택)") and hasSetTextAction())
+
+        // when & then
+        composeTestRule
+            .onNodeWithText("뭉치")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun `선택한_카드의_비밀번호가_입력필드에_자동입력된다`() {
+        // given
+        composeTestRule.onNode(hasText("비밀번호") and hasSetTextAction())
+
+        // when & then
+        composeTestRule
+            .onNodeWithText("••••")
+            .assertIsDisplayed()
+    }
+}
