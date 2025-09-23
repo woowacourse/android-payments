847126d41948598d1adeac750f37f6f9736d82c5
cucumber99 <dlsguq1@gmail.com>
2025-09-23 21:50:51 +0900

    refactor: 사용하지 않는 동반 객체 제거

--- app/src/main/java/woowacourse/payments/ui/screen/cards/CardsActivity.kt
+++ app/src/main/java/woowacourse/payments/ui/screen/cards/CardsActivity.kt
@@ -1,12 +1,9 @@
 package woowacourse.payments.ui.screen.cards
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import woowacourse.payments.ui.model.CardUiModel
 import woowacourse.payments.ui.screen.cards.component.CardsScreen
 import woowacourse.payments.ui.theme.AndroidpaymentsTheme
 
@@ -19,17 +16,5 @@ class CardsActivity : ComponentActivity() {
                 CardsScreen()
             }
         }
-    }
-
-    companion object {
-        const val EXTRA_CARD = "EXTRA_CARD"
-
-        fun newIntent(
-            context: Context,
-            card: CardUiModel,
-        ): Intent =
-            Intent(context, CardsActivity::class.java).apply {
-                putExtra(EXTRA_CARD, card)
-            }
     }
 }
