523e0f46b06779f2fa21f8cd6a71872f6c211734
yrsel <xxknu88@gmail.com>
2025-09-24 17:29:46 +0900

    refactor: 카드사 선택 바텀 시트 파라미터 수정

--- app/src/main/java/woowacourse/payments/ui/registration/component/CardCompanySelectBottomSheet.kt
+++ app/src/main/java/woowacourse/payments/ui/registration/component/CardCompanySelectBottomSheet.kt
@@ -5,35 +5,24 @@ package woowacourse.payments.ui.registration.component
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.SheetState
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import woowacourse.payments.ui.model.CardCompanyUiModel
 
 @Composable
 fun CardCompanySelectBottomSheet(
+    sheetState: SheetState,
     onCardCompanyClick: (CardCompanyUiModel) -> Unit,
     onDismissRequest: () -> Unit,
     modifier: Modifier = Modifier,
-    sheetState: SheetState = rememberModalBottomSheetState(),
 ) {
     ModalBottomSheet(
-        onDismissRequest = onDismissRequest,
         sheetState = sheetState,
+        onDismissRequest = onDismissRequest,
         modifier = modifier,
     ) {
         CardCompanySelectRow(
             onCardCompanyClick = onCardCompanyClick,
         )
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun CardCompanySelectBottomSheetPreview() {
-    CardCompanySelectBottomSheet(
-        onCardCompanyClick = {},
-        onDismissRequest = {},
-    )
 }
