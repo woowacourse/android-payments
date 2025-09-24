fa0e8aef63d3e17b5d03c9fd4efb9fe3eb733aea
지원 <cheerfulboys12@gmail.com>
2025-09-24 17:26:17 +0900

    refactor: CardSelectionState 초기값 제거

--- app/src/main/java/woowacourse/payments/newCard/CardSelectionState.kt
+++ app/src/main/java/woowacourse/payments/newCard/CardSelectionState.kt
@@ -5,10 +5,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import woowacourse.payments.domain.CardCompany
 
-class CardSelectionState(
-    selectedCompany: CardCompany = CardCompany.NOT_SELECTED,
-    isShowBottomSheet: Boolean = true,
-) {
-    var selectedCompany by mutableStateOf(selectedCompany)
-    var isShowBottomSheet by mutableStateOf(isShowBottomSheet)
+class CardSelectionState {
+    var selectedCompany by mutableStateOf(CardCompany.NOT_SELECTED)
+    var isShowBottomSheet by mutableStateOf(true)
 }
