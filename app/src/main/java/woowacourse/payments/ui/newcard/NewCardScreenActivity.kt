36bda86ae1e1d7691467e4969e38e0492634a728
devfeijoa <devfeijoa@gmail.com>
2025-09-24 16:04:54 +0900

    feat: 카드 수정 기능 추가
    - `NewCardScreenActivity`에서 기존 카드 정보를 받아올 수 있도록 수정했습니다.
    - 카드 정보를 수정하고 저장하면, `CardWalletScreen`에서 해당 카드의 정보가 업데이트됩니다.
    - `NewCardScreenActivity`의 `newIntent` 함수에 `existingCard`와 `index` 파라미터를 추가하여, 카드 수정 시 기존 카드 정보와 인덱스를 전달할 수 있도록 했습니다.

--- app/src/main/java/woowacourse/payments/ui/newcard/NewCardScreenActivity.kt
+++ app/src/main/java/woowacourse/payments/ui/newcard/NewCardScreenActivity.kt
@@ -6,18 +6,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import woowacourse.payments.ui.model.CardUiModel
 import woowacourse.payments.ui.theme.AndroidpaymentsTheme
 
 class NewCardScreenActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val existingCard = intent.getParcelableExtra<CardUiModel>(EXISTING_CARD)
+        val existingCardIndex = intent.getIntExtra(EXISTING_CARD_INDEX, -1).takeIf { it != -1 }
+
         setContent {
             AndroidpaymentsTheme {
                 NewCardScreen(
+                    existingCard = existingCard,
                     onBackClick = { finish() },
                     onSaveClick = { paymentCard ->
-                        setResult(RESULT_OK, Intent().putExtra(ADD_NEW_CARD, paymentCard))
+                        val resultIntent =
+                            Intent().apply {
+                                putExtra(ADD_NEW_CARD, paymentCard)
+                                existingCardIndex?.let { putExtra(EXISTING_CARD_INDEX, it) }
+                            }
+                        setResult(RESULT_OK, resultIntent)
                         finish()
                     },
                 )
@@ -27,7 +38,17 @@ class NewCardScreenActivity : ComponentActivity() {
 
     companion object {
         const val ADD_NEW_CARD = "ADD_NEW_CARD"
+        const val EXISTING_CARD = "EXISTING_CARD"
+        const val EXISTING_CARD_INDEX = "EXISTING_CARD_INDEX"
 
-        fun newIntent(context: Context): Intent = Intent(context, NewCardScreenActivity::class.java)
+        fun newIntent(
+            context: Context,
+            existingCard: CardUiModel? = null,
+            index: Int? = null,
+        ): Intent =
+            Intent(context, NewCardScreenActivity::class.java).apply {
+                existingCard?.let { putExtra(EXISTING_CARD, it) }
+                index?.let { putExtra(EXISTING_CARD_INDEX, it) }
+            }
     }
 }
