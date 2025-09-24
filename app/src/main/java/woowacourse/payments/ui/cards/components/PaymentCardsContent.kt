89a80ad6d55f008d91694923d11afeddb16c0a11
rosemin928 <rosemin928@naver.com>
2025-09-24 14:29:36 +0900

    refactor: lazyColumn 마이그레이션

--- app/src/main/java/woowacourse/payments/ui/cards/components/PaymentCardsContent.kt
+++ app/src/main/java/woowacourse/payments/ui/cards/components/PaymentCardsContent.kt
@@ -1,8 +1,12 @@
 package woowacourse.payments.ui.cards.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -90,18 +94,20 @@ private fun MultiCards(
     onEditCard: (String) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Column(
+    LazyColumn(
         modifier = modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
+        contentPadding = PaddingValues(vertical = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
     ) {
-        paymentCards.forEach { card ->
+        items(
+            items = paymentCards,
+            key = { it.id },
+        ) { card ->
             PaymentCard(
                 paymentCard = card,
                 onSelectBank = {},
                 onEditCard = { onEditCard(card.id) },
-                modifier =
-                    Modifier
-                        .padding(top = 16.dp),
             )
         }
     }
