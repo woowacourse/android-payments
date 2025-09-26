e8fe4d893e2de6d70bc839e267373772b7de48d9
yrsel <xxknu88@gmail.com>
2025-09-26 09:10:09 +0900

    refactor: 카드 등록 UiState 파라미터 변경

--- app/src/main/java/woowacourse/payments/ui/registration/state/CardRegistrationScreenUiState.kt
+++ app/src/main/java/woowacourse/payments/ui/registration/state/CardRegistrationScreenUiState.kt
@@ -2,11 +2,10 @@ package woowacourse.payments.ui.registration.state
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
-import woowacourse.payments.ui.model.CardCompanyUiModel
 import woowacourse.payments.ui.model.CardUiModel
 
 @Parcelize
 data class CardRegistrationScreenUiState(
     val card: CardUiModel = CardUiModel(),
-    val cardCompany: CardCompanyUiModel = CardCompanyUiModel.NOT_SELECT,
+    val isBottomSheetOpen: Boolean = true,
 ) : Parcelable
