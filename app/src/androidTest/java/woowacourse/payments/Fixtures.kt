cb7d1b292d34215108de256b82771b822bd082c3
DoableTuple <doabletuple@gmail.com>
2025-09-27 21:37:49 +0900

    refactor: 테스트 코드 수정

--- app/src/androidTest/java/woowacourse/payments/Fixtures.kt
+++ app/src/androidTest/java/woowacourse/payments/Fixtures.kt
@@ -1,20 +1,15 @@
 package woowacourse.payments
 
-import woowacourse.payments.domain.Card
 import woowacourse.payments.domain.CardCompany
-import woowacourse.payments.domain.CardNumber
-import woowacourse.payments.domain.CardholderName
-import woowacourse.payments.domain.ExpirationDate
-import woowacourse.payments.domain.Passcode
 import woowacourse.payments.ui.model.CardUiModel
 import woowacourse.payments.ui.model.toUiModel
-import java.time.YearMonth
 
 val CARD_FIXTURE: CardUiModel =
-    Card(
-        CardNumber("1234123412341234"),
-        ExpirationDate(YearMonth.of(2099, 12)),
-        CardholderName("디랙"),
-        Passcode("1234"),
-        CardCompany.KB_CARD,
-    ).toUiModel()
+    CardUiModel(
+        id = 0,
+        cardNumber = "1234123412341234",
+        expirationDate = "1299",
+        cardholderName = "디랙",
+        passcode = "1234",
+        cardCompany = CardCompany.KB_CARD.toUiModel(),
+    )
