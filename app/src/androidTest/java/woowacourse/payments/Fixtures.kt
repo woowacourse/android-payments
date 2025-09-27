a030c096ba210c4dec7859c7d1559e29b1a2b09b
DoableTuple <doabletuple@gmail.com>
2025-09-27 23:24:24 +0900

    test: 테스트 코드 수정

--- app/src/androidTest/java/woowacourse/payments/Fixtures.kt
+++ app/src/androidTest/java/woowacourse/payments/Fixtures.kt
@@ -4,9 +4,19 @@ import woowacourse.payments.domain.CardCompany
 import woowacourse.payments.ui.model.CardUiModel
 import woowacourse.payments.ui.model.toUiModel
 
-val CARD_FIXTURE: CardUiModel =
+val CARD_FIXTURE_0: CardUiModel =
     CardUiModel(
         id = 0,
+        cardNumber = "1234123412341234",
+        expirationDate = "1299",
+        cardholderName = "디랙",
+        passcode = "1234",
+        cardCompany = CardCompany.KB_CARD.toUiModel(),
+    )
+
+val CARD_FIXTURE_1: CardUiModel =
+    CardUiModel(
+        id = 1,
         cardNumber = "1234123412341234",
         expirationDate = "1299",
         cardholderName = "디랙",
