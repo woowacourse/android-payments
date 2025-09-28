60c88c0dc1f80173f53c2ce5afc84768170de7a0
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-09-28 15:46:45 +0900

    fix: `preview`에서도 `SelectedCardCompanyBottomSheet`가 보일 수 있도록, `SheetState`를 파라미터로 인자를 주입함

--- app/src/main/java/woowacourse/payments/ui/newcard/component/SelectedCardCompanyBottomSheet.kt
+++ app/src/main/java/woowacourse/payments/ui/newcard/component/SelectedCardCompanyBottomSheet.kt
@@ -2,52 +2,48 @@ package woowacourse.payments.ui.newcard.component
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.SheetValue
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
-import woowacourse.payments.ui.newcard.state.CardStateHolder
 import woowacourse.payments.ui.model.CardCompanyUiModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SelectedCardCompanyBottomSheet(
-    cardCompanyUiModel: CardCompanyUiModel = CardCompanyUiModel.Default,
     changeBottomSheet: () -> Unit,
     selectedCardCompany: (CardCompanyUiModel) -> Unit,
+    modifier: Modifier = Modifier,
+    sheetState: SheetState = rememberModalBottomSheetState()
 ) {
-
-    val bottomSheetState = rememberModalBottomSheetState(
-        skipPartiallyExpanded = false,
-    )
-
-    LaunchedEffect(key1 = cardCompanyUiModel) {
-        if (cardCompanyUiModel != CardCompanyUiModel.Default) {
-            bottomSheetState.hide()
-            changeBottomSheet()
-        } else {
-            bottomSheetState.show()
-        }
-    }
-
     ModalBottomSheet(
-        sheetState = bottomSheetState,
-        onDismissRequest = { changeBottomSheet() },
+        sheetState = sheetState,
+        onDismissRequest = changeBottomSheet,
     ) {
         SelectedBankRow(
             selectedBank = { selectedCard ->
                 selectedCardCompany(selectedCard)
+                changeBottomSheet()
             },
+            modifier = modifier,
         )
     }
 }
 
-@Preview
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
 @Composable
 private fun SelectedCardCompanyBottomSheetPreview() {
-    SelectedCardCompanyBottomSheet(CardCompanyUiModel.Default, {}, {})
+    SelectedCardCompanyBottomSheet(
+        changeBottomSheet = { },
+        selectedCardCompany = {},
+        sheetState = SheetState(
+            skipPartiallyExpanded = true,
+            density = LocalDensity.current,
+            initialValue = SheetValue.Expanded,
+        )
+    )
 }
