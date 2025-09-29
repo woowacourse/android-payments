044a6e588ea56902f8d9b0f304a85c1613ab3327
박지민 <pjm5489@naver.com>
2025-09-29 19:16:04 +0900

    refactor: `toYearMonth()`에서 불필요한 null 검사 제거

--- app/src/main/java/woowacourse/payments/ui/YearMonthFormat.kt
+++ app/src/main/java/woowacourse/payments/ui/YearMonthFormat.kt
@@ -6,14 +6,11 @@ import java.time.format.DateTimeFormatter
 fun String.toYearMonth(): YearMonth? {
     val yearOffset = 2000
     if (length != 4) return null
-    val year = substring(2, 4).toIntOrNull()
-    val month = substring(0, 2).toIntOrNull()
+    val year = substring(2, 4).toIntOrNull() ?: return null
+    val month = substring(0, 2).toIntOrNull() ?: return null
     if (month !in 1..12) return null
-    return if (year == null || month == null) {
-        null
-    } else {
-        YearMonth.of(yearOffset + year, month)
-    }
+
+    return YearMonth.of(yearOffset + year, month)
 }
 
 fun YearMonth.toYearMonthString(): String = format(DateTimeFormatter.ofPattern("MM/yy"))
